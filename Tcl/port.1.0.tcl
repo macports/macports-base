8020deee5aaaed6f957103f2a6eb909db8fde990
Landon Fuller <landonf@macports.org>
2002-08-01 19:28:26 +0000

    Require 1.0 versions for consistency
    git-svn-id: https://svn.macports.org/repository/macports/trunk/base@36 d073be05-634f-4543-b044-5fe20cf6d1d6

--- Tcl/port.1.0.tcl
+++ Tcl/port.1.0.tcl
@@ -1,8 +1,8 @@
 # standard package load
 package provide port 1.0
 
-package require portpatch
-package require portchecksum
-package require portextract
-package require portfetch
-package require portmain
+package require portpatch 1.0
+package require portchecksum 1.0
+package require portextract 1.0
+package require portfetch 1.0
+package require portmain 1.0
