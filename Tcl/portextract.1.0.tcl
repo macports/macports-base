758009de7acc901f30665290d7a98e816875abc6
Landon Fuller <landonf@macports.org>
2002-08-01 07:43:51 +0000

    Not-yet-working portextract
    git-svn-id: https://svn.macports.org/repository/macports/trunk/base@27 d073be05-634f-4543-b044-5fe20cf6d1d6

--- Tcl/portextract.1.0.tcl
+++ Tcl/portextract.1.0.tcl
@@ -7,8 +7,30 @@ namespace eval portextract {
 	variable options
 }
 
+# define globals
+globals portextract::options
+
+# define options
+options portextract::options extract_only extract_command extract_before_args extract_after_args
+
 proc portextract::main {args} {
-	global portname
+	global portname workdir distfiles
+
+	# Set up defaults
+	default portextract::options extract_only $distfiles
+	default portextract::options extract_cmd gzip
+	default portextract::options extract_before_args -dc
+	default portextract::options extract_after_args "|tar -xf -"
+
+	set portdir [pwd]
+
+	if [file exists $workdir] {
+		file delete -force $workdir
+	}
+
+	file mkdir $workdir
+	cd $workdir
+
 	puts "Extracting port: $portname"
 	return 0
 }
