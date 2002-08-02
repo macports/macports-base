be44dd5266badea0eb1b1bd0c2e8a56cd5d7b313
Landon Fuller <landonf@macports.org>
2002-08-02 04:11:02 +0000

    make order match actual order and add portconfigure
    git-svn-id: https://svn.macports.org/repository/macports/trunk/base@50 d073be05-634f-4543-b044-5fe20cf6d1d6

--- Tcl/port1.0/port.tcl
+++ Tcl/port1.0/port.tcl
@@ -1,8 +1,9 @@
 # standard package load
 package provide port 1.0
 
-package require portpatch 1.0
+package require portmain 1.0
+package require portfetch 1.0
 package require portchecksum 1.0
 package require portextract 1.0
-package require portfetch 1.0
-package require portmain 1.0
+package require portpatch 1.0
+package require portconfigure 1.0
