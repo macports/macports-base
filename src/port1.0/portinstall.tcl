93336bd69d33ab4c386c62fb87d99248e6ee0721
Landon Fuller <landonf@macports.org>
2002-08-21 11:24:05 +0000

    Empty portinstall target file
    git-svn-id: https://svn.macports.org/repository/macports/trunk/base@234 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ src/port1.0/portinstall.tcl
@@ -0,0 +1,23 @@
+# ex:ts=4
+#
+# Insert some license text here at some point soon.
+#
+
+package provide portinstall 1.0
+package require portutil 1.0
+
+register com.apple.install target build install_main
+register com.apple.install provides install
+register com.apple.install requires main fetch extract checksum patch configure build
+register com.apple.install swdep build depends_run depends_lib
+
+# define options
+#options make.cmd make.type make.target.all make.target.install
+
+set UI_PREFIX "---> "
+
+proc install_main {args} {
+
+    return 0
+}
+
