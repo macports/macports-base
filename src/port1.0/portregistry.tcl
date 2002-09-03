66e9aef177ddf4a5c23791e881ee5d94b4aeaaac
Jordan K. Hubbard <jkh@macports.org>
2002-09-03 10:25:31 +0000

    Don't attempt to md5 checksum directories
    git-svn-id: https://svn.macports.org/repository/macports/trunk/base@509 d073be05-634f-4543-b044-5fe20cf6d1d6

--- src/port1.0/portregistry.tcl
+++ src/port1.0/portregistry.tcl
@@ -100,7 +100,7 @@ proc fileinfo_for_file {fname} {
     global registry.nochecksum
 
     if ![catch {file stat $fname statvar}] {
-	if ![tbool registry.nochecksum] {
+	if {![tbool registry.nochecksum] && ![file isdirectory $fname]} {
 	    set md5regex "^(MD5)\[ \]\\(($fname)\\)\[ \]=\[ \](\[A-Za-z0-9\]+)\n$"
 	    set pipe [open "|md5 $fname" r]
 	    set line [read $pipe]
@@ -128,12 +128,12 @@ proc fileinfo_for_entry {rval dir entry} {
 	    set subpath [file join $path $name]
 	    if [file isdirectory $subpath] {
 		fileinfo_for_entry myrval $subpath ""
-		lappend myrval [fileinfo_for_file $subpath]
 	    } elseif [file readable $subpath] {
 		lappend myrval [fileinfo_for_file $subpath]
 	    }
 	}
-    } elseif [file readable $path] {
+    }
+    if [file readable $path] {
 	lappend myrval [fileinfo_for_file $path]
     }
     return $myrval
