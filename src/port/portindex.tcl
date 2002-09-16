98e022f25bf3c0d951d1453a1c9ab95527872f0f
Landon Fuller <landonf@macports.org>
2002-09-16 11:26:51 +0000

    Add archiving and output directory option to assist in creating remote port download sites
    git-svn-id: https://svn.macports.org/repository/macports/trunk/base@659 d073be05-634f-4543-b044-5fe20cf6d1d6

--- src/port/portindex.tcl
+++ src/port/portindex.tcl
@@ -1,6 +1,6 @@
 #!/usr/bin/tclsh
-# Traverse through all ports running the supplied target.  If target is
-# "index" then just print some useful information about each port.
+# Traverse through all ports, creating an index and archiving port directories
+# if requested
 
 package require darwinports
 dportinit
@@ -11,14 +11,15 @@ set archive 0
 # Standard procedures
 proc print_usage args {
     global argv0
-    puts "Usage: $argv0 \[-a\] \[directory\]"
+    puts "Usage: $argv0 \[-a\] \[-o output directory\] \[directory\]"
     puts "-a:\tArchive port directories (for remote sites)"
+    puts "-o:\tOutput all files to specified directory"
 }
 
 proc port_traverse {func {dir .} {cwd ""}} {
     set pwd [pwd]
     if [catch {cd $dir} err] {
-	ui_error $err
+	puts $err
 	return
     }
     foreach name [readdir .] {
@@ -37,28 +38,34 @@ proc port_traverse {func {dir .} {cwd ""}} {
 }
 
 proc pindex {portdir} {
-    global target fd directory archive
+    global target fd directory archive outdir
     set interp [dportopen file://[file join $directory $portdir]]
     array set portinfo [dportinfo $interp]
     dportclose $interp
     set portinfo(portdir) $portdir
     puts "Adding port $portinfo(portname)"
+    if {$archive == "1"} {
+        if ![file isdirectory [file join $outdir [file dirname $portdir]]] {
+            if {[catch {file mkdir -p [file join $outdir [file dirname $portdir]]} result]} {
+                puts "$result"
+                exit 1
+            }
+        }
+        set portinfo(portarchive) [file join [file dirname $portdir] [file tail $portdir]].tgz
+        cd $directory
+        puts "Archiving port $portinfo(portname) to [file join $outdir $portinfo(portarchive)]"
+        if {[catch {exec tar -cf - [file join $portdir] | gzip -c >[file join $outdir $portinfo(portarchive)]} result]} {
+            puts "Failed to create port archive $portinfo(portarchive): $result"
+            exit 1
+        }
+    }
     set output [array get portinfo]
     set len [expr [string length $output] + 1]
     puts $fd "$portinfo(portname) $len"
     puts $fd $output
-    if {$archive == "1"} {
-        set portinfo(portarchive) [file join [file dirname $portdir] [file tail $portdir]].tgz
-        cd $directory
-        puts "Archiving port $portinfo(portname) to $portinfo(portarchive)"
-        if {[catch {exec tar -cf - [file join $portdir] | gzip -c >[file join $directory $portinfo(portarchive)]} result]} {
-            puts "Failed to create port archive $portinfo(portarchive): $result"
-        }
-    }
 }
 
-set fd [open PortIndex w]
-if {[expr $argc > 2]} {
+if {[expr $argc > 3]} {
     print_usage
     exit 1
 }
@@ -67,8 +74,11 @@ for {set i 0} {$i < $argc} {incr i} {
     set arg [lindex $argv $i]
     switch -regex -- $arg {
         {^-.+} {
-            if {$arg == "-a"} {
+            if {$arg == "-a"} { # Turn on archiving
                 set archive 1
+	    } elseif {$arg == "-o"} { # Set output directory
+		incr i
+		set outdir [lindex $argv $i]
 	    } else {
 		puts "Unknown option: $arg"
 		print_usage
@@ -82,11 +92,31 @@ if {![info exists directory]} {
     set directory .
 }
 
+# cd to input directory 
 if {[catch {cd $directory} result]} {
    puts "$result"
    exit 1
+} else {
+   set directory [pwd]
 }
 
-set directory [pwd]
+# Set output directory to full path
+if [info exists outdir] {
+    if {[catch {file mkdir $outdir} result]} {
+        puts "$result"
+        exit 1
+    }
+    if {[catch {cd $outdir} result]} {
+        puts "$result"
+        exit 1
+    } else {
+        set outdir [pwd]
+    }
+} else {
+    set outdir $directory
+}
+
+puts "Creating software index in $outdir"
+set fd [open [file join $outdir PortIndex] w]
 port_traverse pindex $directory
 close $fd
