a67a22e684dbe683592e65c3f60160d4b8841170
Landon Fuller <landonf@macports.org>
2002-09-29 09:43:57 +0000

    Don't jump indexes if we found matches in the first index This will be entirely less relavent when/if individuals don't have multiple indexes containing the *same* software
    git-svn-id: https://svn.macports.org/repository/macports/trunk/base@838 d073be05-634f-4543-b044-5fe20cf6d1d6

--- src/darwinports1.0/darwinports.tcl
+++ src/darwinports1.0/darwinports.tcl
@@ -288,12 +288,16 @@ proc dportsearch {regexp} {
                 }
                 lappend matches $name
                 lappend matches $line
+		set match 1
             } else {
                 set len [lindex $line 1]
                 seek $fd $len current
             }
         }
         close $fd
+	if {$match == 1} {
+	    break
+	}
     }
     return $matches
 }
