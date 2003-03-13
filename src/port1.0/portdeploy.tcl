e0e56128e38151beb9e8bf6b789f0949eec930b8
Kevin Van Vechten <kevin@macports.org>
2003-03-13 01:21:29 +0000

    Adding new targets. Bug: Submitted by: Reviewed by: Approved by: Obtained from:
    git-svn-id: https://svn.macports.org/repository/macports/branches/kevin-target-api/base@2256 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ src/port1.0/portdeploy.tcl
@@ -0,0 +1,158 @@
+# et:ts=4
+# portinstall.tcl
+#
+# Copyright (c) 2003 Kevin Van Vechten <kevin@opendarwin.org>
+# Copyright (c) 2002 - 2003 Apple Computer, Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. Neither the name of Apple Computer, Inc. nor the names of its contributors
+#    may be used to endorse or promote products derived from this software
+#    without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+
+PortTarget 1.0
+
+name			org.opendarwin.deploy
+maintainers		kevin@opendarwin.org
+description		Copies the contents of destroot onto the system.
+runtype			always
+requires		install
+provides		deploy
+
+proc main {args} {
+    global portname portversion portpath categories description long_description homepage depends_run installPlist package-install uninstall workdir worksrcdir prefix UI_PREFIX destroot
+
+	# Prune empty directories in [option destroot]
+	catch {system "find \"[option destroot]\" -depth -type d -print | xargs rmdir"}
+
+    # Install [option destroot] contents into /
+    directory_dig [option destroot] [option destroot]
+
+    # Package installed successfully, so now we must register it
+    set rhandle [registry_new [option name] [option version]]
+
+    registry_store $rhandle [list prefix [option prefix]]
+    registry_store $rhandle [list categories [option categories]]
+    if [exists description] {
+	registry_store $rhandle [concat description [option description]]
+    }
+    if [exists long_description] {
+	registry_store $rhandle [concat long_description [option long_description]]
+    }
+    if [exists homepage] {
+	registry_store $rhandle [concat homepage [option homepage]]
+    }
+    if [exists depends_run] {
+	registry_store $rhandle [list run_depends [option depends_run]]
+    }
+    if [exists package-install] {
+	registry_store $rhandle [concat package-install [option package-install]]
+    }
+    if [exists installPlist] {
+	registry_store $rhandle [list contents [fileinfo_for_index [option installPlist]]]
+    }
+    if {[info proc pkg_uninstall] == "pkg_uninstall"} {
+	registry_store $rhandle [list uninstall [proc_disasm pkg_uninstall]]
+    }
+    registry_close $rhandle
+    return 0
+}
+
+proc proc_disasm {pname} {
+    set p "proc "
+    append p $pname " {"
+    set space ""
+    foreach arg [info args $pname] {
+	if [info default $pname $arg value] {
+	    append p "$space{" [list $arg $value] "}"
+	} else {
+	    append p $space $arg
+	}
+	set space " "
+    }
+    append p "} {" [info body $pname] "}"
+    return $p
+}
+
+set UI_PREFIX "---> "
+
+proc install_element {src_element dst_element} {
+# don't recursively copy directories
+    if {[file isdirectory $src_element]} {
+	file mkdir $dst_element
+    } else {
+	file copy -force $src_element $dst_element
+    }
+
+    # if the file is a symlink, do not try to set file attributes
+    if {[file type $src_element] != "link"} {
+	set attributes [file attributes $src_element]	
+	for {set i 0} {$i < [llength $attributes]} {incr i} {
+	    set opt [lindex $attributes $i]
+	    incr i
+	    set arg [lindex $attributes $i]
+	    file attributes $dst_element $opt $arg
+	}
+    }
+}
+
+proc directory_dig {rootdir workdir {cwd ""}} {
+    global installPlist
+    set pwd [pwd]
+    if [catch {cd $workdir} err] {
+	puts $err
+	return
+    }
+
+    foreach name [readdir .] {
+	if {[string match $name "."] || [string match $name ".."]} {
+	    continue
+	}
+	set element [file join $cwd $name]
+
+	# XXX jpm's cross-platform code to find file separator
+	# replace with [file seperator] with tcl 8.4
+	if {![info exists root]} {
+	    if {[string match [file tail "/monkey"] "monkey"]} {
+		set root "/"
+	    } elseif {[string match [file tail ":monkey"] "monkey"]} {
+		set root ":" 
+	    } else {
+		set root "\\"		
+	    }
+	}
+
+	set dst_element [file join $root $element]
+	set src_element [file join $rootdir $element]
+	# overwrites files but not directories
+	if {![file exists $dst_element] || ![file isdirectory $dst_element]} {
+	    ui_debug "installing file: $dst_element"
+	    install_element $src_element $dst_element
+	    lappend installPlist $dst_element
+	}
+	if {[file isdirectory $name] && [file type $name] != "link"} {
+	    directory_dig $rootdir $name [file join $cwd $name]
+	}
+    }
+    cd $pwd
+}
