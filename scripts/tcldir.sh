ac82c40cd3495b982a65d0adb9641f0aa33b2a19
Jordan K. Hubbard <jkh@macports.org>
2003-03-30 10:01:17 +0000

    Update for future releases...
    git-svn-id: https://svn.macports.org/repository/macports/trunk/base@2380 d073be05-634f-4543-b044-5fe20cf6d1d6

--- scripts/tcldir.sh
+++ scripts/tcldir.sh
@@ -3,7 +3,13 @@
 # Just tell us where the Tcl installation directory is.
 
 case `uname -s` in
-        Darwin) echo /System/Library/Tcl/8.3/darwinports1.0 ;;
+        Darwin)
+	    if [ `uname -r` = 7.0 ]; then
+		echo /System/Library/Tcl/8.4/darwinports1.0
+	    else
+		echo /System/Library/Tcl/8.3/darwinports1.0
+	    fi
+	    ;;
 	Linux) echo /usr/lib/tcl8.3/darwinports1.0 ;;
 	NetBSD) echo /usr/pkg/lib/tcl8.3/darwinports1.0 ;;
         *) echo /usr/local/lib/tcl8.3/darwinports1.0 ;;
