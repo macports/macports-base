611de111e34cb59649a1687e24827570382f6393
Jordan K. Hubbard <jkh@macports.org>
2003-08-05 21:02:50 +0000

    s/destroot.vars/destroot.destdir/
    It's also an imperfect name, but probably does a better job of describing
    the purpose of this variable.
    Approved by:    landonf
    
    git-svn-id: https://svn.macports.org/repository/macports/trunk/base@3179 d073be05-634f-4543-b044-5fe20cf6d1d6

--- src/port1.0/portdestroot.tcl
+++ src/port1.0/portdestroot.tcl
@@ -48,7 +48,7 @@ default destroot.cmd {${build.cmd}}
 default destroot.pre_args {${destroot.target}}
 default destroot.target install
 default destroot.post_args {${destroot.vars}}
-default destroot.vars {DESTDIR=${destroot}}
+default destroot.destdir {DESTDIR=${destroot}}
 default destroot.clean no
 
 set UI_PREFIX "---> "
