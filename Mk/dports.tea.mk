2fe5a30b58dfbc6503e6f4745688965875ae369f
Landon Fuller <landonf@macports.org>
2003-08-21 23:17:47 +0000

    Move src/pextlib1.0/config.h.in to src/config.h.in
    git-svn-id: https://svn.macports.org/repository/macports/trunk/base@3314 d073be05-634f-4543-b044-5fe20cf6d1d6

--- Mk/dports.tea.mk
+++ Mk/dports.tea.mk
@@ -1,7 +1,7 @@
 .c.o:
 	${CC} -c -DUSE_TCL_STUBS ${CFLAGS} ${TCL_DEFS} ${SHLIB_CFLAGS} $< -o $@
 
-$(SHLIB_NAME):: ${OBJS} config.h
+$(SHLIB_NAME):: ${OBJS}
 	${SHLIB_LD} ${OBJS} -o ${SHLIB_NAME} ${TCL_STUB_LIB_SPEC} ${LIBS}
 
 all:: ${SHLIB_NAME}
@@ -10,7 +10,6 @@ clean::
 	rm -f ${OBJS} ${SHLIB_NAME} so_locations
 
 distclean:: clean
-	rm -f config.h
 
 install:: all
 	$(INSTALL) -d -o ${DSTUSR} -g ${DSTGRP} -m 775 ${INSTALLDIR}
