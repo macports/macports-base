6db1c09d6eb930a7dc9a7d0dbda76e583ac41f38
Landon Fuller <landonf@macports.org>
2003-10-24 07:34:33 +0000

    Fix error output; For whatever reason, I included a trailing \0 in each line buffer Each line buffer was added to the errbuf Tcl_Obj using Tcl_AppendToObj, still containing a trailing NULL. When the command output was printed, the output stopped when the first \0 was hit. I've removed the use of \0 - we track the buffer size elsewhere, there's no need for trailing NULLs.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/base@3790 d073be05-634f-4543-b044-5fe20cf6d1d6

--- src/pextlib1.0/Pextlib.c
+++ src/pextlib1.0/Pextlib.c
@@ -251,13 +251,12 @@ int SystemCmd(ClientData clientData, Tcl_Interp *interp, int objc, Tcl_Obj *CONS
 		int slen;
 
 		/*
-		 * Allocate enough space to insert a terminating
-		 * '\0' if the line is not terminated with a '\n'
+		 * Allocate enough space to hold buf, minus terminating '\n'
 		 */
 		if (buf[linelen - 1] == '\n')
-			slen = linelen;
+			slen = linelen - 1;
 		else
-			slen = linelen + 1;
+			slen = linelen;
 
 		if (circbuf[pos].len == 0)
 			sbuf = malloc(slen);
@@ -274,8 +273,6 @@ int SystemCmd(ClientData clientData, Tcl_Interp *interp, int objc, Tcl_Obj *CONS
 		}
 
 		memcpy(sbuf, buf, linelen);
-		/* terminate line with '\0',replacing '\n' if it exists */
-		sbuf[slen - 1] = '\0';
 
 		circbuf[pos].line = sbuf;
 		circbuf[pos].len = slen;
@@ -304,7 +301,7 @@ int SystemCmd(ClientData clientData, Tcl_Interp *interp, int objc, Tcl_Obj *CONS
 			errbuf = Tcl_NewStringObj(NULL, 0);
 			for (fline = pos; pos < fline + CBUFSIZ; pos++) {
 				if (circbuf[pos % CBUFSIZ].len == 0)
-				continue; /* skip empty lines */
+					continue; /* skip empty lines */
 
 				Tcl_AppendToObj(errbuf, circbuf[pos % CBUFSIZ].line,
 						circbuf[pos % CBUFSIZ].len);
@@ -326,7 +323,7 @@ int SystemCmd(ClientData clientData, Tcl_Interp *interp, int objc, Tcl_Obj *CONS
 			Tcl_AppendToObj(tcl_result, cmdstring, -1);
 			Tcl_AppendToObj(tcl_result, "\" returned error ", -1);
 			Tcl_AppendObjToObj(tcl_result, Tcl_NewIntObj(WEXITSTATUS(ret)));
-			Tcl_AppendToObj(tcl_result, "\nCommand output: ", -1);
+			Tcl_AppendToObj(tcl_result, "\nCommand output:\n", -1);
 			Tcl_AppendObjToObj(tcl_result, errbuf);
 			Tcl_SetObjResult(interp, tcl_result);
 			return TCL_ERROR;
