91b04eb2b8f5dd05e5bfecd7fc55fd419ff14cdd
Jordan K. Hubbard <jkh@macports.org>
2004-04-08 08:52:56 +0000

    Unbreak wacky indentation.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/base@6272 d073be05-634f-4543-b044-5fe20cf6d1d6

--- src/port1.0/resources/group/perl5-1.0.tcl
+++ src/port1.0/resources/group/perl5-1.0.tcl
@@ -1,4 +1,4 @@
-# $Id: perl5-1.0.tcl,v 1.2 2004/03/31 16:08:16 toby Exp $
+# $Id: perl5-1.0.tcl,v 1.3 2004/04/08 08:52:56 jkh Exp $
 # perl5-1.0.tcl
 # 
 # Group file for 'perl5' group.
@@ -8,13 +8,13 @@
 set perl5.bin ${prefix}/bin/perl
 
 proc perl5.extract_config {var {default ""}} {
-	global perl5.bin
+    global perl5.bin
 
-	if {[catch {set val [lindex [split [exec ${perl5.bin} -V:${var}] {'}] 1]}]} {
-		set val ${default}
-	}
+    if {[catch {set val [lindex [split [exec ${perl5.bin} -V:${var}] {'}] 1]}]} {
+	set val ${default}
+    }
 
-	return $val
+    return $val
 }
 
 set perl5.version [perl5.extract_config version]
@@ -30,45 +30,45 @@ set perl5.cpandir ""
 
 # perl5 group setup procedure
 proc perl5.setup {module vers {cpandir ""}} {
-	global perl5.bin perl5.lib perl5.module perl5.cpandir
+    global perl5.bin perl5.lib perl5.module perl5.cpandir
 
-	# define perl5.module
-	set perl5.module ${module}
+    # define perl5.module
+    set perl5.module ${module}
 
-	# define perl5.cpandir
-	# check if optional CPAN dir specified to perl5.setup
-	if {[string length ${cpandir}] == 0} {
-		# if not, default to the first word (before a dash) from the
-		# module name, this is the normal convention on CPAN
-		set perl5.cpandir [lindex [split ${perl5.module} {-}] 0]
-	} else {
-		# else, use what was passed
-		set perl5.cpandir ${cpandir}
+    # define perl5.cpandir
+    # check if optional CPAN dir specified to perl5.setup
+    if {[string length ${cpandir}] == 0} {
+	# if not, default to the first word (before a dash) from the
+	# module name, this is the normal convention on CPAN
+	set perl5.cpandir [lindex [split ${perl5.module} {-}] 0]
+    } else {
+	# else, use what was passed
+	set perl5.cpandir ${cpandir}
+    }
+
+    name			p5-[string tolower ${perl5.module}]
+    version			${vers}
+    categories			perl
+    homepage			http://search.cpan.org/dist/${perl5.module}/
+
+    master_sites		perl_cpan:${perl5.cpandir}
+    distname			${perl5.module}-${vers}
+    dist_subdir			perl5
+
+    depends_build		path:${perl5.bin}:perl5.8
+
+    configure.cmd		${perl5.bin}
+    configure.pre_args		Makefile.PL
+    configure.args		INSTALLDIRS=vendor
+
+    test.run			yes
+
+    destroot.target		pure_install
+
+    post-destroot {
+	foreach packlist [exec find ${destroot}${perl5.lib} -name .packlist] {
+	    ui_info "Fixing packlist ${packlist}"
+	    reinplace "s|${destroot}||" ${packlist}
 	}
-
-	name				p5-[string tolower ${perl5.module}]
-	version				${vers}
-	categories			perl
-	homepage			http://search.cpan.org/dist/${perl5.module}/
-
-	master_sites		perl_cpan:${perl5.cpandir}
-	distname			${perl5.module}-${vers}
-	dist_subdir			perl5
-
-	depends_build		path:${perl5.bin}:perl5.8
-
-	configure.cmd		${perl5.bin}
-	configure.pre_args	Makefile.PL
-	configure.args		INSTALLDIRS=vendor
-
-	test.run			yes
-
-	destroot.target		pure_install
-
-	post-destroot {
-		foreach packlist [exec find ${destroot}${perl5.lib} -name .packlist] {
-			ui_info "Fixing packlist ${packlist}"
-			reinplace "s|${destroot}||" ${packlist}
-		}
-	}
+    }
 }
