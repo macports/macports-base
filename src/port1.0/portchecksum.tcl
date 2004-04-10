88c1c96303adcc0bbd6e4cf2360a9b144a14c9fa
Toby Peterson <toby@macports.org>
2004-04-10 05:10:14 +0000

    Prettify code (*consistent* indentation). Add some useful comments. Update copyright to 2004. Use [string equal] where appropriate. Use [option] where appropriate. Added verbosity (with -v, shows each file as it is checksummed). Displays actual checksum if checksum is missing/incorrect. If checksums option isn't set, uses ui_info now (instead of ui_msg). Goes through all checksums even if one checksum fails, fails at end.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/base@6331 d073be05-634f-4543-b044-5fe20cf6d1d6

--- src/port1.0/portchecksum.tcl
+++ src/port1.0/portchecksum.tcl
@@ -1,7 +1,7 @@
 # et:ts=4
 # portchecksum.tcl
 #
-# Copyright (c) 2002 - 2003 Apple Computer, Inc.
+# Copyright (c) 2002 - 2004 Apple Computer, Inc.
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -37,55 +37,80 @@ target_provides ${com.apple.checksum} checksum
 target_requires ${com.apple.checksum} main fetch
 target_prerun ${com.apple.checksum} checksum_start
 
-# define options
 options checksums
 
 set_ui_prefix
 
+# dmd5
+#
+# Returns the expected checksum for the given file.
+# If no checksum is found, returns -1.
+#
 proc dmd5 {file} {
-    foreach {name type sum} [option checksums] {
-	if {$name == $file} {
-	    return $sum
+	foreach {name type sum} [option checksums] {
+		if {[string equal $name $file]} {
+			return $sum
+		}
 	}
-    }
-    return -1
+
+	return -1
 }
 
+# checksum_start
+#
+# Target prerun procedure; simply prints a message about what we're doing.
+#
 proc checksum_start {args} {
-    global UI_PREFIX portname
-    
-    ui_msg "$UI_PREFIX [format [msgcat::mc "Verifying checksum for %s"] $portname]"
+	global UI_PREFIX
+
+	ui_msg "$UI_PREFIX [format [msgcat::mc "Verifying checksum(s) for %s"] [option portname]]"
 }
 
+# checksum_main
+#
+# Target main procedure. Verifies the checksums of all distfiles.
+#
 proc checksum_main {args} {
-    global distpath all_dist_files UI_PREFIX
-    
-    # If no files have been downloaded there is nothing to checksum
-    if {![info exists all_dist_files]} {
-	return 0
-    }
-    
-    if {![exists checksums]} {
-	ui_error "[msgcat::mc "No checksums statement in Portfile.  File checksums are:"]"
+	global UI_PREFIX all_dist_files
+
+	# If no files have been downloaded, there is nothing to checksum.
+	if {![info exists all_dist_files]} {
+		return 0
+	}
+
+	# Optimization for the two-argument case for checksums.
+	if {[llength [option checksums]] == 2 && [llength $all_dist_files] == 1} {
+		option checksums [linsert [option checksums] 0 $all_dist_files]
+	}
+
+	set fail no
+
 	foreach distfile $all_dist_files {
-	    ui_msg "$distfile md5 [md5 file $distpath/$distfile]"
+		ui_info "$UI_PREFIX [format [msgcat::mc "Checksumming %s"] $distfile]"
+
+		# Calculate the distfile's checksum.
+		set checksum [md5 file [file join [option distpath] $distfile]]
+
+		# Find the expected checksum.
+		set dchecksum [dmd5 $distfile]
+
+		# Check for missing checksum or a mismatch.
+		if {$dchecksum == -1} {
+			ui_info "[format [msgcat::mc "No checksum set for %s"] $distfile]"
+		} elseif {![string equal $checksum $dchecksum]} {
+			ui_info "[format [msgcat::mc "Checksum mismatch for %s"] $distfile]"
+		} else {
+			continue
+		}
+
+		# Print out the actual checksum, and raise the failure flag.
+		ui_info "[format [msgcat::mc "File checksum is: %s"] $checksum]"
+		set fail yes
 	}
-	return -code error "[msgcat::mc "No checksums statement in Portfile."]"
-    }
-    
-    # Optimization for the 2 argument case for checksums
-    if {[llength [option checksums]] == 2 && [llength $all_dist_files] == 1} {
-	option checksums [linsert [option checksums] 0 $all_dist_files]
-    }
-    
-    foreach distfile $all_dist_files {
-	set checksum [md5 file $distpath/$distfile]
-	set dchecksum [dmd5 $distfile]
-	if {$dchecksum == -1} {
-	    ui_warn "[format [msgcat::mc "No checksum recorded for %s"] $distfile]"
-	} elseif {$checksum != $dchecksum} {
-	    return -code error "[format [msgcat::mc "Checksum mismatch for %s"] $distfile]"
+
+	if {[tbool fail]} {
+		return -code error "[msgcat::mc "Checksum failure"]"
 	}
-    }
-    return 0
+
+	return 0
 }
