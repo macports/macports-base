e1a797cb14de4661c2f68a19af346bccfd01381e
Jordan K. Hubbard <jkh@macports.org>
2004-04-11 08:30:56 +0000

    Choose a non-conflicting name for what this provides
    git-svn-id: https://svn.macports.org/repository/macports/trunk/base@6388 d073be05-634f-4543-b044-5fe20cf6d1d6

--- src/package1.0/package.tcl
+++ src/package1.0/package.tcl
@@ -29,7 +29,7 @@
 # POSSIBILITY OF SUCH DAMAGE.
 #
 # standard package load
-package provide pkg 1.0
+package provide dp_package 1.0
 
 package require portrpmpackage 1.0
 package require portpkg 1.0
