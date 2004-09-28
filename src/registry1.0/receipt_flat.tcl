de6c1d5adcf88053c4f1cc607a86e1aa7dd570f8
Will Barton <wbb4@macports.org>
2004-09-28 15:48:41 +0000

    Fix a bug in the way a file is being given to the filemap in register_bulk_files.  This would only affect direct-mode users, but would prevent any file tracking after a port was installed, because the file entry in the filemap would contain all of the file information (MD5, etc), as stored in the receipt, rather than just the filename.  Thanks to rshaw@ for catching this.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/base@8481 d073be05-634f-4543-b044-5fe20cf6d1d6

--- src/registry1.0/receipt_flat.tcl
+++ src/registry1.0/receipt_flat.tcl
@@ -1,5 +1,5 @@
 # receipt_flat.tcl
-# $Id: receipt_flat.tcl,v 1.4 2004/09/28 15:27:47 rshaw Exp $
+# $Id: receipt_flat.tcl,v 1.5 2004/09/28 15:48:41 wbb4 Exp $
 #
 # Copyright (c) 2004 Will Barton <wbb4@opendarwin.org>
 # Copyright (c) 2004 Paul Guyot, DarwinPorts Team.
@@ -613,12 +613,13 @@ proc register_bulk_files {files port} {
 		open_file_map
 	}
 
-	foreach file $files {
-		#if { [file type $file] == "link" } {
-		#	ui_debug "Adding link to file_map: $file for: $port"
-		#} else {
-		#	ui_debug "Adding file to file_map: $file for: $port"
-		#}
+	foreach f $files {
+		set file [lindex $f 0]
+		if { [file type $file] == "link" } {
+			ui_debug "Adding link to file_map: $file for: $port"
+		} else {
+			ui_debug "Adding file to file_map: $file for: $port"
+		}
 		filemap set file_map $file $port
 	}
 }
