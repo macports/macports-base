7628093b37a56522646372befa0dda13e3aba268
Robert Shaw <rshaw@macports.org>
2004-10-15 22:48:17 +0000

    Merge of receipt fix from HEAD:
    Fixed bug where port registry parent directory was not being removed when
    all versions of a port had been uninstalled (thus was left empty).
    
    Submitted by:   Juan
    
    git-svn-id: https://svn.macports.org/repository/macports/branches/rshaw-archivemode/base@8780 d073be05-634f-4543-b044-5fe20cf6d1d6

--- src/registry1.0/receipt_flat.tcl
+++ src/registry1.0/receipt_flat.tcl
@@ -1,5 +1,5 @@
 # receipt_flat.tcl
-# $Id: receipt_flat.tcl,v 1.2.2.3 2004/09/28 16:00:10 rshaw Exp $
+# $Id: receipt_flat.tcl,v 1.2.2.4 2004/10/15 22:48:17 rshaw Exp $
 #
 # Copyright (c) 2004 Will Barton <wbb4@opendarwin.org>
 # Copyright (c) 2004 Paul Guyot, DarwinPorts Team.
@@ -389,7 +389,20 @@ proc delete_entry {name version {revision 0} {variants ""}} {
 
 	set receipt_path [file join ${darwinports::registry.path} receipts ${name} ${version}_${revision}${variants}]
 	if { [file exists ${receipt_path}] } {
-		system "rm -rf ${receipt_path}"
+		# remove port receipt directory
+		ui_debug "deleting directory: ${receipt_path}"
+		file delete -force ${receipt_path}
+		# remove port receipt parent directory (if empty)
+		set receipt_dir [file join ${darwinports::registry.path} receipts ${name}]
+		if { [file isdirectory ${receipt_dir}] } {
+			# 0 item means empty.
+			if { [llength [readdir ${receipt_dir}]] == 0 } {
+				ui_debug "deleting directory: ${receipt_dir}"
+				file delete -force ${receipt_dir}
+			} else {
+				ui_debug "${receipt_dir} is not empty"
+			}
+		}
 		return 1
 	} else {
 		return 0
