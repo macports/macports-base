b233996fbd3dc6503480209e595cd80b79370a67
Landon Fuller <landonf@macports.org>
2004-12-17 06:34:15 +0000

    Fix up error logging Refactor duplicate code into functions Fix up-to-date package detection
    git-svn-id: https://svn.macports.org/repository/macports/trunk/base@9819 d073be05-634f-4543-b044-5fe20cf6d1d6

--- src/portmgr/dpkgall.tcl
+++ src/portmgr/dpkgall.tcl
@@ -1,4 +1,4 @@
-#!/usr/bin/env tclsh
+#!/usr/bin/env tclsh8.4
 # dpkgbuild.tcl
 #
 # Copyright (c) 2004 Landon Fuller <landonf@opendarwin.org>
@@ -99,7 +99,17 @@ proc ui_silent {message} {
 	global dpkg::silentmode
 	if {"${silentmode}" != true} {
 		puts $message
+		ui_msg $message
+	} else {
+		ui_msg $message
 	}
+}
+
+# Outputs message to console and to log
+# Should only be used with errors
+proc ui_noisy_error {message} {
+	puts $message
+	ui_error $message
 }
 
 proc log_message {channel message} {
@@ -144,41 +154,35 @@ proc print_usage {args} {
 proc reset_tree {args} {
 	global dpkg::portprefix dpkg::pkgrepo dpkg::architecture
 
-	ui_silent "Deleting ${portprefix} ..."
-
-	if {[catch {system "rm -Rf ${portprefix}"} error]} {
-		puts stderr "Internal error: $error"
-		exit 1
-	}
-
-	ui_silent "Deleting /usr/X11R6 ..."
-	if {[catch {system "rm -Rf /usr/X11R6"} error]} {
-		puts stderr "Internal error: $error"
-		exit 1
-	}
-
-	ui_silent "Deleting /etc/X11 ..."
-	if {[catch {system "rm -Rf /etc/X11"} error]} {
-		puts stderr "Internal error: $error"
-		exit 1
-	}
-
-	ui_silent "Deletion complete."
-
 	ui_silent "Restoring pristine ${portprefix} from ${pkgrepo}/${architecture}/root.tar.gz"
+	if {[catch {system "rm -Rf ${portprefix}"} error]} {
+		ui_noisy_error "Internal error: $error"
+		exit 1
+	}
+
+	if {[catch {system "rm -Rf /usr/X11R6"} error]} {
+		ui_noisy_error "Internal error: $error"
+		exit 1
+	}
+
+	if {[catch {system "rm -Rf /etc/X11"} error]} {
+		ui_noisy_error "Internal error: $error"
+		exit 1
+	}
+
 	if {[catch {system "cd / && tar xvf ${pkgrepo}/${architecture}/root.tar.gz"} error]} {
-		puts stderr "Internal error: $error"
+		ui_noisy_error "Internal error: $error"
 		exit 1
 	}
 
 	ui_silent "Linking static distfiles directory to ${portprefix}/var/db/dports/distfiles."
 	if {[catch {system "rmdir ${portprefix}/var/db/dports/distfiles"} error]} {
-		puts stderr "Internal error: $error"
+		ui_noisy_error "Internal error: $error"
 		exit 1
 	}
 
 	if {[catch {system "ln -s ${pkgrepo}/distfiles ${portprefix}/var/db/dports/distfiles"} error]} {
-		puts stderr "Internal error: $error"
+		ui_noisy_error "Internal error: $error"
 		exit 1
 	}
 }
@@ -252,7 +256,8 @@ proc main {argc argv} {
 			set fail false
 
 			if {[llength $res] < 2} {
-				puts "Port \"$port\" not found in index"
+				puts 
+				ui_noisy_error "Port \"$port\" not found in index"
 				set fail true
 			}
 		}
@@ -263,14 +268,18 @@ proc main {argc argv} {
 
 	# Ensure that the log directory exists, and open up
 	# the default debug log
-	file mkdir ${pkgrepo}/log/
-	set logfd [open ${pkgrepo}/log/debug.log w 0644]
+	open_default_log
 
 	# Set the dport options
 	# Package build path
-	set options(package.destpath) ${pkgrepo}/${architecture}/Packages/
+	set options(package.destpath) ${pkgrepo}/${architecture}/packages/
+
+	# Ensure that it exists
+	file mkdir $options(package.destpath)
+
 	# Force mode
 	set options(ports_force) yes
+
 	# Noisy output
 	set options(ports_verbose) yes
 	set options(ports_debug) yes
@@ -284,31 +293,30 @@ proc main {argc argv} {
 		exec sleep 10
 	}
 
-	close $logfd
+	close_default_log
 
 	foreach port $portlist {
 		# Open the default debug log write/append
-		set logfd [open ${pkgrepo}/log/debug.log w+ 0644]
-		reset_tree
-
+		open_default_log w+
 
 		set searchstring [escape_portname $port]
 		if {[catch {set res [dportsearch "^${searchstring}\$"]} error]} {
-			puts "Internal error: port search failed: $error"
+			ui_noisy_error "Internal error: port search failed: $error"
 			exit 1
 		}
+
+		# Reset array from previous runs
+		unset -nocomplain portinfo
 		array set portinfo [lindex $res 1]
 
 		if {![info exists portinfo(name)] ||
 			![info exists portinfo(version)] || 
 			![info exists portinfo(revision)] || 
 			![info exists portinfo(categories)]} {
-			puts "Internal error: $name missing some portinfo keys"
+			ui_noisy_error "Internal error: $name missing some portinfo keys"
 			close $logfd
 			continue
 		}
-
-		close $logfd
 
 		# Skip up-to-date software
 		set pkgfile [get_pkgpath $portinfo(name) $portinfo(version) $portinfo(revision)]
@@ -321,12 +329,17 @@ proc main {argc argv} {
 			}
 		}
 
+		# We're going to actually build the package, reset the tree
+		reset_tree
 
 		ui_silent "Building $portinfo(name) ..."
 
+		# Close the main debug log
+		close_default_log
+
 		# Create log directory and open the build log
-		file mkdir [file join ${pkgrepo} log ${port}]
-		set logfd [open ${pkgrepo}/log/${port}/build.log w 0644]
+		file mkdir [file join ${pkgrepo} ${architecture} log build ${port}]
+		set logfd [open ${pkgrepo}/${architecture}/log/build/${port}/build.log w 0644]
 
 		# Install binary dependencies if possible
 		set dependencies [get_dependencies $portinfo(name)]
@@ -335,47 +348,108 @@ proc main {argc argv} {
 		}
 
 		if {[catch {set workername [dportopen $portinfo(porturl) [array get options] [array get variations] yes]} result] || $result == 1} {
-			puts "Internal error: unable to open port: $result"
+			ui_noisy_error "Internal error: unable to open port: $result"
+			exit 1
+		}
+
+		if {[catch {set result [dportexec $workername clean]} result] || $result == 1} {
+			ui_noisy_error "Cleaning $portinfo(name) failed, consult build log"
+
+			# Close the log
+			close $logfd
+
+			# Copy the log to the failure directory
+			copy_failure_log $portinfo(name)
+
+			# Close the port
+			dportclose $workername
+
+			continue
+		}
+
+		# Re-open the port. DarwinPorts doesn't play well with multiple targets, apparently
+		dportclose $workername
+		if {[catch {set workername [dportopen $portinfo(porturl) [array get options] [array get variations] yes]} result] || $result == 1} {
+			ui_noisy_error "Internal error: unable to open port: $result"
 			exit 1
 		}
 
 		if {[catch {set result [dportexec $workername dpkg]} result] || $result == 1} {
-			puts "port package failed: $result"
+			ui_noisy_error "Packaging $portinfo(name) failed, consult build log"
+
+			# Close the log
+			close $logfd
+
+			# Copy the log to the failure directory
+			copy_failure_log $portinfo(name)
+
+			# Close the port
 			dportclose $workername
 
-			# Close the log
-			close $logfd
-
-			# Move the log to the failure log directory
-			file mkdir ${pkgrepo}/failure-logs/${portinfo(name)}
-			file copy -force ${pkgrepo}/log/${port}/build.log ${pkgrepo}/failure-logs/${portinfo(name)}/
-		} else {
-			ui_silent "Package build for $portinfo(name) succeeded"
-
-			# Close the log
-			close $logfd
-
-			# Delete any previous failure logs
-			if {[catch {system "rm -Rf ${pkgrepo}/failure-logs/${portinfo(name)}"} error]} {
-				puts "Internal error: $error"
-				exit 1
-			}
+			continue
 		}
+
+		ui_silent "Package build for $portinfo(name) succeeded"
+
+		# Close the log
+		close $logfd
+
+		# Delete any previous failure logs
+		delete_failure_log $portinfo(name)
 
 		# Close the port
 		dportclose $workername
 	}
 
+	open_default_log w+
+
 	ui_silent "Resetting /usr/dports ..."
+	reset_tree
 	ui_silent "Done."
 
 	ui_silent "Package run finished."
+	close_default_log
+
 	exit 0
 }
 
 proc get_pkgpath {name version revision} {
 	global dpkg::pkgrepo dpkg::architecture
-	return ${pkgrepo}/${architecture}/Packages/${name}_${version}-${revision}.deb
+	if {${revision} == 0} {
+		set revision ""
+	} else {
+		set revision "-${revision}"
+	}
+
+	return ${pkgrepo}/${architecture}/packages/${name}_${version}${revision}_${architecture}.deb
+}
+
+proc open_default_log {{mode w}} {
+	global dpkg::pkgrepo dpkg::architecture dpkg::logfd
+	# Ensure that the log directory exists, and open up
+	# the default debug log
+	file mkdir ${pkgrepo}/${architecture}/log/
+	set logfd [open ${pkgrepo}/${architecture}/log/debug.log ${mode} 0644]
+}
+
+proc close_default_log {args} {
+	global dpkg::logfd
+	close $logfd
+}
+
+proc copy_failure_log {name} {
+	global dpkg::pkgrepo dpkg::architecture
+	# Copy the log to the failure log directory
+	file mkdir ${pkgrepo}/${architecture}/log/failure/${name}
+	file copy -force ${pkgrepo}/${architecture}/log/build/${name}/build.log ${pkgrepo}/${architecture}/log/failure/${name}/
+}
+
+proc delete_failure_log {name} {
+	global dpkg::pkgrepo dpkg::architecture
+	if {[catch {system "rm -Rf ${pkgrepo}/${architecture}/log/failure/${name}"} error]} {
+		ui_noisy_error "Internal error: $error"
+		exit 1
+	}
 }
 
 # Recursive bottom-up approach of building a list of dependencies.
@@ -443,13 +517,13 @@ proc install_binary_if_available {dep} {
 	if {[file readable $pkgpath] && ![file exists $receiptdir/receipt.bz2]} {
 		ui_silent "Installing binary: $pkgpath"
 		if {[catch {system "dpkg --force-depends -i ${pkgpath}"} error]} {
-			puts "Internal error: $error"
+			ui_noisy_error "Internal error: $error"
 			exit 1
 		}
 		# Touch the receipt
 		file mkdir $receiptdir
 		if {[catch {system "touch [file join $receiptdir receipt.bz2]"} error]} {
-			puts "Internal error: $error"
+			ui_noisy_error "Internal error: $error"
 			exit 1
 		}
 	}
