7155d82e95c45b3d27ec91db7832968379cf42ea
Jordan K. Hubbard <jkh@macports.org>
2004-12-19 04:48:37 +0000

    Give up on successive overlays to replace the shadow image.  It's just too failure-prone and complex, and I'll need to work around the issues with volfs some other way (like whining at the filesystems team).
    git-svn-id: https://svn.macports.org/repository/macports/trunk/base@9848 d073be05-634f-4543-b044-5fe20cf6d1d6

--- src/portmgr/buildall.sh
+++ src/portmgr/buildall.sh
@@ -7,63 +7,72 @@
 # If you want to see the main body of this script, go to the end of the file.
 
 # What we want to call the base chroot images
-CHROOTBASE=chrootbase.dmg
+CHROOTBASE=chrootbase.sparseimage
 DISTFILES=distfiles.dmg
+FSTYPE=HFSX
 
-# Mount all the important bits a chroot build env needs.
-mountchrootextras() {
-	dir=$1
-	/sbin/mount_devfs devfs ${dir}/dev
-	/sbin/mount_fdesc -o union fdesc ${dir}/dev
-}
+# Some conservative (and large) defaults.
+BASE_PADDING=2000000
+DISTFILES_SIZE=8192M
 
-# Undo the work of mountchrootextras
-umountchrootextras() {
-	dir=$1
-	umount -f ${dir}/dev
-	umount -f ${dir}/dev
+# deal with fatal errors
+bomb() {
+	echo "Fatal error: $*"
+	echo "BASEDEV=${BASEDEV} DISTDEV=${DISTDEV}"
+	exit 1
 }
 
 # Everything we need to create the base chroot disk image (populated from host)
-mkchrootimage() {
-	if [ $# -lt 1 ]; then
-		echo "Usage: mkchrootimage chrootdir"
-		return 1
-	fi
-
-	dir=$1
-	mkdir -p $dir
-	DEV=""
-
-	# Add to this list as you find minimum dependencies DP really needs.
-	chrootfiles="bin sbin etc tmp var dev/null usr private Developer System/Library Library/Java"
-
-	echo "Calculating chroot image size..."
-	# start with this size to account for other overhead
-	sz=2000000
-	for i in $chrootfiles; do
-		mysz=`cd /; du -sk $i |awk '{print $1}'`
-		sz=$(($sz + $mysz))
-	done
-	echo "Creating bootstrap disk image of ${sz}K bytes"
-	hdiutil create -size ${sz}k -fs HFSX ${CHROOTBASE} > /dev/null
-	DEV=`hdiutil attach ${CHROOTBASE} -mountpoint $dir 2>&1 | awk '/dev/ {if (x == 0) {print $1; x = 1}}'`
-	echo "Image attached as $DEV"
-	echo "Copying chroot files into bootstrap disk image"
-	for i in $chrootfiles; do
-		pax -pe -rw /$i $dir 2>/dev/null
-		# special case for pax
-		cp /bin/pax $dir/bin/pax
-	done
-	if [ -f darwinports.tar.gz ]; then
-		echo "Found darwinports.tar.gz - copying into chroot"
-		tar -xpzf darwinports.tar.gz -C $dir
-	elif [ -d darwinports ]; then
-		pax -rw darwinports $dir
+mkchrootbase() {
+	if [ -f ${CHROOTBASE} ]; then
+		echo "Using existing ${CHROOTBASE} for efficiency"
 	else
-		echo "no darwinports.tar.gz or darwinports directory found - please fix this and try again."
-		return 1
+		dir=$1
+		mkdir -p $dir
+
+		# Add to this list as you find minimum dependencies DP really needs.
+		chrootfiles="bin sbin etc tmp var private dev/null usr Developer System/Library Library/Java"
+
+		echo "Calculating chroot base image size..."
+		# start with this size to account for other overhead
+		sz=${BASE_PADDING}
+		for i in $chrootfiles; do
+			mysz=`cd /; du -sk $i |awk '{print $1}'`
+			sz=$(($sz + $mysz))
+		done
+		echo "Creating bootstrap disk image of ${sz}K bytes"
+		hdiutil create -size ${sz}k -fs ${FSTYPE} -volname base ${CHROOTBASE} > /dev/null
+		BASEDEV=`hdiutil attach ${CHROOTBASE} -mountpoint $dir 2>&1 | awk '/dev/ {if (x == 0) {print $1; x = 1}}'`
+		echo "Image attached as $BASEDEV"
+		echo "Copying chroot files into bootstrap disk image"
+		for i in $chrootfiles; do
+			pax -pe -rw /$i $dir 2>/dev/null
+			# special case for pax
+			cp /bin/pax $dir/bin/pax
+		done
+		# special case nuke to prevent builder pollution
+		rm -rf $dir/usr/X11R6
+		if [ -f darwinports.tar.gz ]; then
+			echo "Found darwinports.tar.gz - copying into chroot"
+			tar -xpzf darwinports.tar.gz -C $dir
+		elif [ -d darwinports ]; then
+			pax -rw darwinports $dir
+		else
+			echo "no darwinports.tar.gz or darwinports directory found - please fix this and try again."
+			exit 1
+		fi
+		bootstrapdports $dir
 	fi
+	if [ -f ${DISTFILES} ]; then
+		echo "Using existing ${DISTFILES} for efficiency"
+	else
+		echo "Creating distfiles cache of size ${DISTFILES_SIZE}"
+		hdiutil create -size ${DISTFILES_SIZE} -fs ${FSTYPE} -volname distfiles ${DISTFILES} > /dev/null
+	fi
+}
+
+bootstrapdports() {
+	dir=$1
 	cat > $dir/bootstrap.sh << EOF
 #!/bin/sh
 cd darwinports/base
@@ -77,30 +86,34 @@ EOF
 	fi
 	chmod 755 $dir/bootstrap.sh
 	echo "Bootstrapping darwinports in chroot"
-	mountchrootextras $dir
+	/sbin/mount_devfs devfs ${dir}/dev
+	/sbin/mount_fdesc -o union fdesc ${dir}/dev
 	chroot $dir /bootstrap.sh && rm $dir/bootstrap.sh
-	umountchrootextras $dir
-	[ -z "$DEV" ] || hdiutil detach $DEV >& /dev/null
-	DEV=""
+	umount ${dir}/dev
+	umount ${dir}/dev
+	hdiutil detach $BASEDEV >& /dev/null && BASEDEV=""
 }
 
-# Do whatever needs to be done to prep the chroot for actual package building
+# Set up the base chroot image
 prepchroot() {
 	dir=$1
-	DEV=""; DISTDEV=""
-	DEV=`hdiutil attach ${CHROOTBASE} -mountpoint $dir -readonly -shadow 2>&1 | awk '/dev/ {if (x == 0) {print $1; x = 1}}'`
-	if [ -f $DISTFILES ]; then
-		DISTDEV=`hdiutil attach ${DISTFILES} -mountpoint $dir/opt/local/var/db/dports/distfiles -union 2>&1 | awk '/dev/ {if (x == 0) {print $1; x = 1}}'`
-	fi
+	BASEDEV=""; DISTDEV=""
+	rm -f ${CHROOTBASE}.shadow
+	BASEDEV=`hdiutil attach ${CHROOTBASE} -mountpoint $dir -readonly -shadow 2>&1 | awk '/dev/ {if (x == 0) {print $1; x = 1}}'`
+ 	DISTDEV=`hdiutil attach ${DISTFILES} -mountpoint $dir/opt/local/var/db/dports/distfiles -union 2>&1 | awk '/dev/ {if (x == 0) {print $1; x = 1}}'`
+	mkdir -p $dir/.vol
+	/sbin/mount_devfs devfs $dir/dev || bomb "unable to mount devfs"
+	/sbin/mount_fdesc -o union fdesc $dir/dev || bomb "unable to mount fdesc"
 }
 
 # Undo the work of prepchroot
 teardownchroot() {
 	dir=$1
-	[ -z "$DISTDEV" ] || hdiutil detach $DISTDEV >& /dev/null
-	[ -z "$DEV" ] || hdiutil detach $DEV >& /dev/null
-	rm -f ${CHROOTBASE}.shadow
-	DEV=""; DISTDEV=""
+	umount $dir/dev  || bomb "unable to umount devfs"
+	umount $dir/dev  || bomb "unable to umount fdesc"
+	[ -z "$DISTDEV" ] || (hdiutil detach $DISTDEV >& /dev/null || bomb "unable to detach DISTDEV")
+	[ -z "$BASEDEV" ] || (hdiutil detach $BASEDEV >& /dev/null || bomb "unable to detach BASEDEV")
+	DISTDEV=""; BASEDEV=""
 }
 
 # main:  This is where we start the show.
@@ -125,6 +138,7 @@ else
 	fi
 fi
 
+rm -rf outputdir
 if [ -z "$TGTPORTS" ]; then
 	if [ -f PortIndex ]; then
 		PINDEX=PortIndex
@@ -135,39 +149,28 @@ if [ -z "$TGTPORTS" ]; then
 		echo "current directory or unpack a darwinports distribution to get it from"
 		exit 1
 	fi
-	rm -rf outputdir
 	mkdir -p outputdir/summary
 	TGTPORTS=outputdir/summary/portsrun
 	awk 'NF == 2 {print $1}' $PINDEX > $TGTPORTS
 fi
 
-if [ -f $CHROOTBASE ]; then
-	echo "Using existing $CHROOTBASE file for efficiency"
-else
-	mkchrootimage $DIR
-fi
-
-mkdir -p outputdir/Packages outputdir/logs/succeeded outputdir/logs/failed
+mkchrootbase $DIR
+mkdir -p outputdir/Packages outputdir/logs/succeeded outputdir/logs/failed outputdir/tmp
 
 echo "Starting packaging run for `wc -l $TGTPORTS | awk '{print $1}'` ports."
 for pkg in `cat $TGTPORTS`; do
-	echo "Starting packaging run for $pkg"
 	prepchroot $DIR
+	echo "Starting packaging run for $pkg"
 	echo "#!/bin/sh" > $DIR/bootstrap.sh
 	echo 'export PATH=$PATH:/opt/local/bin' >> $DIR/bootstrap.sh
-	echo "mkdir -p /.vol" >> $DIR/bootstrap.sh
-	echo "/sbin/mount_volfs /.vol" >> $DIR/bootstrap.sh
-	echo "/sbin/mount_devfs devfs /dev" >> $DIR/bootstrap.sh
-        echo "/sbin/mount_fdesc -o union fdesc /dev" >> $DIR/bootstrap.sh
+	echo '/sbin/mount_volfs /.vol || (echo "unable to mount volfs"; exit 1)' >> $DIR/bootstrap.sh
 	echo "mkdir -p /Package" >> $DIR/bootstrap.sh
 	echo "rm -f /tmp/success" >> $DIR/bootstrap.sh
 	echo "if port -v $PKGTYPE $pkg package.destpath=/Package >& /tmp/$pkg.log; then touch /tmp/success; fi" >> $DIR/bootstrap.sh
-	echo "umount /.vol" >> $DIR/bootstrap.sh
-	echo "umount /dev" >> $DIR/bootstrap.sh
-	echo "umount -f /dev" >> $DIR/bootstrap.sh
+	echo 'umount /.vol || (echo "unable to umount volfs"; exit 1)' >> $DIR/bootstrap.sh
 	echo "exit 0" >> $DIR/bootstrap.sh
 	chmod 755 $DIR/bootstrap.sh
-	chroot $DIR /bootstrap.sh
+	chroot $DIR /bootstrap.sh || bomb "bootstrap script in chroot returned failure status"
 	if [ ! -f $DIR/tmp/success ]; then
 		echo $pkg >> outputdir/summary/portsfailed
 		type="failed"
@@ -184,3 +187,5 @@ for pkg in `cat $TGTPORTS`; do
 	teardownchroot $DIR
 	echo "Finished packaging run for $pkg ($type)"
 done
+echo "Packaging run complete."
+exit 0
