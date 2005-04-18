514089f53ade8a4a225d95f425589759605232b5
Ole Guldberg Jensen <olegb@macports.org>
2005-04-18 13:56:17 +0000

    Bug:            https://trac.macports.org/ticket/2905 Submitted by:   dluke Reviewed by:    jberry, olegb Approved by:    jberry, olegb Obtained from:
    merging the no-same-owner fix by dluke to release1
    
    git-svn-id: https://svn.macports.org/repository/macports/branches/release1/base@11850 d073be05-634f-4543-b044-5fe20cf6d1d6

--- src/port1.0/portextract.tcl
+++ src/port1.0/portextract.tcl
@@ -50,7 +50,7 @@ default extract.only {[disttagclean $distfiles]}
 default extract.dir {${workpath}}
 default extract.cmd gzip
 default extract.pre_args -dc
-default extract.post_args {{| tar --no-same-owner -xf -}}
+default extract.post_args {"| ${portutil::autoconf::tar_command} -xf -"}
 
 set_ui_prefix
 
