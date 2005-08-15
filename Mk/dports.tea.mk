35c9597be41f687328719794d5d850c2604346ea
Juan Manuel Palacios <jmpp@macports.org>
2005-08-15 18:06:58 +0000

    Bug: Submitted by:   pguyot@ Reviewed by:    jmpp@
    Test target is no longer mandatory before installation.
    
    git-svn-id: https://svn.macports.org/repository/macports/branches/release1/base@13551 d073be05-634f-4543-b044-5fe20cf6d1d6

--- Mk/dports.tea.mk
+++ Mk/dports.tea.mk
@@ -11,7 +11,7 @@ clean::
 
 distclean:: clean
 
-install:: all test
+install:: all
 	$(INSTALL) -d -o ${DSTUSR} -g ${DSTGRP} -m ${DSTMODE} ${INSTALLDIR}
 	$(INSTALL) -o ${DSTUSR} -g ${DSTGRP} -m 444 ${SHLIB_NAME} ${INSTALLDIR}
 	$(SILENT) $(TCLSH) ../pkg_mkindex.tcl ${INSTALLDIR}
