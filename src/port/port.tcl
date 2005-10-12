b4da382ff94fee41743c697f62f18ff35a76d608
Paul Guyot <pguyot@macports.org>
2005-10-12 13:21:16 +0000

    This actually was a workaround for a problem with saving and restoring ENV that James fixed.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/base@14578 d073be05-634f-4543-b044-5fe20cf6d1d6

--- src/port/port.tcl
+++ src/port/port.tcl
@@ -2,7 +2,7 @@
 #\
 exec @TCLSH@ "$0" "$@"
 # port.tcl
-# $Id: port.tcl,v 1.128 2005/10/11 22:42:46 jberry Exp $
+# $Id: port.tcl,v 1.129 2005/10/12 13:21:16 pguyot Exp $
 #
 # Copyright (c) 2004 Robert Shaw <rshaw@opendarwin.org>
 # Copyright (c) 2002 Apple Computer, Inc.
@@ -1758,7 +1758,6 @@ switch -- $action {
 						if { $editor == "" } {
 							fatal "No EDITOR is specified in your environment"
 						} else {
-							set editor [darwinports::binaryInPath $editor]
 							if {[catch {eval exec >/dev/stdout </dev/stdin $editor $portfile} result]} {
 								global errorInfo
 								ui_debug "$errorInfo"
