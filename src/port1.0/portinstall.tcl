87a907ccd59e3c72921358a4cd3e37feb3763548
Ole Guldberg Jensen <olegb@macports.org>
2006-01-08 22:31:31 +0000

    wrap install in rpm
    git-svn-id: https://svn.macports.org/repository/macports/branches/dp_light-olegb/base@15765 d073be05-634f-4543-b044-5fe20cf6d1d6

--- src/port1.0/portinstall.tcl
+++ src/port1.0/portinstall.tcl
@@ -1,6 +1,6 @@
 # et:ts=4
 # portinstall.tcl
-# $Id: portinstall.tcl,v 1.78.6.1 2006/01/08 18:36:42 olegb Exp $
+# $Id: portinstall.tcl,v 1.78.6.2 2006/01/08 22:31:31 olegb Exp $
 #
 # Copyright (c) 2002 - 2003 Apple Computer, Inc.
 # Copyright (c) 2004 Robert Shaw <rshaw@opendarwin.org>
@@ -37,168 +37,18 @@ package require portutil 1.0
 set com.apple.install [target_new com.apple.install install_main]
 target_state ${com.apple.install} no
 target_provides ${com.apple.install} install
-target_requires ${com.apple.install} main fetch extract checksum patch configure build destroot
-
-target_prerun ${com.apple.install} install_start
-
-set_ui_prefix
-
-proc install_start {args} {
-	global UI_PREFIX portname portversion portrevision variations portvariants
-    
-	if { ![info exists portvariants] } {
-		set portvariants ""
-
-		set vlist [lsort -ascii [array names variations]]
-
-		# Put together variants in the form +foo+bar for the registry
-		foreach v $vlist {
-			if { ![string equal $v [option os.platform]] && ![string equal $v [option os.arch]] } {
-				set portvariants "${portvariants}+${v}"
-			}
-		}
-	}
-
-	ui_msg "$UI_PREFIX [format [msgcat::mc "Installing %s %s_%s%s"] $portname $portversion $portrevision $portvariants]"
-}
-
-proc install_element {src_element dst_element} {
-    # don't recursively copy directories
-    if {[file isdirectory $src_element] && [file type $src_element] != "link"} {
-	file mkdir $dst_element
-    } else {
-	file copy -force $src_element $dst_element
-    }
-    
-    # if the file is a symlink, do not try to set file attributes
-    # if the destination file is an existing directory,
-    # do not overwrite its file attributes
-    if {[file type $src_element] != "link" || [file isdirectory $dst_element]} {
-	set attributes [file attributes $src_element]	
-	for {set i 0} {$i < [llength $attributes]} {incr i} {
-	    set opt [lindex $attributes $i]
-	    incr i
-	    set arg [lindex $attributes $i]
-	    file attributes $dst_element $opt $arg
-	    # set mtime on installed element
-	    exec touch -r $src_element $dst_element
-	}
-    }
-}
-
-proc directory_dig {rootdir workdir regref {cwd ""}} {
-    global installPlist
-    set pwd [pwd]
-    if {[catch {cd $workdir} err]} {
-	puts $err
-	return
-    }
-    
-    foreach name [readdir .] {
-	set element [file join $cwd $name]
-	
-	# XXX jpm's cross-platform code to find file separator
-	# replace with [file seperator] with tcl 8.4
-	if {![info exists root]} {
-	    if {[string match [file tail "/monkey"] "monkey"]} {
-		set root "/"
-	    } elseif {[string match [file tail ":monkey"] "monkey"]} {
-		set root ":" 
-	    } else {
-		set root "\\"		
-	    }
-	}
-	
-	if { [registry_prop_retr $regref installtype] == "image" } {
-		set imagedir [registry_prop_retr $regref imagedir]
-		set root [file join $root $imagedir]
-	}
-	
-	set dst_element [file join $root $element]
-	set src_element [file join $rootdir $element]
-	# overwrites files but not directories
-	if {![file exists $dst_element] || ![file isdirectory $dst_element]} {
-		if {[file type $src_element] == "link"} {
-			ui_debug "installing link: $dst_element"
-		} elseif {[file isdirectory $src_element]} {
-			ui_debug "installing directory: $dst_element"
-		} else {
-			ui_debug "installing file: $dst_element"
-		}
-	    install_element $src_element $dst_element
-		# only track files/links for registry, not directories
-		if {[file type $dst_element] != "directory"} {
-			lappend installPlist $dst_element
-		}
-	}
-	if {[file isdirectory $name] && [file type $name] != "link"} {
-	    directory_dig $rootdir $name $regref [file join $cwd $name]
-	}
-    }
-    cd $pwd
-}
+target_requires ${com.apple.install} main fetch extract checksum patch configure build destroot rpmpackage
 
 proc install_main {args} {
-	global portname portversion portpath categories description long_description homepage depends_run installPlist package-install uninstall workdir worksrcdir pregrefix UI_PREFIX destroot portrevision maintainers ports_force portvariants targets depends_lib PortInfo epoch
+	global portname portversion portpath categories description long_description homepage depends_run installPlist package-install uninstall workdir worksrcdir pregrefix UI_PREFIX destroot portrevision maintainers ports_force portvariants targets depends_lib PortInfo epoch prefix
 
-	# Begin the registry entry
-   	set regref [registry_new $portname $portversion $portrevision $portvariants $epoch]
-    
-  	# Install the files	
-	directory_dig ${destroot} ${destroot} ${regref}
-    
-	registry_prop_store $regref categories $categories
-    
-    if {[info exists description]} {
-		registry_prop_store $regref description $description
-    }
-    if {[info exists long_description]} {
-		registry_prop_store $regref long_description ${long_description}
-    }
-    if {[info exists homepage]} {
-		registry_prop_store $regref homepage ${homepage}
-    }
-	if {[info exists maintainers]} {
-		registry_prop_store $regref maintainers ${maintainers}
-    }
-	if {[info exists depends_run]} {
-		registry_prop_store $regref depends_run $depends_run
-		registry_register_deps $depends_run $portname
-    }
-	if {[info exists depends_lib]} {
-		registry_prop_store $regref depends_lib $depends_lib
-		registry_register_deps $depends_lib $portname
+	set arch [option os.arch]
+	if {$arch eq "powerpc"} {
+		set arch "ppc"
 	}
-    if {[info exists installPlist]} {
-		registry_prop_store $regref contents [registry_fileinfo_for_index $installPlist]
-		if { [registry_prop_retr $regref installtype] != "image" } {
-			registry_bulk_register_files [registry_fileinfo_for_index $installPlist] $portname
-		}
-	}
-	if {[info exists package-install]} {
-		registry_prop_store $regref package-install ${package-install}
-    }
-    if {[info proc pkg_uninstall] == "pkg_uninstall"} {
-		registry_prop_store $regref uninstall [proc_disasm pkg_uninstall]
-    }
-	
-	registry_write $regref 
+
+	puts "arch: $arch in: ${prefix}/src/apple/RPMS"
+	system "rpm -ivh ${prefix}/src/apple/RPMS/${arch}/${portname}-${portversion}-${portrevision}.${arch}.rpm"
 
     return 0
-}
-
-proc proc_disasm {pname} {
-    set p "proc "
-    append p $pname " {"
-    set space ""
-    foreach arg [info args $pname] {
-	if {[info default $pname $arg value]} {
-	    append p "$space{" [list $arg $value] "}"
-	} else {
-	    append p $space $arg
-	}
-	set space " "
-    }
-    append p "} {" [info body $pname] "}"
-    return $p
 }
