6e426f20a59c7c4426eb55de4bd3eaddc034b513
Kevin Ballard <eridius@macports.org>
2007-04-24 07:36:53 +0000

    Add some more tests, this time about break/continue during traversal
    git-svn-id: https://svn.macports.org/repository/macports/trunk/base@24422 d073be05-634f-4543-b044-5fe20cf6d1d6

--- src/pextlib1.0/tests/fs-traverse.tcl
+++ src/pextlib1.0/tests/fs-traverse.tcl
@@ -4,7 +4,7 @@
 # tclsh fs-traverse.tcl <Pextlib name>
 
 proc main {pextlibname} {
-    global tree1 sub_tree1 tree2 tree3 tree4 errorInfo
+    global trees errorInfo
     
     load $pextlibname
     
@@ -24,35 +24,35 @@ proc main {pextlibname} {
         fs-traverse file $root {
             lappend output $file
         }
-        check_output $output $tree1
+        check_output $output $trees(1)
         
         # Test starting with a symlink
         set output [list]
         fs-traverse file $root/a/c/a {
             lappend output $file
         }
-        check_output $output $sub_tree1
+        check_output $output $trees(sub1)
         
         # Test -depth
         set output [list]
         fs-traverse -depth file $root {
             lappend output $file
         }
-        check_output $output $tree2
+        check_output $output $trees(2)
         
         # Test multiple sources
         set output [list]
         fs-traverse file [list $root/a $root/b] {
             lappend output $file
         }
-        check_output $output $tree3
+        check_output $output $trees(3)
         
         # Test multiple sources with -depth
         set output [list]
         fs-traverse -depth file [list $root/a $root/b] {
             lappend output $file
         }
-        check_output $output $tree4
+        check_output $output $trees(4)
         
         # Error raised for traversing directory that does not exist
         if {![catch {fs-traverse file $root/does_not_exist {}}]} {
@@ -70,9 +70,28 @@ proc main {pextlibname} {
             fs-traverse -depth -ignoreErrors file [list $root/a $root/c $root/b] {
                 lappend output $file
             }
-            check_output $output $tree4
+            check_output $output $trees(4)
         }]} {
             error "fs-traverse raised an error despite -ignoreErrors"
+        }
+        
+        # Test skipping parts of the tree
+        set output [list]
+        fs-traverse file $root {
+            lappend output $file
+            if {[string match */a $file]} {
+                continue
+            }
+        }
+        check_output $output $trees(5)
+        
+        # Test cutting the traversal short
+        set output [list]
+        fs-traverse file $root {
+            lappend output $file
+            if {[file type $file] eq "link"} {
+                break
+            }
         }
     } errMsg]
     set savedInfo $errorInfo
@@ -101,8 +120,8 @@ proc check_output {output tree} {
 }
 
 proc make_root {} {
-    global tree1
-    foreach {entry typelist} $tree1 {
+    global trees
+    foreach {entry typelist} $trees(1) {
         set type [lindex $typelist 0]
         set link [lindex $typelist 1]
         switch $type {
@@ -126,9 +145,11 @@ proc make_root {} {
 }
 
 proc setup_trees {root} {
-    global tree1 sub_tree1 tree2 tree3 tree4
+    global trees
     
-    set tree1 "
+    array set trees {}
+    
+    set trees(1) "
         $root           directory
         $root/a         directory
         $root/a/a       file
@@ -156,7 +177,7 @@ proc setup_trees {root} {
         $root/b/c/c     file
     "
     
-    set sub_tree1 "
+    set trees(sub1) "
         $root/a/c/a     {link ../d}
         $root/a/c/a/a   file
         $root/a/c/a/b   {link ../../b/a}
@@ -164,7 +185,7 @@ proc setup_trees {root} {
         $root/a/c/a/d   file
     "
     
-    set tree2 "
+    set trees(2) "
         $root/a/a       file
         $root/a/b       file
         $root/a/c/a     {link ../d}
@@ -192,7 +213,7 @@ proc setup_trees {root} {
         $root           directory
     "
     
-    set tree3 "
+    set trees(3) "
         $root/a         directory
         $root/a/a       file
         $root/a/b       file
@@ -219,7 +240,7 @@ proc setup_trees {root} {
         $root/b/c/c     file
     "
     
-    set tree4 "
+    set trees(4) "
         $root/a/a       file
         $root/a/b       file
         $root/a/c/a     {link ../d}
@@ -244,6 +265,27 @@ proc setup_trees {root} {
         $root/b/c/c     file
         $root/b/c       directory
         $root/b         directory
+    "
+    
+    set trees(5) "
+        $root           directory
+        $root/a         directory
+        $root/b         directory
+        $root/b/a       directory
+        $root/b/b       directory
+        $root/b/c       directory
+        $root/b/c/a     file
+        $root/b/c/b     file
+        $root/b/c/c     file
+    "
+    
+    set trees(6) "
+        $root           directory
+        $root/a         directory
+        $root/a/a       file
+        $root/a/b       file
+        $root/a/c       directory
+        $root/a/c/a     {link ../d}
     "
 }
 
