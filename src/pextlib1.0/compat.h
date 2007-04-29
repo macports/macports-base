89d34c273c6f68f066c556bef989521fe586b3ce
Juan Manuel Palacios <jmpp@macports.org>
2007-04-29 03:48:55 +0000

    Moving pextlib1.0 to the MacPorts namespace, mostly in file comments.
    git-svn-id: https://svn.macports.org/repository/macports/branches/dp2mp-move/base@24628 d073be05-634f-4543-b044-5fe20cf6d1d6

--- src/pextlib1.0/compat.h
+++ src/pextlib1.0/compat.h
@@ -2,7 +2,7 @@
  * compat.h
  * $Id$
  *
- * Copyright (c) 2004 Paul Guyot, Darwinports Team.
+ * Copyright (c) 2004 Paul Guyot, MacPorts Team.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -13,7 +13,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of Darwinports Team nor the names of its contributors
+ * 3. Neither the name of MacPorts Team nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
  * 
