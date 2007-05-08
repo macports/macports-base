930bcbcb727cd8a8fe99189b3ec111c5e6db8403
James D. Berry <jberry@macports.org>
2007-05-08 16:29:46 +0000

    Further improvements to portname validation for depspec: allow + and - for now in portname, but not /
    git-svn-id: https://svn.macports.org/repository/macports/trunk/base@24934 d073be05-634f-4543-b044-5fe20cf6d1d6

--- src/port1.0/portdepends.tcl
+++ src/port1.0/portdepends.tcl
@@ -48,8 +48,8 @@ proc validate_depends_options {option action {value ""}} {
 		set {
 			foreach depspec $value {
 				switch -regex $depspec {
-					^(lib|bin|path):([-A-Za-z0-9_/.${}^?+()|\\\\]+):([-A-Za-z./0-9_]+)$ {}
-					^(port):([-+A-Za-z./0-9_]+)$ {}
+					^(lib|bin|path):([-A-Za-z0-9_/.${}^?+()|\\\\]+):([-+._A-Za-z0-9]+)$ {}
+					^(port):([-+._A-Za-z0-9]+)$ {}
 					default { return -code error [format [msgcat::mc "invalid depspec: %s"] $depspec] }
 				}
 			}
