40fa11150cfbc97a8f1b532421eefcb448990b60
James D. Berry <jberry@macports.org>
2007-05-17 01:21:44 +0000

    merge https://trac.macports.org/changeset/24913 which I accidentally omitted from previous merge. This completes merges for 1.4.41
    git-svn-id: https://svn.macports.org/repository/macports/branches/release_1_4/base@25265 d073be05-634f-4543-b044-5fe20cf6d1d6

--- src/programs/xar/lib/script.h
+++
@@ -1,7 +0,0 @@
-#ifndef _SCRIPT_H_
-#define _SCRIPT_H_
-
-int32_t xar_script_in(xar_t x, xar_file_t f, const char *attr, void **in, size_t *inlen);
-int32_t xar_script_done(xar_t x, xar_file_t f, const char *attr);
-
-#endif /* _SCRIPT_H_ */
