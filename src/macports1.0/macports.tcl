8b17c57cccad2a577b566dd28dc968dbb9aa5bf9
Juan Manuel Palacios <jmpp@macports.org>
2007-06-13 09:48:38 +0000

    Merging Eridius' https://trac.macports.org/changeset/25980: Wrap dportsearch inner block in a try-finally clause to ensure PortIndex file is always closed even if an error occurs.
    git-svn-id: https://svn.macports.org/repository/macports/branches/dp2mp-move/base@26128 d073be05-634f-4543-b044-5fe20cf6d1d6

--- src/macports1.0/macports.tcl
+++ src/macports1.0/macports.tcl
@@ -1295,6 +1295,7 @@ proc mportsearch {pattern {case_sensitive yes} {matchstyle regexp} {field name}}
 			if {[catch {set fd [open [macports::getindex $source] r]} result]} {
 				ui_warn "Can't open index file for source: $source"
 			} else {
+			    try {
 				incr found 1
 				while {[gets $fd line] >= 0} {
 					array unset portinfo
@@ -1302,50 +1303,55 @@ proc mportsearch {pattern {case_sensitive yes} {matchstyle regexp} {field name}}
 				        set len [lindex $line 1]
 				        set line [read $fd $len]
 
-					if {$easy} {
-						set target $name
-					} else {
-						array set portinfo $line
-						if {![info exists portinfo($field)]} continue
-						set target $portinfo($field)
-					}
-					
-					switch $matchstyle {
-						exact	{ set matchres [expr 0 == ( {$case_sensitive == "yes"} ? [string compare $pattern $target] : [string compare -nocase $pattern $target] )] }
-						glob	{ set matchres [expr {$case_sensitive == "yes"} ? [string match $pattern $target] : [string match -nocase $pattern $target]] }
-						regexp	-
-						default	{ set matchres [expr {$case_sensitive == "yes"} ? [regexp -- $pattern $target] : [regexp -nocase -- $pattern $target]] }
-					}
-					
-					if {$matchres == 1} {
 						if {$easy} {
-							array set portinfo $line
-						}
-						switch -regexp -- [macports::getprotocol ${source}] {
-							{^rsync$} {
-								# Rsync files are local
-								set source_url "file://[macports::getsourcepath $source]"
-							}
-							default {
-								set source_url $source
-							}
-						}
-						if {[info exists portinfo(portarchive)]} {
-							set porturl ${source_url}/$portinfo(portarchive)
-						} elseif {[info exists portinfo(portdir)]} {
-							set porturl ${source_url}/$portinfo(portdir)
-						}
-						if {[info exists porturl]} {
-							lappend line porturl $porturl
-							ui_debug "Found port in $porturl"
+						    set target $name
 						} else {
-							ui_debug "Found port info: $line"
+							array set portinfo $line
+						    if {![info exists portinfo($field)]} continue
+						    set target $portinfo($field)
 						}
-						lappend matches $name
-						lappend matches $line
+
+				    switch $matchstyle {
+					exact { set matchres [expr 0 == ( {$case_sensitive == "yes"} ? [string compare $pattern $target] : [string compare -nocase $pattern $target] )] }
+					glob { set matchres [expr {$case_sensitive == "yes"} ? [string match $pattern $target] : [string match -nocase $pattern $target]] }
+					regexp -
+					default { set matchres [expr {$case_sensitive == "yes"} ? [regexp -- $pattern $target] : [regexp -nocase -- $pattern $target]] }
+				    }
+
+				    if {$matchres == 1} {
+					if {$easy} {
+					    array set portinfo $line
 					}
+					switch -regexp -- [macports::getprotocol ${source}] {
+					    {^rsync$} {
+						# Rsync files are local
+						set source_url "file://[macports::getsourcepath $source]"
+					    }
+					    default {
+						set source_url $source
+					    }
+					}
+					if {[info exists portinfo(portarchive)]} {
+					    set porturl ${source_url}/$portinfo(portarchive)
+					} elseif {[info exists portinfo(portdir)]} {
+					    set porturl ${source_url}/$portinfo(portdir)
+					}
+					if {[info exists porturl]} {
+					    lappend line porturl $porturl
+					    ui_debug "Found port in $porturl"
+					} else {
+					    ui_debug "Found port info: $line"
+					}
+					lappend matches $name
+					lappend matches $line
+				    }
 				}
+			    } catch {*} {
+				ui_warn "It looks like your PortIndex file may be corrupt."
+				throw
+			    } finally {
 				close $fd
+			    }
 			}
 		}
 	}
