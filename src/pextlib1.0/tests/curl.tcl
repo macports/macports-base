3a9ecb43a6a5dbb008184999a6254ea98f479770
Kevin Ballard <eridius@macports.org>
2007-07-05 11:10:27 +0000

    Fix curl test - research.att.com removed the download we were using, so update to the smallest ast-ksh download currently available
    git-svn-id: https://svn.macports.org/repository/macports/trunk/base@26722 d073be05-634f-4543-b044-5fe20cf6d1d6

--- src/pextlib1.0/tests/curl.tcl
+++ src/pextlib1.0/tests/curl.tcl
@@ -28,8 +28,8 @@ proc main {pextlibname} {
 	test {[md5 file $tempfile] == "a4b0f4a5fbd8bec23002ad8023e01729"}
 	
 	# use -u
-	curl fetch -u "I accept www.opensource.org/licenses/cpl:." http://www.research.att.com/~gsf/download/tgz/ast-ksh.2007-01-11.tgz $tempfile
-	test {[md5 file $tempfile] == "a24a0b8d8dc81600d624e3c0f2159e38"}
+	curl fetch -u "I accept www.opensource.org/licenses/cpl:." http://www.research.att.com/~gsf/download/tgz/ast-ksh.2007-03-28.2007-03-25.tgz $tempfile
+	test {[md5 file $tempfile] == "33b5dad7b2f6da0d7c2b16f3a91efeef"}
 	
 	file delete -force $tempfile
 }
