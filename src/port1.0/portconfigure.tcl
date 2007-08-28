54d716eb5b487db8baed17ae102b7c313cd06e83
Juan Manuel Palacios <jmpp@macports.org>
2007-08-28 03:04:19 +0000

    Clean up our beautiful name\!
    git-svn-id: https://svn.macports.org/repository/macports/trunk/base@28317 d073be05-634f-4543-b044-5fe20cf6d1d6

--- src/port1.0/portconfigure.tcl
+++ src/port1.0/portconfigure.tcl
@@ -119,11 +119,11 @@ proc configure_main {args} {
             set configure.cxx "/usr/bin/g++-4.0"
             set configure.cpp "/usr/bin/cpp-4.0" }
         apple-gcc-3.3 {
-            ui_debug "Using Macports Apple gcc 3.3"
+            ui_debug "Using MacPorts Apple gcc 3.3"
             set configure.cc "${prefix}/bin/gcc-apple-3.3"
             set configure.cpp "${prefix}/bin/cpp-apple-3.3" }
         apple-gcc-4.0 {
-            ui_debug "Using Macports Apple gcc 4.0"
+            ui_debug "Using MacPorts Apple gcc 4.0"
             set configure.cc "${prefix}/bin/gcc-apple-4.0"
             set configure.cpp "${prefix}/bin/cpp-apple-4.0" }
         macports-gcc-3.3 {
