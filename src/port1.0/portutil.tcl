f35c79cc19feeb0de1f275d7d36822432211bdeb
Ryan Schmidt <ryandesign@macports.org>
2008-05-14 09:36:20 +0000

    Fix comment: universal support requires the universal SDK be installed
    git-svn-id: https://svn.macports.org/repository/macports/trunk/base@36765 d073be05-634f-4543-b044-5fe20cf6d1d6

--- src/port1.0/portutil.tcl
+++ src/port1.0/portutil.tcl
@@ -1676,7 +1676,7 @@ proc default_universal_variant_allowed {args} {
 }
 
 proc add_default_universal_variant {args} {
-    # Declare default universal variant, on >10.3
+    # Declare default universal variant if universal SDK is installed
     variant universal description {Build for multiple architectures} {
         if {![file exists ${configure.universal_sysroot}]} {
             return -code error "Universal SDK is not installed (are we running on 10.3? did you forget to install it?) and building with +universal will very likely fail"
