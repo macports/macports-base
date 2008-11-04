48c40930bc43aa061a175f32e6d6360735530d1b
Joshua Root <jmr@macports.org>
2008-11-04 06:49:20 +0000

    Add new Norwegian distfiles mirror.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/base@41479 d073be05-634f-4543-b044-5fe20cf6d1d6

--- src/port1.0/resources/fetch/mirror_sites.tcl
+++ src/port1.0/resources/fetch/mirror_sites.tcl
@@ -199,6 +199,7 @@ set portfetch::mirror_sites::sites(macports) {
 
 set portfetch::mirror_sites::sites(macports_distfiles) {
     http://distfiles.macports.org/:mirror
+    http://trd.no.distfiles.macports.org/:mirror
 }
 
 set portfetch::mirror_sites::sites(openbsd) {
