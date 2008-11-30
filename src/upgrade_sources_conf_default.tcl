6f3cf09644067cab958602bbdbc338a427069f4b
Bryan Blackburn <blb@macports.org>
2008-11-30 04:58:48 +0000

    Fix issue where a tag (nosync) was already present in sources.conf, resulting in '... [nosync] [default]' which is incorrect; now it results in the correct form of '... [nosync,default]'
    git-svn-id: https://svn.macports.org/repository/macports/trunk/base@42734 d073be05-634f-4543-b044-5fe20cf6d1d6

--- src/upgrade_sources_conf_default.tcl
+++ src/upgrade_sources_conf_default.tcl
@@ -22,6 +22,16 @@ if {[catch {set sourcesConfChannel [open $sourcesConf r]}]} {
    exit 0
 }
 
+
+proc append_default_tag_to_line {line} {
+   if {[regexp {^(.* )\[(.*)\](.*)$} $line -> line_begin current_tag line_end]} {
+      return "${line_begin}\[${current_tag},default\]${line_end}"
+   } else {
+      return "${line} \[default\]"
+   }
+}
+
+
 set mktempChannel [open "|/usr/bin/mktemp -t macports_sources_upgrade" r]
 set tempfile [read -nonewline $mktempChannel]
 close $mktempChannel
@@ -35,7 +45,7 @@ while {[gets $sourcesConfChannel line] >= 0} {
       if {[string first {[default]} $line] >= 0} {
          set defaultSeen true
       } elseif {[regexp {^\s*rsync://rsync\.macports\.org/release/ports/} $line]} {
-         set line "$line \[default\]"
+         set line [append_default_tag_to_line $line]
          set defaultSeen true
          set defaultWritten true
       } elseif {[regexp {^\s*file://(/[^ #]+)} $line -> filepath]} {
@@ -47,7 +57,7 @@ while {[gets $sourcesConfChannel line] >= 0} {
             }
             close $svnChannel
             if {[regexp {^https?://svn\.macports\.org/repository/macports/trunk/dports} $svnURL]} {
-               set line "${line} \[default\]"
+               set line [append_default_tag_to_line $line]
                set defaultSeen true
                set defaultWritten true
             }
