91155ad67f7e0f42c783d2a5ebd980238af138f3
Joshua Root <jmr@macports.org>
2009-01-28 02:17:10 +0000

    upgrade: use a single depscache for all requested ports
    git-svn-id: https://svn.macports.org/repository/macports/trunk/base@46055 d073be05-634f-4543-b044-5fe20cf6d1d6

--- src/port/port.tcl
+++ src/port/port.tcl
@@ -1870,15 +1870,19 @@ proc action_upgrade { action portlist opts } {
     if {[require_portlist portlist]} {
         return 1
     }
+    # shared depscache for all ports in the list
+    array set depscache {}
     foreachport $portlist {
         if {[catch {registry::installed $portname}]} {
             ui_error "$portname is not installed"
             return 1
         }
-        # Global variations will have to be merged into the specified
-        # variations, but perhaps after the installed variations are
-        # merged. So we pass them into upgrade:
-        macports::upgrade $portname "port:$portname" [array get global_variations] [array get variations] [array get options]
+        if {![info exists depscache(port:$portname)]} {
+            # Global variations will have to be merged into the specified
+            # variations, but perhaps after the installed variations are
+            # merged. So we pass them into upgrade:
+            macports::upgrade $portname "port:$portname" [array get global_variations] [array get variations] [array get options] depscache
+        }
     }
 
     return 0
