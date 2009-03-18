bbc35314f22b38121db9653bbc34b74d027279d1
Perry Lee <perry@macports.org>
2009-03-18 22:55:58 +0000

    port1.0/portlivecheck.tcl - freshmeat's default livecheck.regex is case-insensitive.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/base@48310 d073be05-634f-4543-b044-5fe20cf6d1d6

--- src/port1.0/portlivecheck.tcl
+++ src/port1.0/portlivecheck.tcl
@@ -126,7 +126,7 @@ proc livecheck_main {args} {
                 set livecheck.url "http://freshmeat.net/projects/${livecheck.name}/releases.atom"
             }
             if {${livecheck.regex} eq ""} {
-                set livecheck.regex [list "<title>${livecheck.name} (.*)</title>"]
+                set livecheck.regex [list "(?i)<title>${livecheck.name} (.*)</title>"]
             }
             set livecheck.check "regex"
         }
