4fe7ea98b36664fcc43140978e78e5abc8aa518e
Toby Peterson <toby@macports.org>
2009-03-26 04:04:50 +0000

    cursory support for compiling with clang
    git-svn-id: https://svn.macports.org/repository/macports/trunk/base@48594 d073be05-634f-4543-b044-5fe20cf6d1d6

--- src/port1.0/portconfigure.tcl
+++ src/port1.0/portconfigure.tcl
@@ -188,6 +188,7 @@ proc configure_start {args} {
         macports-gcc-4.2 { set name "MacPorts gcc 4.2" }
         macports-gcc-4.3 { set name "MacPorts gcc 4.3" }
         macports-gcc-4.4 { set name "MacPorts gcc 4.4" }
+        ccc { set name "clang ccc" }
         default { return -code error "Invalid value for configure.compiler" }
     }
     ui_debug "Using compiler '$name'"
@@ -426,6 +427,12 @@ proc configure_get_compiler {type} {
                 fc   { set ret ${prefix}/bin/gfortran-mp-4.4 }
                 f77  { set ret ${prefix}/bin/gfortran-mp-4.4 }
                 f90  { set ret ${prefix}/bin/gfortran-mp-4.4 }
+            }
+        }
+        ccc {
+            switch -exact ${type} {
+                cc   { set ret /Developer/usr/bin/ccc }
+                objc { set ret /Developer/usr/bin/ccc }
             }
         }
     }
