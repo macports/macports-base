7a4de1b84b4c0a623b28c16df4389aa96ff159f0
Andrea D'Amore <and.damore@macports.org>
2009-04-29 08:21:56 +0000

    More little steps for mpbot.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/base@50330 d073be05-634f-4543-b044-5fe20cf6d1d6

--- portmgr/bots/trac.rb
+++ portmgr/bots/trac.rb
@@ -17,8 +17,12 @@ class TracPlugin < Plugin
 		case topic
 		  when "ticket"
 			return "ticket <ticket no.> => show http link for ticket # <ticket no.>"
+		  when "faq"
+			return "faq [help] => show FAQs' URL or help"
+		  when "guide"
+			return "guide [help] => show The Guide's URL or help. Don't Panic."		 
 		  else
-			return "trac commands: ticket"
+			return "trac module provides: !ticket, !faq, !guide"
 		end
 	end
 
@@ -31,8 +35,28 @@ class TracPlugin < Plugin
 			m.reply "Use either #1234 or 1234 for ticket number"
 		end
 	end
+
+	def faq(m, params)
+		if ( params[:parm] )
+			m.reply "Just type !faq for now"
+		else
+			m.reply "FAQs are at: http://trac.macports.org/wiki/FAQ"
+		end
+	end
+
+	def guide(m, params)
+		if ( params[:parm] == "chunked" )
+			m.reply "http://guide.macports.org/chunked/index.html"
+		elsif ( params[:parm] != "" )
+			m.reply "Just type !faq for now"
+		else
+			m.reply "FAQs are at: http://trac.macports.org/wiki/FAQ"
+		end
+	end
 	
 end
 
 plugin = TracPlugin.new
 plugin.map 'ticket :number', :action => 'ticket'
+plugin.map 'faq :parm', :action => 'faq'
+plugin.map 'guide :parm', :action => 'guide'
