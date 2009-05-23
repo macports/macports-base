402100cf7f7680f8016889c4853271263f25f33c
Rainer Müller <raimue@macports.org>
2009-05-23 20:20:58 +0000

    base: Generate pkgIndex.tcl in the source tree, not at install time. This will detect errors earlier.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/base@51382 d073be05-634f-4543-b044-5fe20cf6d1d6

--- Mk/macports.tea.mk
+++ Mk/macports.tea.mk
@@ -8,17 +8,20 @@
 .c.o:
 	${CC} -c -DUSE_TCL_STUBS -DTCL_NO_DEPRECATED ${CFLAGS} ${SHLIB_CFLAGS} $< -o $@
 
+all:: ${SHLIB_NAME} pkgIndex.tcl
+
 $(SHLIB_NAME):: ${OBJS}
 	${SHLIB_LD} ${OBJS} -o ${SHLIB_NAME} ${TCL_STUB_LIB_SPEC} ${SHLIB_LDFLAGS} ${LIBS}
 
-all:: ${SHLIB_NAME}
+pkgIndex.tcl:
+	$(SILENT) ../pkg_mkindex.sh .
 
 clean::
-	rm -f ${OBJS} ${SHLIB_NAME} so_locations
+	rm -f ${OBJS} ${SHLIB_NAME} so_locations pkgIndex.tcl
 
 distclean:: clean
 
 install:: all
 	$(INSTALL) -d -o ${DSTUSR} -g ${DSTGRP} -m ${DSTMODE} ${INSTALLDIR}
 	$(INSTALL) -o ${DSTUSR} -g ${DSTGRP} -m 444 ${SHLIB_NAME} ${INSTALLDIR}
-	$(SILENT) ../pkg_mkindex.sh ${INSTALLDIR}
+	$(INSTALL) -o ${DSTUSR} -g ${DSTGRP} -m 444 pkgIndex.tcl ${INSTALLDIR}
