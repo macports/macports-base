066daf1cad1f47e20c5632d9dd2a18124e22cad9
Joshua Root <jmr@macports.org>
2009-06-04 23:51:48 +0000

    make clean --dist clean the work dir too, otherwise fetch could still be marked as done in the statefile
    git-svn-id: https://svn.macports.org/repository/macports/trunk/base@51838 d073be05-634f-4543-b044-5fe20cf6d1d6

--- src/port1.0/portclean.tcl
+++ src/port1.0/portclean.tcl
@@ -72,8 +72,7 @@ proc portclean::clean_main {args} {
     }
     if {[info exists ports_clean_all] && $ports_clean_all == "yes" || \
         [info exists ports_clean_work] && $ports_clean_work == "yes" || \
-        (!([info exists ports_clean_dist] && $ports_clean_dist == "yes") && \
-         !([info exists ports_clean_archive] && $ports_clean_archive == "yes"))} {
+        (!([info exists ports_clean_archive] && $ports_clean_archive == "yes"))} {
          ui_info "$UI_PREFIX [format [msgcat::mc "Removing build directory for %s"] [option name]]"
          clean_work
     }
