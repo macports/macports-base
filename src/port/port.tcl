f71f6e7a8c6d9a435263e407d43e23ef07aa5c1b
Rainer Müller <raimue@macports.org>
2010-01-31 04:20:15 +0000

    port/port.tcl: Add missing close-bracket, fixes breakage introduced in https://trac.macports.org/changeset/63269
    git-svn-id: https://svn.macports.org/repository/macports/trunk/base@63271 d073be05-634f-4543-b044-5fe20cf6d1d6

--- src/port/port.tcl
+++ src/port/port.tcl
@@ -1472,7 +1472,7 @@ proc action_log { action portlist opts } {
         set portpath [macports::getportdir $porturl]
         set logfile [file join [macports::getportlogpath $portpath] "main.log"]
         if {[file exists $logfile]} {
-            if {[catch {set fp [open $logfile r]} result} {
+            if {[catch {set fp [open $logfile r]} result]} {
                 break_softcontinue "Could not open file $logfile: $result" 1 status
             }
             set data [read $fp]
