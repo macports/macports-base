2b47fb33d5847da2e9498afae5186394c756703a
Joshua Root <jmr@macports.org>
2010-03-12 18:04:32 +0000

    revert https://trac.macports.org/changeset/64642, caused breakage
    git-svn-id: https://svn.macports.org/repository/macports/trunk/base@64643 d073be05-634f-4543-b044-5fe20cf6d1d6

--- src/macports1.0/macports.tcl
+++ src/macports1.0/macports.tcl
@@ -960,9 +960,7 @@ proc macports::worker_init {workername portpath porturl portbuildpath options va
     }
 
     $workername alias ui_prefix ui_prefix
-    if {[llength [info commands ui_channels]] > 0} {
-        $workername alias ui_channels ui_channels
-    }
+    $workername alias ui_channels ui_channels
     
     $workername alias ui_warn_once ui_warn_once
 
