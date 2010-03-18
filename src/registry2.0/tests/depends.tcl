df37953d0c1dacb02a2c73c4542bf2c3d81e0d91
Rainer Müller <raimue@macports.org>
2010-03-18 23:37:15 +0000

    registry2.0/tests: After https://trac.macports.org/changeset/64627, there is no difference between imaged and installed anymore for dependents.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/base@64986 d073be05-634f-4543-b044-5fe20cf6d1d6

--- src/registry2.0/tests/depends.tcl
+++ src/registry2.0/tests/depends.tcl
@@ -22,29 +22,29 @@ proc main {pextlibname} {
     #                   e
 
     registry::write {
-        set a1 [registry::entry create a 1 0 0 {}]
-        set a2 [registry::entry create a 2 0 0 {}]
-        set a3 [registry::entry create a 3 0 0 {}]
+        set a1 [registry::entry create a 1 0 {} 0]
+        set a2 [registry::entry create a 2 0 {} 0]
+        set a3 [registry::entry create a 3 0 {} 0]
         $a1 depends b
         $a2 depends b
         $a3 depends c
 
-        set b1 [registry::entry create b 1 0 0 {}]
-        set b2 [registry::entry create b 2 0 0 {}]
+        set b1 [registry::entry create b 1 0 {} 0]
+        set b2 [registry::entry create b 2 0 {} 0]
         $b1 depends d
         $b1 depends f
         $b2 depends d
         $b2 depends g
 
-        set c [registry::entry create c 1 0 0 {}]
+        set c [registry::entry create c 1 0 {} 0]
         $c depends e
 
-        set d [registry::entry create d 1 0 0 {}]
+        set d [registry::entry create d 1 0 {} 0]
         $d depends e
 
-        set e [registry::entry create e 1 0 0 {}]
-        set f [registry::entry create f 1 0 0 {}]
-        set g [registry::entry create g 1 0 0 {}]
+        set e [registry::entry create e 1 0 {} 0]
+        set f [registry::entry create f 1 0 {} 0]
+        set g [registry::entry create g 1 0 {} 0]
 
         $a1 state installed
         $a2 state imaged
@@ -62,98 +62,24 @@ proc main {pextlibname} {
         test_set {[$a1 dependents]} {}
         test_set {[$a2 dependents]} {}
         test_set {[$a3 dependents]} {}
-        test_set {[$b1 dependents]} {$a1}
-        test_set {[$b2 dependents]} {}
-        test_set {[$c dependents]} {}
-        test_set {[$d dependents]} {$b1}
+        test_set {[$b1 dependents]} {$a1 $a2}
+        test_set {[$b2 dependents]} {$a1 $a2}
+        test_set {[$c dependents]} {$a3}
+        test_set {[$d dependents]} {$b1 $b2}
         test_set {[$e dependents]} {$c $d}
         test_set {[$f dependents]} {$b1}
-        test_set {[$g dependents]} {}
+        test_set {[$g dependents]} {$b2}
 
-        test_set {[$a1 dependencies]} {$b1}
+        test_set {[$a1 dependencies]} {$b1 $b2}
+        test_set {[$a2 dependencies]} {$b1 $b2}
+        test_set {[$a3 dependencies]} {$c}
         test_set {[$b1 dependencies]} {$d $f}
+        test_set {[$b2 dependencies]} {$d $g}
         test_set {[$c dependencies]} {$e}
         test_set {[$d dependencies]} {$e}
         test_set {[$e dependencies]} {}
         test_set {[$f dependencies]} {}
         test_set {[$g dependencies]} {}
-    }
-
-    registry::write {
-        $b1 state imaged
-        $b2 state installed
-        $g state installed
-    }
-
-    registry::read {
-        test_set {[$b1 dependents]} {}
-        test_set {[$b2 dependents]} {$a1}
-        test_set {[$c dependents]} {}
-        test_set {[$d dependents]} {$b2}
-        test_set {[$e dependents]} {$c $d}
-        test_set {[$f dependents]} {}
-        test_set {[$g dependents]} {$b2}
-
-        test_set {[$a1 dependencies]} {$b2}
-        test_set {[$b2 dependencies]} {$d $g}
-        test_set {[$c dependencies]} {$e}
-        test_set {[$d dependencies]} {$e}
-    }
-
-    registry::write {
-        $a1 state imaged
-        $a2 state installed
-        $f state imaged
-    }
-
-    registry::read {
-        test_set {[$b1 dependents]} {}
-        test_set {[$b2 dependents]} {$a2}
-        test_set {[$c dependents]} {}
-        test_set {[$d dependents]} {$b2}
-        test_set {[$e dependents]} {$c $d}
-        test_set {[$f dependents]} {}
-        test_set {[$g dependents]} {$b2}
-
-        test_set {[$a2 dependencies]} {$b2}
-        test_set {[$b2 dependencies]} {$d $g}
-        test_set {[$c dependencies]} {$e}
-        test_set {[$d dependencies]} {$e}
-    }
-
-    registry::write {
-        $a2 state imaged
-        $a3 state installed
-    }
-
-    registry::read {
-        test_set {[$b1 dependents]} {}
-        test_set {[$b2 dependents]} {}
-        test_set {[$c dependents]} {$a3}
-        test_set {[$d dependents]} {$b2}
-        test_set {[$e dependents]} {$c $d}
-        test_set {[$f dependents]} {}
-        test_set {[$g dependents]} {$b2}
-
-        test_set {[$a3 dependencies]} {$c}
-        test_set {[$b2 dependencies]} {$d $g}
-        test_set {[$c dependencies]} {$e}
-        test_set {[$d dependencies]} {$e}
-    }
-
-    registry::write {
-        $b2 state imaged
-        $d state imaged
-    }
-
-    registry::read {
-        test_set {[$c dependents]} {$a3}
-        test_set {[$d dependents]} {}
-        test_set {[$e dependents]} {$c}
-        test_set {[$g dependents]} {}
-
-        test_set {[$a3 dependencies]} {$c}
-        test_set {[$d dependencies]} {$e}
     }
 
     file delete test.db
