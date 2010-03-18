8ef265da751fd6f41a45302b6fa2969315ca283b
Joshua Root <jmr@macports.org>
2010-03-18 17:17:21 +0000

    ensure that os.platform and os.major are set in the registry before comparing them
    git-svn-id: https://svn.macports.org/repository/macports/trunk/base@64968 d073be05-634f-4543-b044-5fe20cf6d1d6

--- src/macports1.0/macports.tcl
+++ src/macports1.0/macports.tcl
@@ -2804,8 +2804,9 @@ proc macports::_upgrade {portname dspec variationslist optionslist {depscachenam
         } elseif {[info exists options(ports_upgrade_enforce-variants)] && $options(ports_upgrade_enforce-variants) eq "yes"
                   && [info exists portinfo(canonical_active_variants)] && $portinfo(canonical_active_variants) != $oldvariant} {
             ui_debug "variant override ... upgrading!"
-        } elseif {[_mportkey $workername "{os.platform}"] != $os_platform_installed
-                  || [_mportkey $workername "{os.major}"] != $os_major_installed} {
+        } elseif {$os_platform_installed != "" && $os_major_installed != "" && $os_platform_installed != 0
+                  && ([_mportkey $workername "{os.platform}"] != $os_platform_installed
+                  || [_mportkey $workername "{os.major}"] != $os_major_installed)} {
             ui_debug "platform mismatch ... upgrading!"
             set build_override 1
         } else {
