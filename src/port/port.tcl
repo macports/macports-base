85f6e5827d35e9cef96439b70d84c2be8ad033c1
Joshua Root <jmr@macports.org>
2010-06-28 06:54:45 +0000

    remove trace from action array (not a real target)
    git-svn-id: https://svn.macports.org/repository/macports/trunk/base@69191 d073be05-634f-4543-b044-5fe20cf6d1d6

--- src/port/port.tcl
+++ src/port/port.tcl
@@ -3782,7 +3782,6 @@ array set action_array [list \
     test        [list action_target         [ACTION_ARGS_PORTS]] \
     lint        [list action_target         [ACTION_ARGS_PORTS]] \
     submit      [list action_target         [ACTION_ARGS_PORTS]] \
-    trace       [list action_target         [ACTION_ARGS_PORTS]] \
     livecheck   [list action_target         [ACTION_ARGS_PORTS]] \
     distcheck   [list action_target         [ACTION_ARGS_PORTS]] \
     mirror      [list action_target         [ACTION_ARGS_PORTS]] \
