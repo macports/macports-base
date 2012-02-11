aba3850e15ef7015e8995f71b7836361003cb74b
Andrea D'Amore <and.damore@macports.org>
2012-02-11 09:04:12 +0000

    base: command "rpm-vercomp" replaced with "vercmp" in code, cf. https://trac.macports.org/changeset/89810
    git-svn-id: https://svn.macports.org/repository/macports/trunk/base@89811 d073be05-634f-4543-b044-5fe20cf6d1d6

--- src/pextlib1.0/Pextlib.c
+++ src/pextlib1.0/Pextlib.c
@@ -588,10 +588,7 @@ int Pextlib_Init(Tcl_Interp *interp)
 	Tcl_CreateObjCommand(interp, "xinstall", InstallCmd, NULL, NULL);
 	Tcl_CreateObjCommand(interp, "fs-traverse", FsTraverseCmd, NULL, NULL);
 	Tcl_CreateObjCommand(interp, "filemap", FilemapCmd, NULL, NULL);
-#if 1
-	/* the name "rpm-vercomp" is deprecated, use "vercmp" instead */
-	Tcl_CreateObjCommand(interp, "rpm-vercomp", VercompCmd, NULL, NULL);
-#endif
+	/* "rpm-vercomp" command removed in r89810, replaced by "vercmp" */
 	Tcl_CreateObjCommand(interp, "vercmp", VercompCmd, NULL, NULL);
 	Tcl_CreateObjCommand(interp, "rmd160", RMD160Cmd, NULL, NULL);
 	Tcl_CreateObjCommand(interp, "sha256", SHA256Cmd, NULL, NULL);
