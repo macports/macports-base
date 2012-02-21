5931f619c111c29ee8578ecdf20627b3edd42f7e
Clemens Lang <cal@macports.org>
2012-02-21 16:51:02 +0000

    copy_xcode_plist: Fix misleading error messages, permissions need to be in octal, only print debug message about copying the plist if copying is actually attempted
    git-svn-id: https://svn.macports.org/repository/macports/trunk/base@90100 d073be05-634f-4543-b044-5fe20cf6d1d6

--- src/macports1.0/macports.tcl
+++ src/macports1.0/macports.tcl
@@ -1186,19 +1186,19 @@ proc macports::copy_xcode_plist {target_homedir} {
     if {[file isfile $user_plist]} {
         if {![file isdirectory "${target_dir}"]} {
             if {[catch {file mkdir "${target_dir}"} result]} {
-                ui_warn "Failed to create Library/Preferences in temporary home directory: $result"
+                ui_warn "Failed to create Library/Preferences in ${target_homedir}: $result"
                 return
             }
         }
-        ui_debug "Copying $user_plist to temporary home directory ${target_dir}"
         if {[file writable ${target_dir}] && [catch {
+            ui_debug "Copying $user_plist to ${target_dir}"
             file copy -force $user_plist $target_dir
             if {[getuid] == 0} {
                 file attributes "${target_dir}/com.apple.dt.Xcode.plist" -owner $macportsuser
             }
-            file attributes "${target_dir}/com.apple.dt.Xcode.plist" -permissions 644
+            file attributes "${target_dir}/com.apple.dt.Xcode.plist" -permissions 0644
         } result]} {
-            ui_warn "Failed to copy com.apple.dt.Xcode.plist to temporary home directory: $result"
+            ui_warn "Failed to copy com.apple.dt.Xcode.plist to ${target_dir}: $result"
         }
     }
 }
