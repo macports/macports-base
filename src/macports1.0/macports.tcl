4859001032604454986b9b39b78a2a35b00e0d1c
Joshua Root <jmr@macports.org>
2012-02-24 12:28:35 +0000

    delete any existing xcode plist from our home dir before attempting to copy a new one
    git-svn-id: https://svn.macports.org/repository/macports/trunk/base@90154 d073be05-634f-4543-b044-5fe20cf6d1d6

--- src/macports1.0/macports.tcl
+++ src/macports1.0/macports.tcl
@@ -1183,6 +1183,7 @@ proc macports::copy_xcode_plist {target_homedir} {
     global macports::user_home macports::macportsuser
     set user_plist "${user_home}/Library/Preferences/com.apple.dt.Xcode.plist"
     set target_dir "${target_homedir}/Library/Preferences"
+    file delete -force "${target_dir}/com.apple.dt.Xcode.plist"
     if {[file isfile $user_plist]} {
         if {![file isdirectory "${target_dir}"]} {
             if {[catch {file mkdir "${target_dir}"} result]} {
