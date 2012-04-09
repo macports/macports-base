aa2d58892e701a7109704812f1efc078d110dafc
Clemens Lang <cal@macports.org>
2012-04-09 13:57:03 +0000

    Tests for https://trac.macports.org/changeset/91744.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/base@91745 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ src/port1.0/tests/portdestroot.tcl
@@ -0,0 +1,39 @@
+package require macports
+mportinit
+
+source [file dirname [info script]]/../portdestroot.tcl
+source [file dirname [info script]]/common.tcl
+
+namespace eval tests {
+
+proc "when destroot cmd is not gmake no -w argument is added" {} {
+    global build.type build.cmd destroot.cmd destroot.target
+
+    set build.type "gnu"
+    set build.cmd "gmake"
+    set destroot.cmd "_destroot_cmd_"
+    set destroot.target "_target_"
+
+    test_equal {[portdestroot::destroot_getargs]} "_target_"
+}
+
+proc "when destroot cmd is gmake a -w argument is added" {} {
+    global build.type build.cmd destroot.cmd destroot.target
+
+    set build.type "gnu"
+    set build.cmd "_build_cmd_"
+    set destroot.cmd "gmake"
+    set destroot.target "_target_"
+
+    test_equal {[portdestroot::destroot_getargs]} "-w _target_"
+}
+
+
+# run all tests
+foreach proc [info procs *] {
+    puts "* ${proc}"
+    $proc
+}
+
+# namespace eval tests
+}
