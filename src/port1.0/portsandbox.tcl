ce4cbcb9a9eb6ac86717306e81a0da995486c511
Joshua Root <jmr@macports.org>
2012-10-24 14:06:52 +0000

    add ccache_dir to sandbox when ccache is enabled
    git-svn-id: https://svn.macports.org/repository/macports/trunk/base@99028 d073be05-634f-4543-b044-5fe20cf6d1d6

--- src/port1.0/portsandbox.tcl
+++ src/port1.0/portsandbox.tcl
@@ -41,7 +41,8 @@ default portsandbox_profile {}
 # command line usage would be:
 # sandbox-exec -p '(version 1) (allow default) (deny file-write*) (allow file-write* <filter>)' some-command
 proc portsandbox::set_profile {target} {
-    global os.major portsandbox_profile workpath distpath altprefix package.destpath
+    global os.major portsandbox_profile workpath distpath altprefix \
+        package.destpath configure.ccache ccache_dir
 
     switch $target {
         activate -
@@ -67,6 +68,9 @@ proc portsandbox::set_profile {target} {
 
     # TODO: remove altprefix support
     lappend allow_dirs $workpath $altprefix
+    if {${configure.ccache}} {
+        lappend allow_dirs $ccache_dir
+    }
 
     set portsandbox_profile "(version 1) (allow default) (deny file-write*) \
 (allow file-write-data (literal \"/dev/null\")) (allow file-write* (regex #\"^(/private)?(/var)?/tmp/\" \
