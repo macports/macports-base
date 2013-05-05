e3630bff210c40d8f337093e15ba911f5ce8ca8b
Clemens Lang <cal@macports.org>
2013-05-05 19:44:56 +0000

    tracelib.c: Fix setrlimit with uninitialized fields
    git-svn-id: https://svn.macports.org/repository/macports/trunk/base@105765 d073be05-634f-4543-b044-5fe20cf6d1d6

--- src/pextlib1.0/tracelib.c
+++ src/pextlib1.0/tracelib.c
@@ -429,17 +429,20 @@ static int TracelibOpenSocketCmd(Tcl_Interp * in)
 	pthread_mutex_unlock(&sock_mutex);
 	
 	interp=in;
-	
+
 	/* raise the limit of open files to the maximum from the default soft limit
 	 * of 256 */
+	if (getrlimit(RLIMIT_NOFILE, &rl) == -1) {
+		ui_warn("getrlimit failed (%d), skipping setrlimit", errno);
+	} else {
 #if defined(__APPLE__) && defined(OPEN_MAX)
-	if (rl.rlim_max > OPEN_MAX)
-		rl.rlim_max = OPEN_MAX;
+		if (rl.rlim_max > OPEN_MAX)
+			rl.rlim_max = OPEN_MAX;
 #endif
-	rl.rlim_cur = rl.rlim_max;
-	if(setrlimit(RLIMIT_NOFILE, &rl)==-1)
-	{
-		ui_warn("setrlimit failed (%d)", errno);
+		rl.rlim_cur = rl.rlim_max;
+		if (setrlimit(RLIMIT_NOFILE, &rl) == -1) {
+			ui_warn("setrlimit failed (%d)", errno);
+		}
 	}
 
 	
