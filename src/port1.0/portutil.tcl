0348268a0f4d42a13484da564af2a7ddfdac6d62
Joshua Root <jmr@macports.org>
2013-06-08 16:13:20 +0000

    update current OS and Xcode versions
    git-svn-id: https://svn.macports.org/repository/macports/trunk/base@106799 d073be05-634f-4543-b044-5fe20cf6d1d6

--- src/port1.0/portutil.tcl
+++ src/port1.0/portutil.tcl
@@ -3058,12 +3058,12 @@ proc _check_xcode_version {} {
             10.7 {
                 set min 4.1
                 set ok 4.1
-                set rec 4.5.2
+                set rec 4.6.2
             }
             default {
                 set min 4.4
                 set ok 4.4
-                set rec 4.5.2
+                set rec 4.6.2
             }
         }
         if {$xcodeversion == "none"} {
