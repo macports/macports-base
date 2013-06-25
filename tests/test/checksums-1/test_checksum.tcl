5b096451f7afad2dd0c2ec34cdc75bac445e063e
Marius Coțofană <marius@macports.org>
2013-06-25 11:06:01 +0000

    Added initial setup for checksum test and library file.
    git-svn-id: https://svn.macports.org/repository/macports/branches/gsoc13-tests@107291 d073be05-634f-4543-b044-5fe20cf6d1d6

--- tests/test/checksums-1/test_checksum.tcl
+++ tests/test/checksums-1/test_checksum.tcl
@@ -1,21 +1,21 @@
 package require tcltest 2
 namespace import tcltest::*
 
-source checksum.tcl
+source ./checksum.tcl
 
 set file "output"
 set dir "work"
+
+# Initial setup
+load_variables
+port_clean
+port_run
+
 
 test md5_checksum {
     Regression test for MD5 Checksum.
 } -constraints {
     root
-} -setup {
-    if { [file exists $file] == 0 } {
-        load_variables
-        clean
-        run
-    }
 } -body {
     get_md5 $file
 } -result "d41d8cd98f00b204e9800998ecf8427e"
@@ -25,12 +25,6 @@ test sha1_checksum {
     Regression test for SHA1 Checksum.
 } -constraints {
     root
-} -setup {
-    if { [file exists $file] == 0 } {
-        load_variables
-        clean
-        run
-    }
 } -body {
     get_sha $file
 } -result "da39a3ee5e6b4b0d3255bfef95601890afd80709"
@@ -40,12 +34,6 @@ test rmd160_checksum {
     Regression test for RMD160 Checksum.
 } -constraints {
     root
-} -setup {
-    if { [file exists $file] == 0 } {
-        load_variables
-        clean
-        run
-    }
 } -body {
     get_rmd $file
 } -result "9c1185a5c5e9fc54612808977ee8f548b2258d31"
@@ -54,4 +42,5 @@ test rmd160_checksum {
 # remove output file and print results
 removeFile $file
 removeDirectory $dir
+
 cleanupTests
