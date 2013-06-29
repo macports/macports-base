617172ae296b001a63b046a6e6e42f6f2e84f2cd
Marius Coțofană <marius@macports.org>
2013-06-29 08:37:56 +0000

    Added port_index and set_dir to library.tcl.
    git-svn-id: https://svn.macports.org/repository/macports/branches/gsoc13-tests@107435 d073be05-634f-4543-b044-5fe20cf6d1d6

--- tests/test/library.tcl
+++ tests/test/library.tcl
@@ -1,3 +1,5 @@
+set autoconf "../../../Mk/macports.autoconf.mk"
+
 # Set of procs used for testing.
 
 # Sets $bindir variable from macports.autoconf.mk
@@ -5,6 +7,7 @@
 proc load_variables {} {
     global autoconf
     global bindir
+    global datadir
 
     if { [file exists $autoconf] == 0 } {
         puts "$autoconf does not exist."
@@ -18,6 +21,39 @@ proc load_variables {} {
     set bin [lrange [split $line "/"] 1 1]
 
     set bindir $prefix/$bin/
+    set datadir $prefix/share
+
+    #TO DO: Add PORTSRC var
+}
+
+# Sets initial directories
+proc set_dir {} {
+    global datadir
+    set path [pwd]
+
+    file delete -force /tmp/macports-tests/
+    file delete -force PortIndex PortIndex.quick
+
+    file mkdir /tmp/macports-tests/ports
+    file mkdir /tmp/macports-tests/opt/local/etc/macports/
+    file mkdir /tmp/macports-tests/opt/local/share/
+    file mkdir /tmp/macports-tests/opt/local/var/macports/receipts/
+    file mkdir /tmp/macports-tests/opt/local/var/macports/registry/
+    file mkdir /tmp/macports-tests/opt/local/var/macports/build/
+
+    file link -symbolic /tmp/macports-tests/opt/local/share/macports $datadir/macports
+    file link -symbolic /tmp/macports-tests/ports/test $path/test
+}
+
+# Run portindex
+proc port_index {} {
+  global bindir
+
+  set cmd "portindex"
+
+  file copy sources.conf /tmp/macports-tests/opt/local/etc/macports/
+  set result [exec $bindir$cmd 2>&1]
+  file copy PortIndex PortIndex.quick /tmp/macports-tests/ports/
 }
 
 # Executes port clean.
