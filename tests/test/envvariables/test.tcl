e5999f1d0b9d8b54585d2eabc145262d02e4c68a
Marius Coțofană <marius@macports.org>
2013-07-03 23:22:37 +0000

    envvariables removed constraints, removed aux file, added description, cleanup.
    git-svn-id: https://svn.macports.org/repository/macports/branches/gsoc13-tests@107657 d073be05-634f-4543-b044-5fe20cf6d1d6

--- tests/test/envvariables/test.tcl
+++ tests/test/envvariables/test.tcl
@@ -3,9 +3,10 @@ namespace import tcltest::*
 
 source [file dirname $argv0]/../library.tcl
 
-set file "output"
-set dir "work"
+makeFile "" $output_file
+makeDirectory $work_dir
 set path [file dirname [file normalize $argv0]]
+
 
 # Initial setup
 load_variables $path
@@ -14,39 +15,28 @@ port_index
 port_clean $path
 
 proc envvar_test {} {
-    global file
+    global output_file
     global path
     global portsrc
     global bindir
 
-    # Build helping script
-    set fp [open script.sh w+]
-    puts -nonewline $fp "export ENVA=A; export ENVB=B; export PORTSRC="
-    puts -nonewline $fp $portsrc
-    puts -nonewline $fp "; "
-    puts -nonewline $fp $bindir
-    puts -nonewline $fp "port test"
-    close $fp
+    # Build helping string
+    append string "export ENVA=A; export ENVB=B; "
+    append string "export PORTSRC=" $portsrc "; "
+    append string $bindir "port test"
 
-    exec sh script.sh > output
-    set line [get_line $path/$file "a"]
-    set line2 [get_line $path/$file "b"]
+    exec sh -c $string > output
+    set line [get_line $path/$output_file "a"]
+    set line2 [get_line $path/$output_file "b"]
     return $line$line2
 }
 
 test envvariables {
     Regression test for Environment Variables.
-} -constraints {
-    root
 } -body {
     envvar_test
 } -result "ab"
 
-
-# remove output file and print results
-removeFile script.sh
-removeFile $file
-removeDirectory $dir
 
 cleanup
 cleanupTests
