9b29e40a4c2d509f312dd3dd3849d22b74eac7a3
Marius Coțofană <marius@macports.org>
2013-07-04 15:29:50 +0000

    Switch xcodeversion test to use initial_setup proc.
    git-svn-id: https://svn.macports.org/repository/macports/branches/gsoc13-tests@107712 d073be05-634f-4543-b044-5fe20cf6d1d6

--- tests/test/xcodeversion/test.tcl
+++ tests/test/xcodeversion/test.tcl
@@ -2,18 +2,9 @@ package require tcltest 2
 namespace import tcltest::*
 
 source [file dirname $argv0]/../library.tcl
-
-makeFile "" $output_file
-makeDirectory $work_dir
 set path [file dirname [file normalize $argv0]]
 
-
-# Initial setup
-load_variables $path
-set_dir
-port_index
-port_clean $path
-port_run $path
+initial_setup
 
 proc xcode_ver {} {
     global output_file
