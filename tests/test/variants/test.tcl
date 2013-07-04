2ae04c05067cc1acefd81c9bcc6c19b3ce12a1f4
Marius Coțofană <marius@macports.org>
2013-07-04 15:25:20 +0000

    Added variants test.
    git-svn-id: https://svn.macports.org/repository/macports/branches/gsoc13-tests@107710 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ tests/test/variants/test.tcl
@@ -0,0 +1,27 @@
+package require tcltest 2
+namespace import tcltest::*
+
+source [file dirname $argv0]/../library.tcl
+set path [file dirname [file normalize $argv0]]
+
+initial_setup
+
+proc var_check {} {
+    global output_file
+    global path
+
+    set var "utopia variant*"
+    set line [get_line $path/$output_file $var]
+    return $line
+}
+
+
+test variants {
+    Regression test for variants.
+} -body {
+    var_check
+} -result "utopia variant -- 2"
+
+
+cleanup
+cleanupTests
