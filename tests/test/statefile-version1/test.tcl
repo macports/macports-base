ef9507bd50636f23d4e3712d27f19d392a0f5631
Marius Coțofană <marius@macports.org>
2013-07-05 23:32:04 +0000

    Added statefile-version1 test.
    git-svn-id: https://svn.macports.org/repository/macports/branches/gsoc13-tests@107801 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ tests/test/statefile-version1/test.tcl
@@ -0,0 +1,36 @@
+package require tcltest 2
+namespace import tcltest::*
+
+source [file dirname $argv0]/../library.tcl
+
+makeFile "" $output_file
+makeDirectory $work_dir
+set path [file dirname [file normalize $argv0]]
+
+
+# Initial setup
+load_variables $path
+set_dir
+port_index
+port_config $path
+port_desroot $path
+port_clean $path
+
+proc statefile_v1 {} {
+    global path
+    global output_file
+
+    set msg "*staging*destroot*"
+    set line [get_line $path/$output_file $msg]
+    return $line
+}
+
+test envvariables {
+    Regression test for statefile-version1.
+} -body {
+    statefile_v1
+} -result "--->  staging statefile-version1 into destroot"
+
+
+cleanup
+cleanupTests
