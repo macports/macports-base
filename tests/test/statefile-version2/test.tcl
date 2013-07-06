b1eb4fdf9b51467d3da1f67857d7d054439d80ac
Marius Coțofană <marius@macports.org>
2013-07-06 17:20:52 +0000

    Fixed statefile-v2 and added second test case.
    git-svn-id: https://svn.macports.org/repository/macports/branches/gsoc13-tests@107830 d073be05-634f-4543-b044-5fe20cf6d1d6

--- tests/test/statefile-version2/test.tcl
+++ tests/test/statefile-version2/test.tcl
@@ -13,22 +13,35 @@ load_variables $path
 set_dir
 port_index
 port_config $path
+file copy -force $path/statefile $work_dir/.macports.statefile-version2.state
+exec sed -i'' -E "s/@CHECKSUM@/`openssl dgst -sha256 Portfile | \
+    awk '{print \$\$2}'`/" $work_dir/.macports.statefile-version2.state
 port_desroot $path
 port_clean $path
 
-proc statefile_v2 {} {
+proc statefile_v2 {arg} {
     global path
     global output_file
 
-    set msg "*staging*destroot*"
+    if {[string compare $arg "no"]} {
+        set msg "*discarding previous state*"
+    } else {
+        set msg "*staging*destroot*"
+    }
     set line [get_line $path/$output_file $msg]
     return $line
 }
 
+test statefile-v2-discard {
+    Regression test for statefile-version2 no discard.
+} -body {
+    statefile_v2 yes
+} -result "-1"
+
 test statefile-v2 {
     Regression test for statefile-version2.
 } -body {
-    statefile_v2
+    statefile_v2 no
 } -result "--->  staging statefile-version2 into destroot"
 
 
