4b4d8513876d19c93044e44a8b1ae7ee84835dc4
Marius Coțofană <marius@macports.org>
2013-07-08 11:27:13 +0000

    Added color, formatted output and help proc.
    git-svn-id: https://svn.macports.org/repository/macports/branches/gsoc13-tests@107883 d073be05-634f-4543-b044-5fe20cf6d1d6

--- tests/test.tcl
+++ tests/test.tcl
@@ -24,21 +24,28 @@ set test_name ""
 
 proc print_help {arg} {
     if { $arg == "tests" } {
-        puts "test list"
+        puts "The list of abailable tests is:"
+        foreach test $::test_suite {
+            puts [puts -nonewline "  "]$test
+        }
     } else {
-        puts "help message"
+        puts "Usage: tclsh test.tcl \[-debug level\] \[-t test\] \[-l\]\n"
+        puts "  -debug LVL : sets the level of printed debug info \[0-3\]"
+        puts "  -t TEST    : run a specific test"
+        puts "  -l         : print the list of available tests"
+        puts "  -h, -help  : print this message\n"
     }
 }
 
 # Process args
 foreach arg $argv {
     if { $arg == "-h" || $arg == "-help" } {
-        print_help
+        print_help ""
         exit 0
     } elseif { $arg == "-debug" } {
         set index [expr [lsearch $argv $arg] + 1]
         set level [lindex $argv $index]
-        if { $level >= 0 && $level <= 5 } {
+        if { $level >= 0 && $level <= 3 } {
             append arguments "-debug " $level
         } else {
             puts "Invalid debug level."
@@ -57,8 +64,12 @@ foreach arg $argv {
             print_help tests
             exit 1
         }
-    }       
+    } elseif { $arg == "-l" } {
+        print_help tests
+        exit 0
+    }
 }
+
 
 # Run tests
 if { $test_name != ""} {
@@ -72,7 +83,18 @@ if { $test_name != ""} {
         cd test/$test
     
         set result [eval exec tclsh test.tcl $arguments]
-        puts $result
+        set total [lrange [split $result "\t"] 2 2]
+        set pass [lrange [split $result "\t"] 4 4]
+        set fail [lrange [split $result "\t"] 8 8]
+
+        set out ""
+        if { $fail != 0 } {
+            # Color failed tests.
+            append out "\x1b\[1;31mTotal:" $total " Passed:" $pass " Failed:" $fail "  \x1b\[0m" $test
+        } else {
+            append out "Total:" $total " Passed:" $pass " Failed:" $fail "  " $test
+        }
+        puts $out
     
         cd ../..
     }
