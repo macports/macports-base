248988f40c5badfc4fb728c59ceb65eb9d41b7f4
Marius Coțofană <marius@macports.org>
2013-07-09 16:56:42 +0000

    trace test: added working version.
    git-svn-id: https://svn.macports.org/repository/macports/branches/gsoc13-tests@107930 d073be05-634f-4543-b044-5fe20cf6d1d6

--- tests/test/trace/test.tcl
+++ tests/test/trace/test.tcl
@@ -3,38 +3,55 @@ namespace import tcltest::*
 
 source [file dirname $argv0]/../library.tcl
 
-#makeFile "" $output_file
+makeFile "" $output_file
 makeDirectory $work_dir
 set path [file dirname [file normalize $argv0]]
 
+load_variables $path
+
 proc test_trace {} {
     global path
+    global autoconf
+    global output_file
 
-    load_variables $path
+    set line [get_line $autoconf "runusr*"]
+    set user [lrange [split $line " "] 1 1]
+
     set_dir
     port_index
     port_clean $path
 
-    exec mkdir ../tracetesttmp
-    exec chown macports ../tracetesttmp
-    exec sudo -u macports touch  ../tracetesttmp/delete-trace
-    exec sudo -u macports touch ../tracetesttmp/rename-trace
-    exec sudo -u macports mkdir ../tracetesttmp/rmdir-trace
+
+    makeDirectory ../tracetesttmp
+    exec chown $user ../tracetesttmp
+    exec sudo -u $user touch  ../tracetesttmp/delete-trace
+    exec sudo -u $user touch ../tracetesttmp/rename-trace
+    exec sudo -u $user mkdir ../tracetesttmp/rmdir-trace
     file delete -force /tmp/hello-trace
     file link -symbolic /tmp/link-trace2 /usr/include/unistd.h
-    exec chown -h macports /tmp/link-trace2
+    exec chown -h $user /tmp/link-trace2
 
     port_trace $path
     
     file delete -force /tmp/link-trace2
     file delete -force /tmp/hello-trace
+
+    set err "error*"
+    set line [get_line $path/$output_file $err]
+    if { $line == -1 } {
+        return "No errors found."
+    } else {
+        return $line
+    }
 }
 
 test trace {
     Regression test for trace.
+} -constraints {
+    root
 } -body {
     test_trace
-} -result ""
+} -result "No errors found."
 
 
 cleanup
