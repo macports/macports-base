9f7d4d8613ad6a145993f69f4ad2916467da5431
Lawrence Velázquez <larryv@macports.org>
2013-09-14 01:12:32 +0000

    portconfigure.tcl: Add extra level of quoting. D'oh.
    git-svn-id: https://svn.macports.org/repository/macports/trunk/base@111054 d073be05-634f-4543-b044-5fe20cf6d1d6

--- src/port1.0/portconfigure.tcl
+++ src/port1.0/portconfigure.tcl
@@ -51,11 +51,11 @@ default configure.cmd       ./configure
 default configure.nice      {${buildnicevalue}}
 default configure.dir       {${worksrcpath}}
 default autoreconf.dir      {${worksrcpath}}
-default autoreconf.pre_args {-vi}
+default autoreconf.pre_args {{--install --verbose}}
 default autoconf.dir        {${worksrcpath}}
-default autoconf.pre_args   {-v}
+default autoconf.pre_args   {--verbose}
 default automake.dir        {${worksrcpath}}
-default automake.pre_args   {-v}
+default automake.pre_args   {--verbose}
 default xmkmf.cmd           xmkmf
 default xmkmf.dir           {${worksrcpath}}
 default use_configure       yes
