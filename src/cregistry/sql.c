b276777755b4f7d041fbaf062eb785dda99f7dfb
Joshua Root <jmr@macports.org>
2014-02-25 20:31:16 +0000

    re-revert reverted reversion
    git-svn-id: https://svn.macports.org/repository/macports/trunk/base@117414 d073be05-634f-4543-b044-5fe20cf6d1d6

--- src/cregistry/sql.c
+++ src/cregistry/sql.c
@@ -264,7 +264,7 @@ int update_db(sqlite3* db, reg_error* errPtr) {
 
         /* we can't call vercmp directly because it's static, but we have
          * sql_version, which is basically an alias */
-        if (sql_version(NULL, -1, version, -1, "1.1") < 0) {
+        if (sql_version(NULL, -1, version, -1, "1.100") < 0) {
             /* we need to update to 1.1, add binary field and index to files
              * table */
             static char* version_1_1_queries[] = {
