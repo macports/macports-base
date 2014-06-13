0e38d48da7d8226e1ef193946802d79ad2d75ec0
Shashwat Pandey <shasha@macports.org>
2014-06-13 16:51:06 +0000

    First attempt at interaction
    git-svn-id: https://svn.macports.org/repository/macports/branches/gsoc14-interactive/base@120983 d073be05-634f-4543-b044-5fe20cf6d1d6

--- src/macports1.0/macports.tcl
+++ src/macports1.0/macports.tcl
@@ -4614,6 +4614,15 @@ proc macports::revupgrade_scanandrebuild {broken_port_counts_name opts} {
         }
         set broken_ports [lsort -unique $broken_ports]
 
+		##
+		# User Interaction Question
+		# Asking before rebuilding in rev-upgrade
+		ui_msg "You can always run 'port rev-upgrade' again to fix errors."
+		set retvalue [ui_ask_yesno "The following ports will be rebuilt:" "TestCase#1" $broken_ports {y} 0]
+		if {retvalue == 1} {
+			set $macports::revupgrade_mode dontrebuild
+		}
+		
         if {$macports::revupgrade_mode eq {rebuild}} {
             # don't try to rebuild ports that don't exist in the tree
             set temp_broken_ports {}
