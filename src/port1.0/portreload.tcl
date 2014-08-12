aebf002f6df249f82b2ad357e69c1fcd804f8e38
Clemens Lang <cal@macports.org>
2014-08-12 11:12:29 +0000

    base: portreload.tcl: use standard modeline, update copyright
    https://lists.macosforge.org/pipermail/macports-dev/2014-August/027588.html
    
    git-svn-id: https://svn.macports.org/repository/macports/trunk/base@123675 d073be05-634f-4543-b044-5fe20cf6d1d6

--- src/port1.0/portreload.tcl
+++ src/port1.0/portreload.tcl
@@ -1,9 +1,7 @@
-# et:ts=4
-# portreload.tcl
+# -*- coding: utf-8; mode: tcl; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- vim:fenc=utf-8:ft=tcl:et:sw=4:ts=4:sts=4
 # $Id$
 #
-# Copyright (c) 2007, 2009, 2011 The MacPorts Project
-# Copyright (c) 2007 James D. Berry
+# Copyright (c) 2014 The MacPorts Project
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
