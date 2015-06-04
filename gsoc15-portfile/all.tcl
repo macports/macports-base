f0290af06454cd1bd32a0ddcd0331bd5d87b0c80
Chunyang Xu <chunyang@macports.org>
2015-06-04 14:57:45 +0000

    port-create: Extract name and version from URL
    git-svn-id: https://svn.macports.org/repository/macports/branches/gsoc15-portfile@137089 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ gsoc15-portfile/all.tcl
@@ -0,0 +1,3 @@
+package require tcltest
+namespace import ::tcltest::*
+runAllTests
--- gsoc15-portfile/port-create.tcl
+++ gsoc15-portfile/port-create.tcl
@@ -8,8 +8,38 @@
 # 2. Feed template
 # 3. Print result
 
-set fp [open "Portfile.template" r]
-set template [read $fp]
-close $fp
+# Get tarball filename from tarball URL
+proc get_tarball_filename {url} {
+    set length [string length $url]
+    set last [string last "/" $url]
+    incr last;                  # Discard prefix "/"
+    return [string range $url $last $length]
+}
 
-puts $template
+# Get name from tarball filename
+proc get_name {tarball} {
+    set last_dash [string last "-" $tarball]
+    incr last_dash -1;          # Discard suffix "-"
+    return [string range $tarball 0 $last_dash]
+}
+
+# Get version from tarball filename
+proc get_version {tarball} {
+    set rtv [regexp {(\d+\.)?(\d+\.)?(\*|\d+)} $tarball match]
+    if {$rtv == 1} {
+        return $match
+    } else {
+        return 0
+    }
+}
+
+proc read_template {file} {
+    set fp [open $file r]
+    set template [read $fp]
+    close $fp
+    return $template
+}
+
+proc feed_template {template metadata} {
+    return $template
+}
