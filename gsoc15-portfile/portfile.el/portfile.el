ff866500aa12a899ad919532a6ad652ddee24326
Chunyang Xu <chunyang@macports.org>
2015-06-08 19:20:49 +0000

    portfile.el: Initial commit
    git-svn-id: https://svn.macports.org/repository/macports/branches/gsoc15-portfile@137305 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ gsoc15-portfile/portfile.el/portfile.el
@@ -0,0 +1,18 @@
+;;; portfile.el --- Major mode for editing MacPorts Portfiles  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2015  Chunyang Xu
+
+;; Author: Chunyang Xu <chunyang@macports.org>
+;; URL: https://trac.macports.org/browser/branches/gsoc15-portfile/portfile.el/
+;; Version: 0.0.1
+;; Created: Tue Jun  9 03:14:33 CST 2015
+;; Keywords: MacPorts Portfile
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(provide 'portfile)
+;;; portfile.el ends here
