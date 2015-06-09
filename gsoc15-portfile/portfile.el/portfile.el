d7ab4d9d620e4f133863b23f5f728ef7c35a11b9
Chunyang Xu <chunyang@macports.org>
2015-06-09 13:13:38 +0000

    portfile.el: Highlight some keywords
    git-svn-id: https://svn.macports.org/repository/macports/branches/gsoc15-portfile@137332 d073be05-634f-4543-b044-5fe20cf6d1d6

--- gsoc15-portfile/portfile.el/portfile.el
+++ gsoc15-portfile/portfile.el/portfile.el
@@ -14,5 +14,38 @@
 
 ;;; Code:
 
+(defvar portfile-required-list
+  '("PortSystem" "name" "version" "maintainers"
+    "homepage" "platforms"
+    "master_sites" "master_sites-append" "master_sites-delete"
+    "categories" "categories-append" "categories-delete"
+    "description" "description-append"
+    "long_description" "long_description-append"))
+
+(defvar portfile-optional-list
+  '("PortGroup" "epoch" "revision" "patch_sites"
+    "license" "conflicts" "license_noconflict"
+    "replaced_by" "supported_archs"
+    "distfiles" "distfiles-append" "distfiles-delete"
+    "distname" "dist_subdir" "worksrcdir"
+    "installs_libs" "nextgroup=PortfileYesNo" "skipwhite"
+    "depends_skip_archcheck" "depends_skip_archcheck-append" "depends_skip_archcheck-delete"
+    "checksums" "checksums-append" "checksums-delete"))
+
+(defvar portfile-mode-font-lock-keywords
+  (list (cons (concat "\\_<"
+                      (regexp-opt (append portfile-required-list
+                                          portfile-optional-list) t) "\\_>")
+              'font-lock-keyword-face)))
+
+(define-derived-mode portfile-mode tcl-mode "portfile-mode"
+  "Portfile mode"
+  (font-lock-add-keywords nil portfile-mode-font-lock-keywords))
+
+;; Test:
+;; (add-hook 'tcl-mode-hook (lambda ()
+;;                            (when (string-equal (buffer-name) "Portfile")
+;;                              (portfile-mode))))
+
 (provide 'portfile)
 ;;; portfile.el ends here
