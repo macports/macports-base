bfaec498c5a9775255ed1e96b28dbdaef0a2287c
Jackson Isaac <ijackson@macports.org>
2015-06-10 07:34:39 +0000

    Add option for libsolv 'l' Add description to doc/port.1.txt for libsolv option.
    git-svn-id: https://svn.macports.org/repository/macports/branches/gsoc15-dependency/base@137353 d073be05-634f-4543-b044-5fe20cf6d1d6

--- src/port/port.tcl
+++ src/port/port.tcl
@@ -4566,6 +4566,10 @@ proc parse_options { action ui_options_name global_options_name } {
                         }
                         break
                     }
+                    l {
+                      ## Option for libsolv
+                      set global_options(ports_depengine) "libsolv"
+                    }
                     default {
                         print_usage; exit 1
                     }
