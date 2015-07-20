99ea4e3a3d3a30a8344b6990eed5e32e8f7b21e4
Jackson Isaac <ijackson@macports.org>
2015-07-20 18:25:44 +0000

    Add installed port information to the $pool repo $repo_installed by reading registry::installed.
    git-svn-id: https://svn.macports.org/repository/macports/branches/gsoc15-dependency/base@138807 d073be05-634f-4543-b044-5fe20cf6d1d6

--- src/macports1.0/macports.tcl
+++ src/macports1.0/macports.tcl
@@ -2753,6 +2753,7 @@ proc mportsearch {pattern {case_sensitive yes} {matchstyle regexp} {field name}}
             # macports::libsolv::print
             set search_res [macports::libsolv::search $pattern \
             $case_sensitive $matchstyle $field]
+            # macports::libsolv::create_installed_pool
             return $search_res
         }
     } else {
