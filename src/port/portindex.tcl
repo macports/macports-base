1fa78a91b889fae106ef776bbfb8c22f4645068c
Rainer Müller <raimue@macports.org>
2016-04-08 00:01:31 +0000

    Write portgroups key from portinfo into PortIndex
    git-svn-id: https://svn.macports.org/repository/macports/branches/portgroups-selector/base@147561 d073be05-634f-4543-b044-5fe20cf6d1d6

--- src/port/portindex.tcl
+++ src/port/portindex.tcl
@@ -261,7 +261,8 @@ set save_prefix ${macports::prefix}
 foreach key {categories depends_fetch depends_extract depends_build \
              depends_lib depends_run depends_test description epoch homepage \
              long_description maintainers name platforms revision variants \
-             version portdir replaced_by license installs_libs conflicts} {
+             version portdir replaced_by license installs_libs conflicts \
+             portgroups} {
     set keepkeys($key) 1
 }
 
