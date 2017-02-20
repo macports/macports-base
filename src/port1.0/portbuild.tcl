macports/macports-base

commit 8e3dbde092090cff0e7884a830966cf464c9e19c

port1.0: Make portbuild signal-safe
Additionally, switch memory-computations in portbuild::build_getjobs to powers
of 1024, since that's what sysctl returns.

git-svn-id: https://svn.macports.org/repository/macports/trunk/base@146715 d073be05-634f-4543-b044-5fe20cf6d1d6

src/port1.0/portbuild.tcl
@@ -134,14 +134,20 @@ proc portbuild::build_getjobs {args} {
     set jobs $buildmakejobs
     # if set to '0', use the number of cores for the number of jobs
     if {$jobs == 0} {
-        if {[catch {set jobs [sysctl hw.activecpu]}] || [catch {set memsize [sysctl hw.memsize]}]} {
-            set jobs 2
+        try -pass_signal {
+            set jobs [sysctl hw.activecpu]
+        } catch {{*} eCode eMessage} {
             ui_warn "failed to determine the number of available CPUs (probably not supported on this platform)"
             ui_warn "defaulting to $jobs jobs, consider setting buildmakejobs to a nonzero value in macports.conf"
+            set jobs 2
         }
-        if {[info exists memsize] && $jobs > $memsize / 1000000000 + 1} {
-            set jobs [expr {$memsize / 1000000000 + 1}]
-        }
+
+        try -pass_signal {
+            set memsize [sysctl hw.memsize]
+            if {$jobs > $memsize / (1024 * 1024 * 1024) + 1} {
+                set jobs [expr {$memsize / (1024 * 1024 * 1024) + 1}]
+            }
+        } catch {*} {}
     }
     if {![string is integer -strict $jobs] || $jobs <= 1} {
         set jobs 1

commit 3d4e819e3bac8591d70b4089b27027b13fa85e5c

Fix bug introduced in 8e3dbde

src/port1.0/portbuild.tcl
@@ -136,9 +136,9 @@ proc portbuild::build_getjobs {args} {
         try -pass_signal {
             set jobs [sysctl hw.activecpu]
         } catch {{*} eCode eMessage} {
+            set jobs 2
             ui_warn "failed to determine the number of available CPUs (probably not supported on this platform)"
             ui_warn "defaulting to $jobs jobs, consider setting buildmakejobs to a nonzero value in macports.conf"
-            set jobs 2
         }
 
         try -pass_signal {