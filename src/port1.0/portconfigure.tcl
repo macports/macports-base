fe89e3fd10ec9565ebae724099c574522f2a474e
Jeremy Huddleston Sequoia <jeremyhu@macports.org>
2017-03-26 00:45:58 -0700

    portconfigure: Ignore errors from xcrun when searching for an SDK
    Signed-off-by: Jeremy Huddleston Sequoia <jeremyhu@macports.org>

--- src/port1.0/portconfigure.tcl
+++ src/port1.0/portconfigure.tcl
@@ -410,13 +410,11 @@ proc portconfigure::configure_get_sdkroot {sdk_version} {
         return $sdk
     }
 
-    set sdk [exec xcrun --sdk macosx${sdk_version} --show-sdk-path]
-    if {[file exists $sdk]} {
+    if {![catch {set sdk [exec xcrun --sdk macosx${sdk_version} --show-sdk-path 2> /dev/null]}]} {
         return $sdk
     }
 
-    set sdk [exec xcrun --sdk macosx --show-sdk-path]
-    if {[file exists $sdk]} {
+    if {![catch {set sdk [exec xcrun --sdk macosx --show-sdk-path 2> /dev/null]}]} {
         ui_warn "Unable to determine location of the macOS ${sdk_version} SDK.  Using the default macOS SDK."
         return $sdk
     }
