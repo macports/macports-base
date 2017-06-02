macports/macports-base

commit ccda02dadd4adf18613191c564731952f5f46428

Fix configure checks for frameworks
Remove reference to no longer existing OBJC_RUNTIME variable. The
previous conidition was also slightly incorrect, as it should check for
the availability of the linked frameworks.

aclocal.m4
@@ -334,7 +334,7 @@ AC_DEFUN(MP_CHECK_FRAMEWORK_COREFOUNDATION, [
 		AC_DEFINE([HAVE_FRAMEWORK_COREFOUNDATION], [], [Define if CoreFoundation framework is available])
 	fi
 
-	AC_SUBST(HAVE_FRAMEWORK_COREFOUNDATION)
+	AC_SUBST(HAVE_FRAMEWORK_COREFOUNDATION, [$mp_cv_have_framework_corefoundation])
 ])
 
 
@@ -448,7 +448,7 @@ AC_DEFUN(MP_CHECK_FRAMEWORK_SYSTEMCONFIGURATION, [
 		AC_DEFINE([HAVE_FRAMEWORK_SYSTEMCONFIGURATION], [], [Define if SystemConfiguration framework is available])
 	fi
 
-	AC_SUBST(HAVE_FRAMEWORK_SYSTEMCONFIGURATION)
+	AC_SUBST(HAVE_FRAMEWORK_SYSTEMCONFIGURATION, [$mp_cv_have_framework_systemconfiguration])
 ])
 
 
@@ -505,7 +505,7 @@ AC_DEFUN(MP_CHECK_FRAMEWORK_IOKIT, [
 		AC_DEFINE([HAVE_FRAMEWORK_IOKIT], [], [Define if IOKit framework is available])
 	fi
 
-	AC_SUBST(HAVE_FRAMEWORK_IOKIT)
+	AC_SUBST(HAVE_FRAMEWORK_IOKITi, [$mp_cv_have_framework_iokit])
 ])
 
 

configure
@@ -684,7 +684,7 @@ MPCONFIGDIR
 PORTSDIR
 EXTRA_PROGS
 HAVE_FUNCTION_CFNOTIFICATIONCENTERGETDARWINNOTIFYCENTER
-HAVE_FRAMEWORK_IOKIT
+HAVE_FRAMEWORK_IOKITi
 HAVE_FRAMEWORK_SYSTEMCONFIGURATION
 HAVE_FRAMEWORK_COREFOUNDATION
 CFLAGS_WERROR
@@ -6252,8 +6252,6 @@ $as_echo "" >&6; }
 
 
 
-ACX_PTHREAD
-
 
 	FRAMEWORK_LIBS="-framework CoreFoundation"
 
@@ -6312,6 +6310,7 @@ $as_echo "#define HAVE_FRAMEWORK_COREFOUNDATION /**/" >>confdefs.h
 
 	fi
 
+	HAVE_FRAMEWORK_COREFOUNDATION=$mp_cv_have_framework_corefoundation
 
 
 
@@ -6372,6 +6371,7 @@ $as_echo "#define HAVE_FRAMEWORK_SYSTEMCONFIGURATION /**/" >>confdefs.h
 
 	fi
 
+	HAVE_FRAMEWORK_SYSTEMCONFIGURATION=$mp_cv_have_framework_systemconfiguration
 
 
 
@@ -6432,6 +6432,7 @@ $as_echo "#define HAVE_FRAMEWORK_IOKIT /**/" >>confdefs.h
 
 	fi
 
+	HAVE_FRAMEWORK_IOKITi=$mp_cv_have_framework_iokit
 
 
 

src/macports1.0/Makefile.in
@@ -11,8 +11,10 @@ SHLIB_NAME=	MacPorts${SHLIB_SUFFIX}
 INSTALLDIR=	${DESTDIR}${TCL_PACKAGE_PATH}/macports1.0
 OLDINSTALLDIR= ${datadir}/macports/Tcl
 
-ifeq ($(OBJC_RUNTIME), APPLE_RUNTIME)
+ifeq (@HAVE_FRAMEWORK_COREFOUNDATION@,yes)
+ifeq (@HAVE_FRAMEWORK_SYSTEMCONFIGURATION@,yes)
 	LIBS+= -framework CoreFoundation -framework SystemConfiguration
+endif
 endif
 
 pkgIndex.tcl: $(SRCS)

commit e3a0dc2ebde62a9c5feac6a1edee1708a95bb02a

Fix typo in HAVE_FRAMEWORK_IOKIT
This typo was introduced in ccda02dadd4adf18613191c564731952f5f46428.

aclocal.m4
@@ -505,7 +505,7 @@ AC_DEFUN(MP_CHECK_FRAMEWORK_IOKIT, [
 		AC_DEFINE([HAVE_FRAMEWORK_IOKIT], [], [Define if IOKit framework is available])
 	fi
 
-	AC_SUBST(HAVE_FRAMEWORK_IOKITi, [$mp_cv_have_framework_iokit])
+	AC_SUBST(HAVE_FRAMEWORK_IOKIT, [$mp_cv_have_framework_iokit])
 ])
 
 

configure
@@ -684,7 +684,7 @@ MPCONFIGDIR
 PORTSDIR
 EXTRA_PROGS
 HAVE_FUNCTION_CFNOTIFICATIONCENTERGETDARWINNOTIFYCENTER
-HAVE_FRAMEWORK_IOKITi
+HAVE_FRAMEWORK_IOKIT
 HAVE_FRAMEWORK_SYSTEMCONFIGURATION
 HAVE_FRAMEWORK_COREFOUNDATION
 CFLAGS_WERROR
@@ -6432,7 +6432,7 @@ $as_echo "#define HAVE_FRAMEWORK_IOKIT /**/" >>confdefs.h
 
 	fi
 
-	HAVE_FRAMEWORK_IOKITi=$mp_cv_have_framework_iokit
+	HAVE_FRAMEWORK_IOKIT=$mp_cv_have_framework_iokit
 
 
 