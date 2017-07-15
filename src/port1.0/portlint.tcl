53e9a1b9fa20d9da161c768954bf1f02c8148dbb
Kurt Hindenburg <kurt.hindenburg@gmail.com>
2017-07-15 11:04:36 -0400

    portlint: add warning(s) about modeline
    closes https://trac.macports.org/ticket/52767

--- src/port1.0/portlint.tcl
+++ src/port1.0/portlint.tcl
@@ -197,9 +197,19 @@ proc portlint::lint_main {args} {
             incr warnings
         }
 
-        if {($lineno == $topline_number) && [string match "*-\*- *" $line]} {
-            ui_info "OK: Line $lineno has emacs/vim Mode"
-            incr topline_number
+        if {($lineno == $topline_number)} {
+            if {$line eq "# -*- coding: utf-8; mode: tcl; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- vim:fenc=utf-8:ft=tcl:et:sw=4:ts=4:sts=4"} {
+                ui_info "OK: Line $lineno has standard modeline"
+            } else {
+                ui_warn "Line $lineno should have the standard MacPort's modeline"
+                incr warnings
+            }
+        } else {
+            # Find any modelines not on top line
+            if {[string match "#\* -\*- coding:*" $line]} {
+                ui_warn "Line $lineno appears to have a modeline; only top line should have the standard MacPort's modeline"
+                incr warnings
+            }
         }
 
         if {[string match "*\$Id*\$" $line]} {
