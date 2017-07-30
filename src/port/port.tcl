00f5a9e755760103e0a7f55c55cadea859ef51d2
Kurt Hindenburg <kurt.hindenburg@gmail.com>
2017-07-30 19:50:45 -0400

    Add port provides option -q to only output port
    closes: https://trac.macports.org/ticket/38428

--- src/port/port.tcl
+++ src/port/port.tcl
@@ -2497,7 +2497,10 @@ proc action_provides { action portlist opts } {
             if {![file isdirectory $file] || [file type $file] eq "link"} {
                 set port [registry::file_registered $file]
                 if { $port != 0 } {
-                    puts "$file is provided by: $port"
+                    if {![macports::ui_isset ports_quiet]} {
+                        puts -nonewline "$file is provided by: "
+                    }
+                    puts $port
                 } else {
                     puts "$file is not provided by a MacPorts port."
                 }
