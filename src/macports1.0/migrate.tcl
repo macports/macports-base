56918a963e0f932955b502eecaa5490762be569b
Umesh Singla <umeshksingla@macports.org>
2017-08-28 03:44:22 +0530

    Deactivate ports before `restore`
    Move sorting and `recover_state` procedures to restore module
    and let migrate call the restore procedures.
    
    Also, instead of using other APIs to uninstall/deactivate which
    again sort the dependencies, directly make them use the API
    registry::run_target, since this runs fetching dependencies only
    once.

--- src/macports1.0/migrate.tcl
+++ src/macports1.0/migrate.tcl
@@ -34,6 +34,7 @@ package require macports 1.0
 package require registry 1.0
 package require Pextlib 1.0
 package require snapshot 1.0
+package require restore 1.0
 package require registry_uninstall 2.0
 
 namespace eval migrate {
@@ -51,275 +52,42 @@ namespace eval migrate {
         # create a snapshot
         ui_msg "$macports::ui_prefix Taking a snapshot of the current state.."
         set snapshot [snapshot::main $opts]
+        set id [$snapshot id]
         set note [$snapshot note]
         set datetime [$snapshot created_at]
-        ui_msg "$macports::ui_prefix Done: snapshot '$note' created at $datetime"
+        ui_msg "$macports::ui_prefix Done: snapshot '$id':'$note' created at $datetime"
 
-        ui_msg "$macports::ui_prefix Fetching ports.."
-        set portlist [$snapshot ports]
+        if {[info exists macports::ui_options(questions_yesno)]} {
+            set msg "Migration will first uninstall all the installed ports and then reinstall."
+            set retvalue [$macports::ui_options(questions_yesno) $msg "MigrationPrompt" "" {y} 0 "Would you like to continue?"]
+            if {$retvalue == 0} {
+                ui_msg "$macports::ui_prefix Uninstalling all ports.."
+                uninstall_installed [registry::entry imaged]
+            } else {
+                ui_msg "Not uninstalling ports."
+                return 0
+            }
+        }
 
-        ui_msg "$macports::ui_prefix Uninstalling all ports.."
-        uninstall_installed [registry::entry imaged]
+        ui_msg "$macports::ui_prefix Fetching ports to install.."
+        set snapshot_portlist [$snapshot ports]
 
         ui_msg "$macports::ui_prefix Restoring the original state.."
-        recover_ports_state $portlist
+        restore::restore_state $snapshot_portlist
 
         # TODO: CLEAN PARTIAL BUILDS STEP HERE
-    }
-
-    proc portlist_sort_dependencies_first {portlist} {
-
-        # Sorts a list of port references such that ports appear after
-        # their dependencies. Ideal for installing a port.
-        #
-        # Args:
-        #       portlist - the list of port references
-        #
-        # Returns:
-        #       the list in dependency-sorted order
-
-        array set port_installed {}
-        array set port_deps {}
-        array set port_in_list {}
-
-        set new_list [list]
-
-        foreach port $portlist {
-
-            set name [lindex $port 0]
-            set requested [lindex $port 1]
-            if {$requested eq 0} {
-                continue
-            }
-            set active 0
-            if {[lindex $port 2] eq "installed"} {
-                set active 1
-            }
-            set variantstr [lindex $port 3]
-            if {$variantstr eq "(null)"} {
-                set variantstr ""
-            }
-            set variants ""
-            if {[info exists variantstr]} {
-                while 1 {
-                    set nextplus [string last + $variantstr]
-                    set nextminus [string last - $variantstr]
-                    if {$nextplus > $nextminus} {
-                        set next $nextplus
-                        set sign +
-                    } else {
-                        set next $nextminus
-                        set sign -
-                    }
-                    if {$next == -1} {
-                        break
-                    }
-                    set v [string range $variantstr [expr $next + 1] end]
-                    lappend variants $v $sign
-                    set variantstr [string range $variantstr 0 [expr $next - 1]]
-                }
-            }
-            if {![info exists port_in_list($name)]} {
-                set port_in_list($name) 1
-                set port_installed($name) 0
-            } else {
-                incr port_in_list($name)
-            }
-
-            if {![info exists port_deps(${name},${variants})]} {
-                set port_deps(${name},${variants}) [portlist_sort_dependencies_first_helper $name $variants]
-            }
-            lappend new_list [list $name $variants $active]
-        }
-
-        set operation_list [list]
-        while {[llength $new_list] > 0} {
-
-            set oldLen [llength $new_list]
-            foreach port $new_list {
-                foreach {name variants active} $port break
-
-                if {$active && $port_installed($name) < ($port_in_list($name) - 1)} {
-                    continue
-                }
-                set installable 1
-                foreach dep $port_deps(${name},${variants}) {
-                    if {[info exists port_installed($dep)] && $port_installed($dep) == 0} {
-                        set installable 0
-                        break
-                    }
-                }
-                if {$installable} {
-                    lappend operation_list [list $name $variants $active]
-                    incr port_installed($name)
-                    set index [lsearch $new_list [list $name $variants $active]]
-                    set new_list [lreplace $new_list $index $index]
-                }
-            }
-            if {[llength $new_list] == $oldLen} {
-                return -code error "Stuck in loop"
-            }
-        }
-        return $operation_list
-    }
-
-    proc portlist_sort_dependencies_first_helper {portname variant_info} {
-        set dependency_list [list]
-        set port_search_result [mportlookup $portname]
-        if {[llength $port_search_result] < 2} {
-            ui_warn "Skipping $portname (not in the ports tree)"
-            return $dependency_list
-        }
-        array set portinfo [lindex $port_search_result 1]
-        if {[catch {set mport [mportopen $portinfo(porturl) [list subport $portinfo(name)] $variant_info]} result]} {
-            global errorInfo
-            puts stderr "$errorInfo"
-            return -code error "Unable to open port '$portname': $result"
-        }
-        array unset portinfo
-        array set portinfo [mportinfo $mport]
-        mportclose $mport
-        set dependency_types { depends_fetch depends_extract depends_build depends_lib depends_run }
-        foreach dependency_type $dependency_types {
-            if {[info exists portinfo($dependency_type)] && [string length $portinfo($dependency_type)] > 0} {
-                foreach dependency $portinfo($dependency_type) {
-                    lappend dependency_list [lindex [split $dependency:] end]
-                }
-            }
-        }
-        return $dependency_list
-    }
-
-    proc portlist_sort_dependencies_later {portlist} {
-
-        # Sorts a list of port references such that ports come before
-        # their dependencies. Ideal for uninstalling a port.
-        #
-        # Args:
-        #       portlist - the list of port references
-        #
-        # Returns:
-        #       the list in dependency-sorted order
-
-        foreach port $portlist {
-            set portname [$port name]
-            lappend entries($portname) $port
-            # Avoid adding ports in loop
-            if {![info exists dependents($portname)]} {
-                set dependents($portname) {}
-                foreach result [$port dependents] {
-                    lappend dependents($portname) [$result name]
-                }
-            }
-        }
-        set ret {}
-        foreach port $portlist {
-            portlist_sort_dependencies_later_helper $port entries dependents seen ret
-        }
-        return $ret
-    }
-
-    proc portlist_sort_dependencies_later_helper {port up_entries up_dependents up_seen up_retlist} {
-        upvar 1 $up_seen seen
-        if {![info exists seen($port)]} {
-            set seen($port) 1
-            upvar 1 $up_entries entries $up_dependents dependents $up_retlist retlist
-            set name [$port name]
-            foreach dependent $dependents($name) {
-                if {[info exists entries($dependent)]} {
-                    foreach entry $entries($dependent) {
-                        portlist_sort_dependencies_later_helper $entry entries dependents seen retlist
-                    }
-                }
-            }
-            lappend retlist $port
-        }
+        return 1
     }
 
     proc uninstall_installed {portlist} {
-
-        set portlist [portlist_sort_dependencies_later $portlist]
-
-        if {[info exists macports::ui_options(questions_yesno)]} {
-
-            set msg "Migration will first uninstall all the installed ports."
-            set retvalue [$macports::ui_options(questions_yesno) $msg "MigrationPrompt" "" {y} 0 "Would you like to continue?"]
-
-            if {$retvalue == 0} {
-                foreach port $portlist {
-                    set portname [$port name]
-                    ui_msg "Uninstalling: $portname"
-
-                    try -pass_signal {
-                        # 'registry_uninstall' takes name, version, revision, variants and an options list for a port
-                        registry_uninstall::uninstall [$port name] [$port version] [$port revision] [$port variants] {}
-                    } catch {{*} eCode eMessage} {
-                        ui_error "Error uninstalling $portname: $eMessage"
-                    }
-                }
-            } else {
-                ui_msg "Not uninstalling ports."
-                return 1
-            }
-        }
-        return 0
-    }
-
-    proc recover_ports_state {portlist} {
-
-        set sorted_portlist [portlist_sort_dependencies_first $portlist]
-        foreach port $sorted_portlist {
-            puts "$port"
-        }
-
-        puts "MacPorts Version: [macports::version]"
-        foreach port $sorted_portlist {
-            
-            set name [string trim [lindex $port 0]]
-            set variations [lindex $port 1]
-            set active [lindex $port 2]
-
-            if {!$active} {
-                set target install
-                ui_msg "Installing (not activating): $name $variations"
-            } else {
-                set target activate
-                ui_msg "Installing (and activating): $name $variations"
-            }
-
-            if {[catch {set res [mportlookup $name]} result]} {
-                global errorInfo
-                ui_debug "$errorInfo"
-                return -code error "lookup of port $name failed: $result"
-            }
-            if {[llength $res] < 2} {
-                # not in the index, but we already warned about that earlier
+        set portlist [portlist_sort_dependencies_later [registry::entry imaged]]
+        foreach port $portlist {
+            ui_msg "Uninstalling: [$port name]"
+            if {[registry::run_target $port uninstall]} {
                 continue
-            }
-
-            array unset portinfo
-            array set portinfo [lindex $res 1]
-            set porturl $portinfo(porturl)
-
-            set options(ports_requested) 1
-            set options(subport) $portinfo(name)
-            
-            if {[catch {set workername [mportopen $porturl [array get options] $variations]} result]} {
-                global errorInfo
-                puts stderr "$errorInfo"
-                return -code error "Unable to open port '$name': $result"
-            }
-
-            if {[catch {set result [mportexec $workername $target]} result]} {
-                global errorInfo
-                mportclose $workername
-                ui_msg "$errorInfo"
-                return -code error "Unable to execute target 'install' for port '$name': $result"
             } else {
-                mportclose $workername
+                ui_error "Error uninstalling [$port name]"
             }
-            # TODO: some ports may get re-activated to fulfil dependencies - recheck?
         }
-
     }
 }
