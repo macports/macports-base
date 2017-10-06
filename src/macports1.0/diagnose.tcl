587b720759fb2c5728904a42a150bbe66dc7c555
Rainer Müller <raimue@macports.org>
2017-10-06 18:09:04 +0200

    diagnose: Rework fixing PATH in shell profile

--- src/macports1.0/diagnose.tcl
+++ src/macports1.0/diagnose.tcl
@@ -701,20 +701,26 @@ namespace eval diagnose {
 
             # XXX Only works for bash. Should set default profile_path based on the shell.
             if {[info exists macports::ui_options(questions_yesno)] && $shell_name eq "bash"} {
-                set retval [$macports::ui_options(questions_yesno) "" "DiagnoseFixPATH" "" n 0 "Would you like to add $port_loc/bin to your \$PATH variable now?"]
+                ui_msg "MacPorts can also write the required configuration to $profile_path for you."
+                set question "Would you like to add $port_loc/bin to your \$PATH variable now?"
+                set retval [$macports::ui_options(questions_yesno) $msg "DiagnoseFixPATH" "" n 0 $question]
                 if {$retval == 0} {
                     # XXX: this should use the same paths and comments as the
                     # postflight script of the pkg installer. Maybe they could even
                     # share code?
-                    ui_msg "Attempting to add $port_loc/bin to $profile_path"
+                    ui_debug "Attempting to add $port_loc/bin to $profile_path"
 
                     if {[file exists $profile_path]} {
                         if {[file writable $profile_path]} {
+                            # XXX: Should keep a backup like postflight script does
                             set fd [open $profile_path a]
+                            puts $fd ""
+                            puts $fd "# MacPorts diagnose addition on [clock format $date -format "%Y-%m-%dT%H:%M:%S%z"]"
                             puts $fd "export PATH=$port_loc/bin:$port_loc/sbin:\$PATH"
+                            puts $fd ""
                             close $fd
 
-                            ui_msg "Added PATH properly. Please open a new terminal window to load the modified ${profile_path}."
+                            ui_msg "Added PATH environment variable to your $shell_name configuration. It is important that you now close this terminal window and then open a new terminal window to load the modified environment from ${profile_path}."
                         } else {
                             ui_error "Can't write to ${profile_path}."
                         }
