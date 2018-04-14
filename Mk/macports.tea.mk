208bd884450474fab2fa7842fe8c70bdb0a686af
Rainer Müller <raimue@macports.org>
2018-04-14 13:08:15 +0200

    Mk: Record header dependencies in *.d
    When changing a header, this automatically re-makes the object targets
    for the source files that include this header.

--- Mk/macports.tea.mk
+++ Mk/macports.tea.mk
@@ -1,9 +1,13 @@
-%.o: %.c
-	${CC} -c -DUSE_TCL_STUBS -DTCL_NO_DEPRECATED ${CFLAGS} ${CPPFLAGS} ${SHLIB_CFLAGS} $< -o $@
-
 PKGINDEX ?= pkgIndex.tcl
 
 all:: ${SHLIB_NAME} ${PKGINDEX}
+
+# OBJS are *.o produced from *.c with header dependencies in *.d
+OBJSDEP = ${OBJS:.o=.d}
+-include ${OBJSDEP}
+
+%.o: %.c
+	${CC} -c -DUSE_TCL_STUBS -DTCL_NO_DEPRECATED ${CFLAGS} ${CPPFLAGS} ${SHLIB_CFLAGS} -MMD $< -o $@
 
 $(SHLIB_NAME): ${OBJS}
 	${SHLIB_LD} ${OBJS} -o ${SHLIB_NAME} ${TCL_STUB_LIB_SPEC} ${SHLIB_LDFLAGS} ${LIBS}
@@ -12,7 +16,7 @@ pkgIndex.tcl: $(SHLIB_NAME)
 	$(SILENT) ../pkg_mkindex.sh . || ( rm -rf $@ && exit 1 )
 
 clean::
-	rm -f ${OBJS} ${SHLIB_NAME} so_locations pkgIndex.tcl
+	rm -f ${OBJS} ${OBJSDEP} ${SHLIB_NAME} so_locations pkgIndex.tcl
 
 distclean:: clean
 
