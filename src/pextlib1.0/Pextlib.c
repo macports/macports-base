fa0249d6a779a95209b05c7b590de37c1b76de0d
Rainer Müller <raimue@macports.org>
2018-04-14 11:47:22 +0200

    pextlib: Remove dead code for base32
    This is not used anywhere in base.

--- src/pextlib1.0/Pextlib.c
+++ src/pextlib1.0/Pextlib.c
@@ -83,7 +83,6 @@
 #include "sha1cmd.h"
 #include "rmd160cmd.h"
 #include "sha256cmd.h"
-#include "base32cmd.h"
 #include "fs-traverse.h"
 #include "filemap.h"
 #include "curl.h"
@@ -1091,8 +1090,6 @@ int Pextlib_Init(Tcl_Interp *interp)
 	Tcl_CreateObjCommand(interp, "vercmp", VercompCmd, NULL, NULL);
 	Tcl_CreateObjCommand(interp, "rmd160", RMD160Cmd, NULL, NULL);
 	Tcl_CreateObjCommand(interp, "sha256", SHA256Cmd, NULL, NULL);
-	Tcl_CreateObjCommand(interp, "base32encode", Base32EncodeCmd, NULL, NULL);
-	Tcl_CreateObjCommand(interp, "base32decode", Base32DecodeCmd, NULL, NULL);
 	Tcl_CreateObjCommand(interp, "sha1", SHA1Cmd, NULL, NULL);
 	Tcl_CreateObjCommand(interp, "umask", UmaskCmd, NULL, NULL);
 	Tcl_CreateObjCommand(interp, "pipe", PipeCmd, NULL, NULL);
