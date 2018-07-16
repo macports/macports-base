macports/macports-base

commit a9579836b7b31158c1b3a67c4c5a7d9ffdbf572f

port1.0: Include non-default SDK in trace sandbox.

src/port1.0/porttrace.tcl
@@ -131,7 +131,7 @@ namespace eval porttrace {
     # @param workpath The $workpath of the current installation
     proc trace_start {workpath} {
         global \
-            developer_dir distpath env macportsuser os.platform \
+            developer_dir distpath env macportsuser os.platform configure.sdkroot \
             portpath prefix
 
         variable fifo
@@ -206,6 +206,9 @@ namespace eval porttrace {
                 allow trace_sandbox $tmpdir
             }
         }
+
+        # Allow access to SDK if it's not inside the Developer folder.
+        allow trace_sandbox "${configure.sdkroot}"
 
         # Allow access to some Xcode specifics
         allow trace_sandbox "/var/db/xcode_select_link"

commit edff5a86e5bda0ef1e4ae410ae89e2d2b20954c8

port1.0: Fix tracing with empty configure.sdkroot
a9579836 introduced adding $configure.sdkroot to the trace exception
list for users that set it manually to a path outside of
/Applications/Xcode.app. However, in case this does not end up being
set, it adds an empty entry to the trace mode sandbox, which completely
breaks it.

Fix it by only adding the path if it is not empty.

src/port1.0/porttrace.tcl
@@ -208,7 +208,9 @@ namespace eval porttrace {
         }
 
         # Allow access to SDK if it's not inside the Developer folder.
-        allow trace_sandbox "${configure.sdkroot}"
+        if {${configure.sdkroot} ne ""} {
+            allow trace_sandbox "${configure.sdkroot}"
+        }
 
         # Allow access to some Xcode specifics
         allow trace_sandbox "/var/db/xcode_select_link"