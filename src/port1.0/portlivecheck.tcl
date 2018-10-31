macports/macports-base

commit 84f97ca201c6705d0b8e4d73e8566b76c7ff2a3c

pextlib       : Support custom HTTP headers
portlivecheck : Add new livecheck.curloptions option. Default is :-
{{ "--append-http-header"
   "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8" }}

src/port1.0/portlivecheck.tcl
@@ -45,7 +45,7 @@ namespace eval portlivecheck {
 }
 
 # define options
-options livecheck.url livecheck.type livecheck.md5 livecheck.regex livecheck.name livecheck.distname livecheck.version livecheck.ignore_sslcert
+options livecheck.url livecheck.type livecheck.md5 livecheck.regex livecheck.name livecheck.distname livecheck.version livecheck.ignore_sslcert livecheck.curloptions
 
 # defaults
 default livecheck.url {$homepage}
@@ -56,10 +56,12 @@ default livecheck.name default
 default livecheck.distname default
 default livecheck.version {$version}
 default livecheck.ignore_sslcert no
+default livecheck.curloptions {{ "--append-http-header" "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8" }}
 
 proc portlivecheck::livecheck_main {args} {
     global livecheck.url livecheck.type livecheck.md5 livecheck.regex livecheck.name livecheck.distname livecheck.version \
            livecheck.ignore_sslcert \
+           livecheck.curloptions \
            homepage portpath workpath \
            master_sites name subport distfiles
 
@@ -75,7 +77,7 @@ proc portlivecheck::livecheck_main {args} {
 
     ui_debug "Port (livecheck) version is ${livecheck.version}"
 
-    set curl_options {}
+    set curl_options ${livecheck.curloptions}
     if {[tbool livecheck.ignore_sslcert]} {
         lappend curl_options "--ignore-ssl-cert"
     }
@@ -144,6 +146,7 @@ proc portlivecheck::livecheck_main {args} {
         "regexm" {
             # single and multiline regex
             ui_debug "Fetching ${livecheck.url}"
+            ui_debug "Using CURL options ${curl_options}"
             set updated -1
             if {[catch {curl fetch {*}$curl_options ${livecheck.url} $tempfile} error]} {
                 ui_error "cannot check if $subport was updated ($error)"

src/pextlib1.0/curl.c
@@ -178,6 +178,9 @@ CurlFetchCmd(Tcl_Interp* interp, int objc, Tcl_Obj* CONST objv[])
 		};
 		char* effectiveURL = NULL;
 		char* userAgent = PACKAGE_NAME "/" PACKAGE_VERSION " libcurl/" LIBCURL_VERSION;
+		const int MAXHTTPHEADERS = 100;
+		int numHTTPHeaders = 0;
+		const char* httpHeaders[MAXHTTPHEADERS];
 		int optioncrsr;
 		int lastoption;
 		const char* theURL;
@@ -236,6 +239,26 @@ CurlFetchCmd(Tcl_Interp* interp, int objc, Tcl_Obj* CONST objv[])
 				} else {
 					Tcl_SetResult(interp,
 						"curl fetch: --user-agent option requires a parameter",
+						TCL_STATIC);
+					theResult = TCL_ERROR;
+					break;
+				}
+			} else if (strcmp(theOption, "--append-http-header") == 0) {
+				/* check we also have the parameter */
+				if (optioncrsr < lastoption) {
+					optioncrsr++;
+					if ( numHTTPHeaders < MAXHTTPHEADERS ) {
+					  httpHeaders[numHTTPHeaders++] = Tcl_GetString(objv[optioncrsr]);
+					} else {
+					  Tcl_SetResult(interp,
+					    "curl fetch: Too many --append-http-header options",
+					    TCL_STATIC);
+					  theResult = TCL_ERROR;
+					  break;
+					}
+				} else {
+					Tcl_SetResult(interp,
+						"curl fetch: --append-http-header option requires a parameter",
 						TCL_STATIC);
 					theResult = TCL_ERROR;
 					break;
@@ -466,6 +489,10 @@ CurlFetchCmd(Tcl_Interp* interp, int objc, Tcl_Obj* CONST objv[])
 
 		/* Clear the Pragma: no-cache header */
 		headers = curl_slist_append(headers, "Pragma:");
+		/* Append any optional headers */
+ 		for ( int iH = 0; iH < numHTTPHeaders; ++iH ) {
+		  headers = curl_slist_append(headers, httpHeaders[iH]);
+		}
 		theCurlCode = curl_easy_setopt(theHandle, CURLOPT_HTTPHEADER, headers);
 		if (theCurlCode != CURLE_OK) {
 			theResult = SetResultFromCurlErrorCode(interp, theCurlCode);

commit 7dc85e69e89c63c4d3510a1d40addfcb55e2af39

unbreak livecheck
Broken by 84f97ca.

src/port1.0/portlivecheck.tcl
@@ -56,7 +56,7 @@ default livecheck.name default
 default livecheck.distname default
 default livecheck.version {$version}
 default livecheck.ignore_sslcert no
-default livecheck.curloptions {{ "--append-http-header" "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8" }}
+default livecheck.curloptions {"--append-http-header" "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"}
 
 proc portlivecheck::livecheck_main {args} {
     global livecheck.url livecheck.type livecheck.md5 livecheck.regex livecheck.name livecheck.distname livecheck.version \