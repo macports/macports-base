3b0046ab75546fee3fbf7b635fb10fb50dbb1796
Mihir Luthra <42952059+MihirLuthra@users.noreply.github.com>
2019-03-31 19:38:18 +0530

    tracelib.c: removed unnecessary condition in if
    if ((kev.flags & EV_ERROR) == 0 || ((kev.flags & EV_ERROR) > 0 && kev.data != 0))
    If compilers find first operand true while evaluating || , the second operand isn’t checked.
    So the second operand is only checked if (kev.flags & EV_ERROR) != 0.
    So (kev.flags & EV_ERROR) > 0  it doesn’t need to be checked in conjunction with kev.data != 0.
    It doesn’t return negative.(tested)

--- src/pextlib1.0/tracelib.c
+++ src/pextlib1.0/tracelib.c
@@ -771,7 +771,7 @@ static int TracelibRunCmd(Tcl_Interp *in) {
         /* kevent(2) on EV_RECEIPT: When passed as input, it forces EV_ERROR to
          * always be returned. When a filter is successfully added, the data field
          * will be zero. */
-        if ((kev.flags & EV_ERROR) == 0 || ((kev.flags & EV_ERROR) > 0 && kev.data != 0)) {
+        if ((kev.flags & EV_ERROR) == 0 || (kev.data != 0)) {
             error2tcl("kevent (listen socket receipt): ", kev.data, in);
             goto error_locked;
         }
