21aaaaee907eb2b17306da01ea6c5ff35dc0191e
Chris Jones <jonesc@hep.phy.cam.ac.uk>
2019-07-23 11:55:12 +0100

    clang 3.9 and 4 have been obsoleted, in favour of clang 8.0, so remove from known compiler fallbacks

--- src/port1.0/portconfigure.tcl
+++ src/port1.0/portconfigure.tcl
@@ -946,21 +946,13 @@ proc portconfigure::get_clang_compilers {} {
         source ${compiler_file}
     } else {
         ui_debug "clang_compilers.tcl not found in ports tree, using built-in selections"
-        lappend compilers macports-clang-7.0 macports-clang-6.0 macports-clang-5.0
-        if {${os.major} < 18} {
-            # see https://github.com/macports/macports-ports/commit/d387f4e4a47b298b1775ea8bf61772e2c2e6cd8b
-            lappend compilers macports-clang-4.0
-            if {${os.major} < 17} {
-                # The High Sierra SDK requires a toolchain that can apply nullability to uuid_t
-                lappend compilers macports-clang-3.9
-                if {${os.major} < 16} {
-                    # The Sierra SDK requires a toolchain that supports class properties
-                    lappend compilers macports-clang-3.7
-                    lappend compilers macports-clang-3.4
-                    if {${os.major} < 9} {
-                        lappend compilers macports-clang-3.3
-                    }
-                }
+        lappend compilers macports-clang-8.0 macports-clang-7.0 macports-clang-6.0 macports-clang-5.0
+        if {${os.major} < 16} {
+            # The Sierra SDK requires a toolchain that supports class properties
+            lappend compilers macports-clang-3.7
+            lappend compilers macports-clang-3.4
+            if {${os.major} < 9} {
+                lappend compilers macports-clang-3.3
             }
         }
     }
