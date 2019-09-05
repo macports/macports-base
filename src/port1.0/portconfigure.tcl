f634538bea86073cc189d9c40c48d0f4a51f0679
Joshua Root <jmr@macports.org>
2019-09-06 00:11:18 +1000

    merge lappend calls
    (cherry picked from commit e01423b2f2a8c610ebe30e844d595847ddca8366)

--- src/port1.0/portconfigure.tcl
+++ src/port1.0/portconfigure.tcl
@@ -957,8 +957,7 @@ proc portconfigure::get_clang_compilers {} {
         lappend compilers macports-clang-8.0 macports-clang-7.0 macports-clang-6.0 macports-clang-5.0
         if {${os.major} < 16} {
             # The Sierra SDK requires a toolchain that supports class properties
-            lappend compilers macports-clang-3.7
-            lappend compilers macports-clang-3.4
+            lappend compilers macports-clang-3.7 macports-clang-3.4
             if {${os.major} < 9} {
                 lappend compilers macports-clang-3.3
             }
