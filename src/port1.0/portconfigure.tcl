db9cb8d7f41cf38046a90a4cdafe762b5b342ad4
Joshua Root <jmr@macports.org>
2019-09-29 03:59:46 +1000

    don't prevent disabling apple-gcc-4.2 altogether
    (cherry picked from commit 427ce2062d66581788a75aba625681cc79f3fbe3)

--- src/port1.0/portconfigure.tcl
+++ src/port1.0/portconfigure.tcl
@@ -1031,13 +1031,13 @@ proc portconfigure::get_compiler_fallback {} {
     }
     set system_compilers ""
     foreach c ${available_apple_compilers} {
-        if {$c eq "apple-gcc-4.2"} {
-            # provided by a port, should be the latest version
-            lappend system_compilers $c
-            continue
-        }
         set vmin [portconfigure::get_min_command_line $c]
         if {$vmin ne "none"} {
+            if {$c eq "apple-gcc-4.2"} {
+                # provided by a port, should be the latest version
+                lappend system_compilers $c
+                continue
+            }
             set v [compiler.command_line_tools_version $c]
             if {[vercmp ${vmin} $v] <= 0} {
                 lappend system_compilers $c
