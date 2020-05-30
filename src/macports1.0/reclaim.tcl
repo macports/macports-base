d08d261f93844cea87f602e1f295e8a0ddc83bde
Kurt Hindenburg <kurt.hindenburg@gmail.com>
2020-05-30 10:54:56 -0400

    reclaim: separate removing build and ccache locations (#178)
    Do not actually remove the build and ccache locations, only the files
    and folders underneath.
    A followup to the discussions in (#175)
    
    See https://trac.macports.org/ticket/60126

--- src/macports1.0/reclaim.tcl
+++ src/macports1.0/reclaim.tcl
@@ -77,6 +77,7 @@ namespace eval reclaim {
         uninstall_inactive
         remove_distfiles
         remove_builds
+        remove_ccache
 
         if {![macports::global_option_isset ports_dryrun]} {
             set last_run_contents [read_last_run_file]
@@ -106,28 +107,77 @@ namespace eval reclaim {
     }
 
     proc remove_builds {} {
-        # Delete portdbpath/build
+        # Delete portdbpath/build directories
         global macports::portdbpath
 
-        # The root build folder location
+        # The root build directory location
         set root_build      [file join ${macports::portdbpath} build]
 
         ui_msg "$macports::ui_prefix Build location: ${root_build}"
 
+        if {[macports::global_option_isset ports_dryrun]} {
+            ui_msg "Deleting... (dry run)"
+            ui_info [msgcat::mc "Skipping deletion of all build directories under %s (dry run)" $root_build]
+            return
+        }
+
+        set builddirs [glob -nocomplain -directory $root_build *]
+        if {[llength $builddirs] == 0} {
+            ui_info [msgcat::mc "No build directories to delete"]
+            return
+        }
+
         set retval 0
         if {[info exists macports::ui_options(questions_yesno)]} {
-            set retval [$macports::ui_options(questions_yesno) "" "" "" "y" 0 "Would you like to delete the entire build directory (including the default ccache location)?"]
+            set retval [$macports::ui_options(questions_yesno) "" "" "" "y" 0 "Would you like to delete all the build directories?"]
         }
 
         if {${retval} == 0} {
+            ui_info [msgcat::mc "Deleting all build directories under %s" $root_build]
             try -pass_signal {
-                if {[macports::global_option_isset ports_dryrun]} {
-                    ui_msg "Deleting... (dry run)"
-                    ui_info [msgcat::mc "Skipping deletion of %s (dry run)" $root_build]
-                } else {
-                    ui_info [msgcat::mc "Deleting %s" $root_build]
-                    file delete -force $root_build
-                }
+                file delete -force -- {*}$builddirs
+            } catch {{*} eCode eMessage} {
+                ui_debug "$::errorInfo"
+                ui_error "$eMessage"
+            }
+        }
+    }
+
+    proc remove_ccache {} {
+        # Delete everything under ccache directory - default is build/.ccache
+        global macports::ccache_dir
+
+        if {![file exists ${macports::ccache_dir}]} {
+            ui_info [msgcat::mc "Skipping deletion of ccache directory: %s does not exist." $macports::ccache_dir]
+            return
+        }
+
+        ui_msg "$macports::ui_prefix ccache location: ${macports::ccache_dir}"
+
+        if {[macports::global_option_isset ports_dryrun]} {
+            ui_msg "Deleting... (dry run)"
+            ui_info [msgcat::mc "Skipping deletion of everything under %s (dry run)" $macports::ccache_dir]
+            return
+        }
+
+        set ccachedirs [glob -nocomplain -directory $macports::ccache_dir *]
+        if {[llength $ccachedirs] == 0} {
+            ui_info [msgcat::mc "No ccache directories to delete"]
+            return
+        }
+
+        set retval 0
+        if {[info exists macports::ui_options(questions_yesno)]} {
+            set retval [$macports::ui_options(questions_yesno) "" "" "" "y" 0 "Would you like to delete everything under the ccache directory?"]
+        }
+
+        if {${retval} == 0} {
+            ui_info [msgcat::mc "Deleting everything under %s" $macports::ccache_dir]
+            try -pass_signal {
+                file delete -force -- {*}$ccachedirs
+            } catch {{*} eCode eMessage} {
+                ui_debug "$::errorInfo"
+                ui_error "$eMessage"
             }
         }
     }
