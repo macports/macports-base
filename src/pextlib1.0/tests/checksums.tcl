f3bf62e621abcd52a4c3b34b0a98ba0205d13e2b
Ryan Schmidt <ryandesign@macports.org>
2020-06-16 23:30:17 -0500

    checksums.tcl: Convert file to utf-8

--- src/pextlib1.0/tests/checksums.tcl
+++ src/pextlib1.0/tests/checksums.tcl
@@ -14,22 +14,22 @@ proc main {pextlibname} {
 	# create a dummy file.
 	set chan [open $testfile w]
 	puts $chan "Article premier"
-	puts $chan "  Tous les êtres humains naissent libres et égaux en dignité "
-	puts $chan " et en droits. Ils sont doués de raison et de conscience et "
+	puts $chan "  Tous les Ãªtres humains naissent libres et Ã©gaux en dignitÃ© "
+	puts $chan " et en droits. Ils sont douÃ©s de raison et de conscience et "
 	puts $chan " doivent agir les uns envers les autres dans un esprit de "
-	puts $chan " fraternité."
+	puts $chan " fraternitÃ©."
 	puts $chan "Article 2"
-	puts $chan "  Chacun peut se prévaloir de tous les droits et de toutes les "
-	puts $chan " libertés proclamés dans la présente Déclaration, sans "
+	puts $chan "  Chacun peut se prÃ©valoir de tous les droits et de toutes les "
+	puts $chan " libertÃ©s proclamÃ©s dans la prÃ©sente DÃ©claration, sans "
 	puts $chan " distinction aucune, notamment de race, de couleur, de sexe, "
 	puts $chan " de langue, de religion, d'opinion politique ou de toute autre "
 	puts $chan " opinion, d'origine nationale ou sociale, de fortune, de "
 	puts $chan " naissance ou de toute autre situation."
-	puts $chan "  De plus, il ne sera fait aucune distinction fondée sur le "
+	puts $chan "  De plus, il ne sera fait aucune distinction fondÃ©e sur le "
 	puts $chan " statut politique, juridique ou international du pays ou du "
 	puts $chan " territoire dont une personne est ressortissante, que ce pays "
-	puts $chan " ou territoire soit indépendant, sous tutelle, non autonome ou "
-	puts $chan " soumis à une limitation quelconque de souveraineté."
+	puts $chan " ou territoire soit indÃ©pendant, sous tutelle, non autonome ou "
+	puts $chan " soumis Ã  une limitation quelconque de souverainetÃ©."
 	close $chan
 	
 	# checksum the file.
