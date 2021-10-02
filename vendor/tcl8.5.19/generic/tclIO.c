f1100f154cde2bc4d6568944bfd98fc5cbd6dceb
Christopher Chavez <chrischavez@gmx.us>
2021-10-02 15:01:04 -0500

    Fix Tcl bug 98af80f133: unsafe buffer lifetime

--- vendor/tcl8.5.19/generic/tclIO.c
+++ vendor/tcl8.5.19/generic/tclIO.c
@@ -3765,6 +3765,7 @@ Write(
 				/* State info for channel */
     char *nextNewLine = NULL;
     int endEncoding, saved = 0, total = 0, flushed = 0, needNlFlush = 0;
+    char safe[BUFFER_PADDING];
 
     if (srcLen) {
         WillWrite(chanPtr);
@@ -3783,7 +3784,7 @@ Write(
 
     while (srcLen + saved + endEncoding > 0) {
 	ChannelBuffer *bufPtr;
-	char *dst, safe[BUFFER_PADDING];
+	char *dst;
 	int result, srcRead, dstLen, dstWrote, srcLimit = srcLen;
 
 	if (nextNewLine) {
