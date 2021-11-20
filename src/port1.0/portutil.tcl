macports/macports-base

commit da3524e8a3006a9b9a2370b66fa91bb554dd768b

Basic target progress bar support (#150)
- Add callback event support for monitoring the progress of SystemCmd.
- Use SystemCmd events to implement basic progress bar support for a subset of targets (including build).

Closes: https://trac.macports.org/ticket/15939

src/port1.0/portutil.tcl
@@ -368,33 +368,55 @@ proc command_string {command} {
 }
 
 # Given a command name, execute it with the options.
-# command_exec command [-notty] [-varprefix variable_prefix] [command_prefix [command_suffix]]
+# command_exec [-notty] [-callback proc] [-varprefix variable_prefix] command [command_prefix [command_suffix]]
 # command           name of the command
 # variable_prefix   name of the variable prefix to use (defaults to command)
 # command_prefix    additional command prefix (typically pipe command)
 # command_suffix    additional command suffix (typically redirection)
-proc command_exec {command args} {
-    set varprefix "${command}"
+proc command_exec {args} {
+    set callback ""
     set notty ""
     set command_prefix ""
     set command_suffix ""
 
-    if {[llength $args] > 0} {
-        if {[lindex $args 0] eq "-notty"} {
-            set notty "-notty"
-            set args [lrange $args 1 end]
-        }
-
-        if {[lindex $args 0] eq "-varprefix"} {
-            set varprefix [lindex $args 1]
-            set args [lrange $args 2 end]
-        }
-
-        if {[llength $args] > 0} {
-            set command_prefix [lindex $args 0]
-            if {[llength $args] > 1} {
-                set command_suffix [lindex $args 1]
+    while {[llength $args] > 0} {
+        switch -glob -- [lindex $args 0] {
+            -notty {
+                set notty "-notty"
+                set args [lrange $args 1 end]
             }
+            -callback {
+                set callback [lrange $args 0 1]
+                set args [lrange $args 2 end]
+            }
+            -varprefix {
+                set varprefix [lindex $args 1]
+                set args [lrange $args 2 end]
+            }
+            -* {
+                return -code error "unknown option [lindex $args 0]"
+            }
+            -- {
+                set args [lrange $args 1 end]
+                break
+            }
+            default {
+                break
+            }
+        }
+    }
+
+    if {[llength $args] == 0} {
+        return -code error "Missing command argument"
+    }
+
+    set command [lindex $args 0]
+    set varprefix "${command}"
+
+    if {[llength $args] > 1} {
+        set command_prefix [lindex $args 1]
+        if {[llength $args] > 2} {
+            set command_suffix [lindex $args 2]
         }
     }
 
@@ -453,7 +475,7 @@ proc command_exec {command args} {
     # Call the command.
     set fullcmdstring "$command_prefix $cmdstring $command_suffix"
     ui_info "Executing: $fullcmdstring"
-    set code [catch {system {*}$notty {*}$nice $fullcmdstring} result]
+    set code [catch {system {*}$notty {*}$callback {*}$nice $fullcmdstring} result]
     # Save variables in order to re-throw the same error code.
     set errcode $::errorCode
     set errinfo $::errorInfo

src/macports1.0/macports.tcl
@@ -1440,9 +1440,13 @@ proc macports::worker_init {workername portpath porturl portbuildpath options va
     foreach priority $macports::ui_priorities {
         $workername alias ui_$priority ui_$priority
     }
-    # add the UI progress call-back
-    if {[info exists macports::ui_options(progress_download)]} {
-        $workername alias ui_progress_download $macports::ui_options(progress_download)
+    # add the UI progress call-backs (or a no-op alias, if unavailable)
+    foreach pname {progress_download progress_generic} {
+        if {[info exists macports::ui_options($pname)]} {
+            $workername alias ui_$pname $macports::ui_options($pname)
+        } else {
+            $workername alias ui_$pname return -level 0
+        }
     }
 
     # notifications callback

src/package1.0/portarchivefetch.tcl
@@ -196,7 +196,7 @@ proc portarchivefetch::fetchfiles {args} {
     if {$portverbose eq "yes"} {
         lappend fetch_options "--progress"
         lappend fetch_options "builtin"
-    } elseif {[llength [info commands ui_progress_download]] > 0} {
+    } else {
         lappend fetch_options "--progress"
         lappend fetch_options "ui_progress_download"
     }

src/port1.0/Makefile.in
@@ -7,7 +7,7 @@ INSTALLDIR=	${TCL_PACKAGE_PATH}/port1.0
 
 SRCS_AUTOCONF= port_autoconf.tcl
 SRCS=	port.tcl portchecksum.tcl portconfigure.tcl portextract.tcl	    \
-	portfetch.tcl portmain.tcl portbuild.tcl portpatch.tcl portutil.tcl \
+	portfetch.tcl portmain.tcl portbuild.tcl portpatch.tcl portprogress.tcl portutil.tcl \
 	portinstall.tcl portuninstall.tcl portdepends.tcl portdestroot.tcl \
 	portlint.tcl portclean.tcl porttest.tcl portactivate.tcl portbump.tcl \
 	portdeactivate.tcl portstartupitem.tcl porttrace.tcl portlivecheck.tcl \

src/port1.0/portbuild.tcl
@@ -32,6 +32,7 @@
 
 package provide portbuild 1.0
 package require portutil 1.0
+package require portprogress 1.0
 
 set org.macports.build [target_new org.macports.build portbuild::build_main]
 target_provides ${org.macports.build} build
@@ -199,7 +200,7 @@ proc portbuild::build_main {args} {
 
     set realcmd ${build.cmd}
     set build.cmd "${build.cmd}$jobs_suffix"
-    command_exec build
+    command_exec -callback portprogress::target_progress_callback build
     set build.cmd ${realcmd}
     return 0
 }

src/port1.0/portconfigure.tcl
@@ -32,6 +32,7 @@
 
 package provide portconfigure 1.0
 package require portutil 1.0
+package require portprogress 1.0
 
 set org.macports.configure [target_new org.macports.configure portconfigure::configure_main]
 target_provides ${org.macports.configure} configure
@@ -1522,32 +1523,34 @@ proc portconfigure::configure_main {args} {
         global configure.${flags} configure.universal_${flags}
     }
 
+    set callback [list "-callback" portprogress::target_progress_callback]
+
     if {[tbool use_autoreconf]} {
-        if {[catch {command_exec autoreconf} result]} {
+        if {[catch {command_exec {*}${callback} autoreconf} result]} {
             return -code error "[format [msgcat::mc "%s failure: %s"] autoreconf $result]"
         }
     }
 
     if {[tbool use_automake]} {
-        if {[catch {command_exec automake} result]} {
+        if {[catch {command_exec {*}${callback} automake} result]} {
             return -code error "[format [msgcat::mc "%s failure: %s"] automake $result]"
         }
     }
 
     if {[tbool use_autoconf]} {
-        if {[catch {command_exec autoconf} result]} {
+        if {[catch {command_exec {*}${callback} autoconf} result]} {
             return -code error "[format [msgcat::mc "%s failure: %s"] autoconf $result]"
         }
     }
 
     if {[tbool use_xmkmf]} {
         parse_environment xmkmf
-        if {[catch {command_exec xmkmf} result]} {
+        if {[catch {command_exec {*}${callback} xmkmf} result]} {
             return -code error "[format [msgcat::mc "%s failure: %s"] xmkmf $result]"
         }
 
         parse_environment xmkmf
-        if {[catch {command_exec "cd ${worksrcpath} && make Makefiles" -varprefix xmkmf} result]} {
+        if {[catch {command_exec {*}${callback} "cd ${worksrcpath} && make Makefiles" -varprefix xmkmf} result]} {
             return -code error "[format [msgcat::mc "%s failure: %s"] "make Makefiles" $result]"
         }
     } elseif {[tbool use_configure]} {
@@ -1637,7 +1640,7 @@ proc portconfigure::configure_main {args} {
         }
 
         # Execute the command (with the new environment).
-        if {[catch {command_exec configure} result]} {
+        if {[catch {command_exec {*}${callback} configure} result]} {
             global configure.dir
             if {[file exists ${configure.dir}/config.log]} {
                 ui_error "[format [msgcat::mc "Failed to configure %s, consult %s/config.log"] [option subport] ${configure.dir}]"

src/port1.0/portdestroot.tcl
@@ -121,7 +121,7 @@ proc portdestroot::destroot_start {args} {
 }
 
 proc portdestroot::destroot_main {args} {
-    command_exec destroot
+    command_exec -callback portprogress::target_progress_callback destroot
     return 0
 }
 

commit ce4235490972c60fad3b9bf7a1af605512ce3abd

Fix command_exec to honor -varprefix again
This was broken by da3524e8a3006a9b9a2370b66fa91bb554dd768b.

See: https://trac.macports.org/ticket/64011

src/port1.0/portutil.tcl
@@ -378,6 +378,7 @@ proc command_exec {args} {
     set notty ""
     set command_prefix ""
     set command_suffix ""
+    set varprefix ""
 
     while {[llength $args] > 0} {
         switch -glob -- [lindex $args 0] {
@@ -411,7 +412,9 @@ proc command_exec {args} {
     }
 
     set command [lindex $args 0]
-    set varprefix "${command}"
+    if {$varprefix eq ""} {
+        set varprefix "$command"
+    }
 
     if {[llength $args] > 1} {
         set command_prefix [lindex $args 1]