4ae09d389cfc046d3fa598f1bdb2da60380e16b2
Zero King <l2dy@macports.org>
2022-02-14 16:24:08 +0000

    daemondo: tabs to spaces

--- src/programs/daemondo/main.c
+++ src/programs/daemondo/main.c
@@ -111,7 +111,7 @@ io_connect_t        pwrRootPort         = 0;
 int                 restartOnWakeup     = 0;        // TRUE to restart daemon on wake from sleep
 CFRunLoopTimerRef   restartTimer        = NULL;     // Timer for scheduled restart
 CFTimeInterval      restartHysteresis   = 5.0;      // Default hysteresis is 5 seconds
-int				    restartWait		   	= 3;      	// Default wait during restart is 3 seconds
+int                 restartWait         = 3;        // Default wait during restart is 3 seconds
 
 
 __printflike(1, 2)
@@ -296,15 +296,15 @@ DestroyPidFile(void)
             break;
         case kPidStyleExec:         // We wrote the file, and we'll remove it
         case kPidStyleFileClean:    // The process wrote the file, but we'll remove it
-			if (verbosity >= 5)
-				LogMessage("Attempting to delete pidfile %s\n", pidFile);
+            if (verbosity >= 5)
+                LogMessage("Attempting to delete pidfile %s\n", pidFile);
             if (unlink(pidFile) && verbosity >= 3)
-				LogMessage("Failed attempt to delete pidfile %s (%d)\n", pidFile, errno);            
+                LogMessage("Failed attempt to delete pidfile %s (%d)\n", pidFile, errno);
             break;
         }
     } else {
-		if (verbosity >= 5)
-			LogMessage("No pidfile to delete: none specified\n");
+        if (verbosity >= 5)
+            LogMessage("No pidfile to delete: none specified\n");
     }
 }
 
@@ -352,21 +352,21 @@ DeletePreexistingPidFile(void)
     
     // Log information about the discovered pid file
     if (verbosity >= 3 && pid != -1) {
-    	LogMessage("Discovered preexisting pidfile %s containing pid %d which is a %s process\n", pidFile, pid, 
-    		(valid) ? "valid" : "invalid");
+        LogMessage("Discovered preexisting pidfile %s containing pid %d which is a %s process\n", pidFile, pid,
+            (valid) ? "valid" : "invalid");
     }
-    
+
     // Try to delete the pidfile if it's present
     if (pid != -1) {
-	    if (unlink(pidFile)) {
-	    	if (verbosity >= 3)
-		  		LogMessage("Error %d while trying to cleanup preexisting pidfile %s\n", errno, pidFile);
-	  	} else {
- 	  		if (verbosity >= 3)
-		  		LogMessage("Deleted preexisting pidfile %s\n", pidFile);
-		}
-	}
-    
+        if (unlink(pidFile)) {
+            if (verbosity >= 3)
+                LogMessage("Error %d while trying to cleanup preexisting pidfile %s\n", errno, pidFile);
+        } else {
+            if (verbosity >= 3)
+                LogMessage("Deleted preexisting pidfile %s\n", pidFile);
+        }
+    }
+
     return pid;
 }
 
@@ -548,8 +548,8 @@ Start(void)
     
     if (verbosity >= 1)
         LogMessage("Starting process\n");
-	if (pidFile != NULL)
-		DeletePreexistingPidFile();
+    if (pidFile != NULL)
+        DeletePreexistingPidFile();
     if (verbosity >= 2)
         LogMessage("Running start-cmd %s\n", CatArray(startArgs, buf, sizeof(buf)));
         
@@ -686,8 +686,8 @@ Restart(void)
     }
     else
     {
-    	// Bug: we should recapture the target process id from the pidfile in this case
-    	
+        // Bug: we should recapture the target process id from the pidfile in this case
+
         // Execute the restart-cmd and trust it to do the job
         if (verbosity >= 1)
             LogMessage("Restarting process\n");
