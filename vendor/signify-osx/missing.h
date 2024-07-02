b005e6d62b4ad399217a943641dabe83abcc7800
Joshua Root <jmr@macports.org>
2024-07-03 00:17:38 +1000

    signify: restore compatibility functions

--- vendor/signify-osx/missing.h
+++ vendor/signify-osx/missing.h
@@ -2,3 +2,15 @@
 
 void explicit_bzero(void *, size_t);
 void freezero(void *, size_t);
+
+#ifndef HAVE_ARC4RANDOM_BUF
+void arc4random_buf(void *, size_t);
+#endif
+
+#ifndef HAVE_GETENTROPY
+int getentropy(void *, size_t);
+#endif
+
+#ifndef HAVE_TIMINGSAFE_BCMP
+int timingsafe_bcmp(const void *, const void *, size_t);
+#endif
---
+++ vendor/signify-osx/src/lib/libc/crypt/arc4random.h
@@ -0,0 +1,1 @@
+#include <arc4random/arc4random_osx.h>
