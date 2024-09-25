0fcf3314f3e4b85d631d4debd1826d1aed4d82a0
Ryan Carsten Schmidt <git@ryandesign.com>
2024-09-25 02:50:47 -0500

    cp: NULL-terminate new_argv
    Closes: https://trac.macports.org/ticket/70897

--- src/programs/cp/cp.c
+++ src/programs/cp/cp.c
@@ -9,11 +9,11 @@ int main(int argc, char *argv[])
 {
     const char *cp_path = "/bin/cp";
     const char *clone_arg = "-c";
-    const char **new_argv = malloc(sizeof(char *) * (argc+1));
+    const char **new_argv = malloc(sizeof(char *) * (argc+2));
     if (new_argv) {
         new_argv[0] = cp_path;
         new_argv[1] = clone_arg;
-        for (int i = 1; i < argc; i++) {
+        for (int i = 1; i <= argc; i++) {
             new_argv[i+1] = argv[i];
         }
         execv(cp_path, new_argv);
