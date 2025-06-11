2d92aaae3e120a58249e3d53662744db0663a867
Joshua Root <jmr@macports.org>
2025-06-12 07:27:50 +1000

    Makefiles: quote $(MAKE)
    See: https://trac.macports.org/ticket/72582
    (cherry picked from commit 7fbe0c8a92e21374f84faf082ba9d66adf51e2b8)

--- Mk/macports.subdir.mk
+++ Mk/macports.subdir.mk
@@ -7,24 +7,24 @@
 all::
 	@for subdir in $(SUBDIR); do\
 		echo ===\> making $@ in ${DIRPRFX}$$subdir; \
-		( cd $$subdir && $(MAKE) DIRPRFX=${DIRPRFX}$$subdir/ $@) || exit 1; \
+		( cd $$subdir && "$(MAKE)" DIRPRFX="${DIRPRFX}$$subdir/" $@) || exit 1; \
 	done
 
 distclean:: clean
 clean distclean::
 	@for subdir in $(SUBDIR); do\
 		echo ===\> making $@ in ${DIRPRFX}$$subdir; \
-		( cd $$subdir && $(MAKE) DIRPRFX=${DIRPRFX}$$subdir/ $@) || exit 1; \
+		( cd $$subdir && "$(MAKE)" DIRPRFX="${DIRPRFX}$$subdir/" $@) || exit 1; \
 	done
 
 test::
 	@for subdir in $(SUBDIR); do\
 		echo ===\> making $@ in ${DIRPRFX}$$subdir; \
-		( cd $$subdir && $(MAKE) DIRPRFX=${DIRPRFX}$$subdir/ $@) || exit 1; \
+		( cd $$subdir && "$(MAKE)" DIRPRFX="${DIRPRFX}$$subdir/" $@) || exit 1; \
 	done
 
 install::
 	@for subdir in $(SUBDIR); do\
 		echo ===\> making $@ in ${DIRPRFX}$$subdir; \
-		( cd $$subdir && $(MAKE) DIRPRFX=${DIRPRFX}$$subdir/ $@) || exit 1; \
+		( cd $$subdir && "$(MAKE)" DIRPRFX="${DIRPRFX}$$subdir/" $@) || exit 1; \
 	done
