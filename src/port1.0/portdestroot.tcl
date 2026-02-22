74895e10b0a117b70d77b55e18d71d8cd191ff84
Joshua Root <jmr@macports.org>
2026-02-23 03:02:50 +1100

    destroot_finish: use lchown, preserve permissions
    See: https://trac.macports.org/ticket/73516
    (cherry picked from commit fc5a3d6fada1ab4edf617ac5742d2136ae2c616a)

--- src/port1.0/portdestroot.tcl
+++ src/port1.0/portdestroot.tcl
@@ -169,14 +169,31 @@ proc portdestroot::destroot_finish {args} {
     fs-traverse -depth fullpath [list $destroot] {
         file lstat $fullpath statinfo
         # Ensure installed files are not owned by the unprivileged account
-        if {$fix_ownership} {
+        if {$fix_ownership && ($statinfo(uid) == $macports_uid || $statinfo(gid) == $macports_gid)} {
             if {$statinfo(uid) == $macports_uid} {
                 ui_debug "Changing owner to ${install.user} for $fullpath"
-                file attributes $fullpath -owner ${install.user}
+                set new_owner ${install.user}
+            } else {
+                # only group needs to be changed
+                set new_owner [name_to_uid $statinfo(uid)]
             }
             if {$statinfo(gid) == $macports_gid} {
                 ui_debug "Changing group to ${install.group} for $fullpath"
-                file attributes $fullpath -group ${install.group}
+                set new_group ${install.group}
+            } else {
+                # only owner needs to be changed
+                set new_group [name_to_gid $statinfo(gid)]
+            }
+            # Changing owner may also change permissions, so we restore
+            # them afterwards. 'file attributes' doesn't work on links
+            # (it operates on the link target instead) but links should
+            # not have setuid/setgid bits set anyway.
+            if {$statinfo(type) ne "link"} {
+                set saved_perms [file attributes $fullpath -permissions]
+            }
+            lchown $fullpath $new_owner $new_group
+            if {$statinfo(type) ne "link"} {
+                file attributes $fullpath -permissions $saved_perms
             }
         }
         if {[file extension $fullpath] eq ".la" && $statinfo(type) in {file link}} {
