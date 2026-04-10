f119b923181d554a9ee9545498d2c21196d0b370
Joshua Root <jmr@macports.org>
2026-04-11 00:24:31 +1000

    Restore tcl/compat/zlib/contrib/minizip
    It's now needed by zipfs.

--- vendor/patches/tcl/delete-files.sh
+++ vendor/patches/tcl/delete-files.sh
@@ -4,6 +4,11 @@
 # Run from vendor/
 
 rm tcl9.*/compat/zlib/doc/crc-doc.1.0.pdf
-rm -r tcl9.*/compat/zlib/{amiga,contrib,msdos,nintendods,old,os400,qnx,watcom,win32,win64,win64-arm}
+rm -r tcl9.*/compat/zlib/{amiga,msdos,nintendods,old,os400,qnx,watcom,win32,win64,win64-arm}
+for contrib_dir in tcl9.*/compat/zlib/contrib/*; do
+    if [ "$(basename "$contrib_dir")" != "minizip" ]; then
+        rm -r "$contrib_dir"
+    fi
+done
 rm -r tcl9.*/pkgs/{sqlite3,itcl,tdbc,thread}*/win
 rm -r tcl9.*/win
