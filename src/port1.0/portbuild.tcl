bb15d6726d4d21de892823b85e0332e2bdc2e62c
Joshua Root <jmr@macports.org>
2026-09-24 12:40:26 +1000

    build_getjobs: consolidate globals

--- src/port1.0/portbuild.tcl
+++ src/port1.0/portbuild.tcl
@@ -153,10 +153,9 @@ proc portbuild::build_getjobs {args} {
         }
 
         macports_try -pass_signal {
-            global os.major os.platform
+            global os.major os.platform build.mem_per_job
             set sysctl_name [expr {(${os.major} >= 23 && ${os.platform} eq "darwin") ? "hw.memsize_usable" : "hw.memsize"}]
             set memsize [sysctl $sysctl_name]
-            global build.mem_per_job
             set jobs_limit_mem [expr {int($memsize / (${build.mem_per_job} * 1024 * 1024)) + 1}]
             if {$jobs > $jobs_limit_mem} {
                 set jobs $jobs_limit_mem
